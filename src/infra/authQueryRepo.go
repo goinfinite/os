de1bfacca665e4e46e1890e5f5ac597384fbb6a5
ntorga <northontorga@gmail.com>
2023-06-21 14:57:03 -0300

    fix: add pam service name

--- src/infra/authQueryRepo.go
+++ src/infra/authQueryRepo.go
@@ -13,20 +13,22 @@ type AuthQueryRepo struct {
 
 func (repo AuthQueryRepo) IsLoginValid(login dto.Login) bool {
 	tx, err := pam.StartFunc(
-		"",
+		"system-auth",
 		login.Username.String(),
 		func(s pam.Style, msg string) (string, error) {
 			switch s {
 			case pam.PromptEchoOff:
 				return login.Password.String(), nil
 			case pam.PromptEchoOn, pam.ErrorMsg, pam.TextInfo:
+				log.Printf("PamMessage: %s", msg)
 				return "", nil
 			}
-			log.Println("PamAuthFailed")
-			return "", errors.New("PamAuthFailed")
+			log.Println("UnhandledPamMessageStyle")
+			return "", errors.New("UnhandledPamMessageStyle")
 		})
 
 	if err != nil {
+		log.Printf("PamError: %v", err)
 		return false
 	}
 
