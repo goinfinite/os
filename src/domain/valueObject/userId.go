c7534eda79db57cfd2b1caf38899ade5f9cd0bd5
ntorga <northontorga@gmail.com>
2023-06-23 18:31:30 -0300

    feat: add new user id from float method

--- src/domain/valueObject/userId.go
+++ src/domain/valueObject/userId.go
@@ -23,6 +23,16 @@ func NewUserIdFromStringPanic(value string) UserId {
 	return UserId(accId)
 }
 
+func NewUserIdFromFloat(value float64) (UserId, error) {
+	accId, err := strconv.ParseInt(
+		strconv.FormatFloat(value, 'f', -1, 64), 10, 64,
+	)
+	if err != nil {
+		return 0, errors.New("InvalidUserId")
+	}
+	return UserId(accId), nil
+}
+
 func NewUserIdFromFloatPanic(value float64) UserId {
 	accId, err := strconv.ParseInt(
 		strconv.FormatFloat(value, 'f', -1, 64), 10, 64,
