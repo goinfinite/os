837959c2d7d3e4ecf0752c1ac4eb8a9cd400926b
ntorga <northontorga@gmail.com>
2023-07-14 10:48:46 -0300

    feat: add update file

---
+++ src/infra/helper/updateFile.go
@@ -0,0 +1,32 @@
+package infraHelper
+
+import (
+	"bufio"
+	"os"
+)
+
+func UpdateFile(filePath string, content string, shouldOverwrite bool) error {
+	fileFlags := os.O_WRONLY | os.O_CREATE | os.O_APPEND
+	if shouldOverwrite {
+		fileFlags = os.O_WRONLY | os.O_CREATE | os.O_TRUNC
+	}
+
+	file, err := os.OpenFile(filePath, fileFlags, 0600)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	_, err = writer.WriteString(content)
+	if err != nil {
+		return err
+	}
+
+	err = writer.Flush()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
