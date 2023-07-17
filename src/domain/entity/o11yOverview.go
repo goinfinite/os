4808e22ea3eeea5007a8c5b7de0d89032af8cf3d
ntorga <northontorga@gmail.com>
2023-07-17 14:22:14 -0300

    fix: add secs to uptime

--- src/domain/entity/o11yOverview.go
+++ src/domain/entity/o11yOverview.go
@@ -7,7 +7,7 @@ import (
 type O11yOverview struct {
 	Hostname             valueObject.Fqdn                 `json:"hostname"`
 	RuntimeContext       valueObject.RuntimeContext       `json:"runtimeContext"`
-	Uptime               uint64                           `json:"uptime"`
+	UptimeSecs           uint64                           `json:"uptimeSecs"`
 	PublicIpAddress      valueObject.IpAddress            `json:"publicIp"`
 	HardwareSpecs        valueObject.HardwareSpecs        `json:"specs"`
 	CurrentResourceUsage valueObject.CurrentResourceUsage `json:"currentUsage"`
@@ -24,7 +24,7 @@ func NewO11yOverview(
 	return O11yOverview{
 		Hostname:             hostname,
 		RuntimeContext:       runtimeContext,
-		Uptime:               uptime,
+		UptimeSecs:           uptime,
 		PublicIpAddress:      publicIpAddress,
 		HardwareSpecs:        hardwareSpecs,
 		CurrentResourceUsage: currentResourceUsage,
