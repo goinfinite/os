7d628b223161e1e419eb53c494a5ba3077a89a39
ntorga <northontorga@gmail.com>
2023-07-24 14:04:50 -0300

    fix: remove useless error return

--- src/presentation/api/helper/missingParam.go
+++ src/presentation/api/helper/missingParam.go
@@ -7,7 +7,7 @@ import (
 func CheckMissingParams(
 	requestBody map[string]interface{},
 	requiredParams []string,
-) error {
+) {
 	missingParams := []string{}
 	for _, param := range requiredParams {
 		if _, ok := requestBody[param]; !ok {
@@ -18,6 +18,4 @@ func CheckMissingParams(
 	if len(missingParams) > 0 {
 		panic("MissingParams: " + strings.Join(missingParams, ", "))
 	}
-
-	return nil
 }
