fda05e8b0c441a341bdf1819ee2d1af0d10ea362
ntorga <northontorga@gmail.com>
2023-07-25 13:50:08 -0300

    feat: add php version entity

---
+++ src/domain/entity/phpVersion.go
@@ -0,0 +1,18 @@
+package entity
+
+import "github.com/speedianet/sam/src/domain/valueObject"
+
+type PhpVersion struct {
+	Value   valueObject.PhpVersion   `json:"value"`
+	Options []valueObject.PhpVersion `json:"options"`
+}
+
+func NewPhpVersion(
+	value valueObject.PhpVersion,
+	options []valueObject.PhpVersion,
+) PhpVersion {
+	return PhpVersion{
+		Value:   value,
+		Options: options,
+	}
+}
