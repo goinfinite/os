c1209c86782393080ac46b9ffce32437365bdce0
ntorga <northontorga@gmail.com>
2023-07-25 13:50:00 -0300

    feat: add php configs entity

---
+++ src/domain/entity/phpConfigs.go
@@ -0,0 +1,26 @@
+package entity
+
+import (
+	"github.com/speedianet/sam/src/domain/valueObject"
+)
+
+type PhpConfigs struct {
+	Hostname valueObject.Fqdn `json:"hostname"`
+	Version  PhpVersion       `json:"version"`
+	Settings []PhpSetting     `json:"settings"`
+	Modules  []PhpModule      `json:"modules"`
+}
+
+func NewPhpConfigs(
+	hostname valueObject.Fqdn,
+	version PhpVersion,
+	settings []PhpSetting,
+	modules []PhpModule,
+) PhpConfigs {
+	return PhpConfigs{
+		Hostname: hostname,
+		Version:  version,
+		Settings: settings,
+		Modules:  modules,
+	}
+}
