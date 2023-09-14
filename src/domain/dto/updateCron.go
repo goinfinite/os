030c1415cf4a837ccc594a23b33a1dd05aeb271b
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2023-09-14 11:03:54 -0300

    fix: add id to udpateCron

--- src/domain/dto/updateCron.go
+++ src/domain/dto/updateCron.go
@@ -3,17 +3,20 @@ package dto
 import "github.com/speedianet/sam/src/domain/valueObject"
 
 type UpdateCron struct {
-	Schedule valueObject.CronSchedule `json:"schedule"`
-	Command  *valueObject.UnixCommand `json:"command,omitempty"`
-	Comment  *valueObject.CronComment `json:"comment,omitempty"`
+	Id       valueObject.CronId        `json:"id"`
+	Schedule *valueObject.CronSchedule `json:"schedule,omitempty"`
+	Command  *valueObject.UnixCommand  `json:"command,omitempty"`
+	Comment  *valueObject.CronComment  `json:"comment,omitempty"`
 }
 
 func NewUpdateCron(
-	schedule valueObject.CronSchedule,
+	id valueObject.CronId,
+	schedule *valueObject.CronSchedule,
 	command *valueObject.UnixCommand,
 	comment *valueObject.CronComment,
 ) UpdateCron {
 	return UpdateCron{
+		Id:       id,
 		Schedule: schedule,
 		Command:  command,
 		Comment:  comment,
