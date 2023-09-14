9748fd779cb9d8a35a007f67a0686d3eb57a4abe
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2023-09-14 11:31:04 -0300

    feat: add GetById to cronQueryRepo interface

--- src/domain/repository/cronQueryRepo.go
+++ src/domain/repository/cronQueryRepo.go
@@ -2,8 +2,12 @@ package repository
 
 import (
 	"github.com/speedianet/sam/src/domain/entity"
+	"github.com/speedianet/sam/src/domain/valueObject"
 )
 
 type CronQueryRepo interface {
 	Get() ([]entity.Cron, error)
+	GetById(
+		croId valueObject.CronId,
+	) (entity.Cron, error)
 }
