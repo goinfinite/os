4a0479a14ab9fa627277f8042129f6e38d6cba4c
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2023-09-14 13:03:39 -0300

    refact: add cronjob as parameter in Update in CronCmdRepo interface

--- src/domain/repository/cronCmdRepo.go
+++ src/domain/repository/cronCmdRepo.go
@@ -2,9 +2,10 @@ package repository
 
 import (
 	"github.com/speedianet/sam/src/domain/dto"
+	"github.com/speedianet/sam/src/domain/entity"
 )
 
 type CronCmdRepo interface {
 	Add(addCron dto.AddCron) error
-	Update(updateCron dto.UpdateCron) error
+	Update(cronjob entity.Cron, updateCron dto.UpdateCron) error
 }
