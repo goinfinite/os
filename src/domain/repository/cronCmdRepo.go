cf297a5626d26135d91f22c48d4f39b73d706465
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2023-09-18 02:31:23 -0300

    feat: add Delete method to CronCmdRepo interface

--- src/domain/repository/cronCmdRepo.go
+++ src/domain/repository/cronCmdRepo.go
@@ -3,9 +3,11 @@ package repository
 import (
 	"github.com/speedianet/sam/src/domain/dto"
 	"github.com/speedianet/sam/src/domain/entity"
+	"github.com/speedianet/sam/src/domain/valueObject"
 )
 
 type CronCmdRepo interface {
 	Add(addCron dto.AddCron) error
 	Update(cronjob entity.Cron, updateCron dto.UpdateCron) error
+	Delete(accId valueObject.CronId) error
 }
