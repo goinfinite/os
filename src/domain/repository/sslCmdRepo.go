26fadfe5a473bd62454ded33c3b9e5dbe30b1513
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2023-10-04 18:34:11 -0300

    feat: add Delete method to sslCmdRepo domain interface

--- src/domain/repository/sslCmdRepo.go
+++ src/domain/repository/sslCmdRepo.go
@@ -2,8 +2,10 @@ package repository
 
 import (
 	"github.com/speedianet/sam/src/domain/dto"
+	"github.com/speedianet/sam/src/domain/valueObject"
 )
 
 type SslCmdRepo interface {
 	Add(addSsl dto.AddSsl) error
+	Delete(sslId valueObject.SslId) error
 }
