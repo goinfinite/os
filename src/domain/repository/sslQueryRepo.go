bef835fa693886bc2d52df35eaf13e66da512f12
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2023-10-04 19:24:09 -0300

    feat: add GetById to SslQueryRepo domain interface

--- src/domain/repository/sslQueryRepo.go
+++ src/domain/repository/sslQueryRepo.go
@@ -2,8 +2,10 @@ package repository
 
 import (
 	"github.com/speedianet/sam/src/domain/entity"
+	"github.com/speedianet/sam/src/domain/valueObject"
 )
 
 type SslQueryRepo interface {
 	Get() ([]entity.Ssl, error)
+	GetById(sslId valueObject.SslId) (entity.Ssl, error)
 }
