64e071bcfdedb35e90cca91656284b7c217ba165
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2023-10-05 13:31:44 -0300

    feat: implements InterfaceToUint vo helper to sslId

--- src/domain/valueObject/sslId.go
+++ src/domain/valueObject/sslId.go
@@ -2,36 +2,15 @@ package valueObject
 
 import (
 	"errors"
-	"reflect"
 	"strconv"
+
+	voHelper "github.com/speedianet/sam/src/domain/valueObject/helper"
 )
 
 type SslId uint64
 
 func NewSslId(value interface{}) (SslId, error) {
-	var sslId uint64
-	var err error
-	switch v := value.(type) {
-	case string:
-		sslId, err = strconv.ParseUint(v, 10, 64)
-	case int, int8, int16, int32, int64:
-		intValue := reflect.ValueOf(v).Int()
-		if intValue < 0 {
-			err = errors.New("InvalidSslId")
-		}
-		sslId = uint64(intValue)
-	case uint, uint8, uint16, uint32, uint64:
-		sslId = uint64(reflect.ValueOf(v).Uint())
-	case float32, float64:
-		floatValue := reflect.ValueOf(v).Float()
-		if floatValue < 0 {
-			err = errors.New("InvalidSslId")
-		}
-		sslId = uint64(floatValue)
-	default:
-		err = errors.New("InvalidSslId")
-	}
-
+	sslId, err := voHelper.InterfaceToUint(value)
 	if err != nil {
 		return 0, errors.New("InvalidSslId")
 	}
