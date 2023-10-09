486a1dc1836c650f481fbb26bcd065aaae71901e
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2023-10-09 02:31:25 -0300

    refact: remove virtualHost vo and implements in ssl entity

--- src/domain/entity/ssl.go
+++ src/domain/entity/ssl.go
@@ -3,16 +3,16 @@ package entity
 import "github.com/speedianet/sam/src/domain/valueObject"
 
 type Ssl struct {
-	Id                valueObject.SslId       `json:"id"`
-	Hostname          valueObject.VirtualHost `json:"hostname"`
-	Certificate       SslPair                 `json:"certificate"`
-	Key               SslPrivateKey           `json:"key"`
-	ChainCertificates []SslPair               `json:"chainCertificates"`
+	Id                valueObject.SslId `json:"id"`
+	Hostname          valueObject.Fqdn  `json:"hostname"`
+	Certificate       SslPair           `json:"certificate"`
+	Key               SslPrivateKey     `json:"key"`
+	ChainCertificates []SslPair         `json:"chainCertificates"`
 }
 
 func NewSsl(
 	id valueObject.SslId,
-	hostname valueObject.VirtualHost,
+	hostname valueObject.Fqdn,
 	certificate SslPair,
 	key SslPrivateKey,
 	chainCertificates []SslPair,
