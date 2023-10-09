0ed82d28259c17f8862cc74afdc8b6ff86371010
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2023-10-09 02:25:59 -0300

    refact: delete sslCertificate and sslPrivateKey and transform them into entities

---
+++ src/domain/entity/sslPair.go
@@ -0,0 +1,37 @@
+package entity
+
+import (
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+	"math/big"
+	"time"
+)
+
+type SslPair struct {
+	Certificate  string
+	SerialNumber big.Int
+	CommonName   string
+	IssuedAt     time.Time
+	ExpiresAt    time.Time
+}
+
+func NewSslPair(certificate string) (SslPair, error) {
+	block, _ := pem.Decode([]byte(certificate))
+	if block == nil {
+		return SslPair{}, errors.New("SslPairError")
+	}
+
+	parsedCert, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		return SslPair{}, err
+	}
+
+	return SslPair{
+		Certificate:  certificate,
+		SerialNumber: *parsedCert.SerialNumber,
+		CommonName:   parsedCert.Subject.CommonName,
+		IssuedAt:     parsedCert.NotBefore,
+		ExpiresAt:    parsedCert.NotAfter,
+	}, nil
+}
---
+++ src/domain/entity/sslPrivateKey.go
@@ -0,0 +1,26 @@
+package entity
+
+import (
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+)
+
+type SslPrivateKey struct {
+	Key string
+}
+
+func NewSslPrivateKey(privateKey string) (SslPrivateKey, error) {
+	block, _ := pem.Decode([]byte(privateKey))
+	if block == nil {
+		return SslPrivateKey{}, errors.New("SslPrivateKeyError")
+	}
+	_, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return SslPrivateKey{}, nil
+	}
+
+	return SslPrivateKey{
+		Key: privateKey,
+	}, nil
+}
