28f02b481b4d59478be33bc98c8f8d2b7b9502bb
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2023-10-13 17:21:47 -0300

    refact: remove getSslPair DTO and return the entire entity

--- src/domain/useCase/getSslPairs.go
+++ src/domain/useCase/getSslPairs.go
@@ -1,29 +1,12 @@
 package useCase
 
 import (
-	"github.com/speedianet/sam/src/domain/dto"
+	"github.com/speedianet/sam/src/domain/entity"
 	"github.com/speedianet/sam/src/domain/repository"
 )
 
 func GetSslPairs(
 	sslQueryRepo repository.SslQueryRepo,
-) ([]dto.GetSslPair, error) {
-	sslPairList, err := sslQueryRepo.GetSslPairs()
-	if err != nil {
-		return []dto.GetSslPair{}, err
-	}
-
-	sslPairListFormatted := []dto.GetSslPair{}
-	for _, ssl := range sslPairList {
-		sslFormatted := dto.NewGetSslPair(
-			ssl.SerialNumber,
-			ssl.Hostname,
-			ssl.Certificate,
-			ssl.Key,
-			ssl.ChainCertificates,
-		)
-		sslPairListFormatted = append(sslPairListFormatted, sslFormatted)
-	}
-
-	return sslPairListFormatted, nil
+) ([]entity.SslPair, error) {
+	return sslQueryRepo.GetSslPairs()
 }
