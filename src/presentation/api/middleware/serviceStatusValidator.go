04427e4c3a72a1efe98d85b5264ea5218541e5e7
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2023-10-16 15:41:00 -0300

    refact: move service checking function to shared presentation layer and calls in serviceStatusValidator API & CLI middleware

--- src/presentation/api/middleware/serviceStatusValidator.go
+++ src/presentation/api/middleware/serviceStatusValidator.go
@@ -4,37 +4,17 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
-	"github.com/speedianet/sam/src/domain/valueObject"
-	"github.com/speedianet/sam/src/infra"
+	"github.com/speedianet/sam/src/presentation/shared"
 )
 
 func ServiceStatusValidator(serviceNameStr string) echo.MiddlewareFunc {
-	servicesQueryRepo := infra.ServicesQueryRepo{}
-
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
-			serviceName, err := valueObject.NewServiceName(serviceNameStr)
-
-			currentSvcStatus, err := servicesQueryRepo.GetByName(serviceName)
+			err := shared.CheckServices(serviceNameStr)
 			if err != nil {
-				return err
-			}
-
-			var badRequestMessage string
-
-			isStopped := currentSvcStatus.Status.String() == "stopped"
-			if isStopped {
-				badRequestMessage = "ServiceStopped"
-			}
-			isUninstalled := currentSvcStatus.Status.String() == "uninstalled"
-			if isUninstalled {
-				badRequestMessage = "ServiceNotInstalled"
-			}
-			shouldInstall := isStopped || isUninstalled
-			if shouldInstall {
 				return echo.NewHTTPError(http.StatusBadRequest, map[string]interface{}{
 					"status": http.StatusBadRequest,
-					"body":   badRequestMessage,
+					"body":   err.Error(),
 				})
 			}
 
--- src/presentation/cli/middleware/serviceStatusValidator.go
+++ src/presentation/cli/middleware/serviceStatusValidator.go
@@ -1,36 +1,16 @@
 package cliMiddleware
 
 import (
-	"github.com/speedianet/sam/src/domain/valueObject"
-	"github.com/speedianet/sam/src/infra"
 	cliHelper "github.com/speedianet/sam/src/presentation/cli/helper"
+	"github.com/speedianet/sam/src/presentation/shared"
 	"github.com/spf13/cobra"
 )
 
 func ServiceStatusValidator(serviceNameStr string) func(cmd *cobra.Command, args []string) {
-	servicesQueryRepo := infra.ServicesQueryRepo{}
-
 	return func(cmd *cobra.Command, args []string) {
-		serviceName, err := valueObject.NewServiceName(serviceNameStr)
-
-		currentSvcStatus, err := servicesQueryRepo.GetByName(serviceName)
+		err := shared.CheckServices(serviceNameStr)
 		if err != nil {
-			cliHelper.ResponseWrapper(false, "Failed to get "+serviceNameStr+" service: "+err.Error())
-		}
-
-		var badCommandMessage string
-
-		isStopped := currentSvcStatus.Status.String() == "stopped"
-		if isStopped {
-			badCommandMessage = "Service paused"
-		}
-		isUninstalled := currentSvcStatus.Status.String() == "uninstalled"
-		if isUninstalled {
-			badCommandMessage = "Service not installed"
-		}
-		shouldInstall := isStopped || isUninstalled
-		if shouldInstall {
-			cliHelper.ResponseWrapper(false, badCommandMessage)
+			cliHelper.ResponseWrapper(false, err.Error())
 		}
 	}
 }
