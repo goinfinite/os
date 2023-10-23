30c5e6c4e54f34e625284c9bae52542cb274d37a
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2023-10-23 17:42:33 -0300

    refact: change schedule regex names

--- src/domain/valueObject/cronSchedule.go
+++ src/domain/valueObject/cronSchedule.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 )
 
-const cronScheduleFrequencyRegex string = `^((?P<frequencyStr>(@(annually|yearly|monthly|weekly|daily|hourly|reboot))|(@every (\d+(ns|us|µs|ms|s|m|h))+)) ?|((?P<minute>(\d+,)+\d+|(\d+(\/|-)\d+)|\d+|\*|\*/\d+){1})(?: )((?P<hour>(\d+,)+\d+|(\d+(\/|-)\d+)|\d+|\*|\*/\d+){1})(?: )((?P<day>(\d+,)+\d+|(\d+(\/|-)\d+)|\d+|\*|\*/\d+){1})(?: )((?P<month>(\d+,)+\d+|(\d+(\/|-)\d+)|\d+|\*|\*/\d+){1})(?: )((?P<weekday>(\d+,)+\d+|(\d+(\/|-)\d+)|\d+|\*|\*/\d+){1})(?: )?)$`
+const cronScheduleRegex string = `^((?P<frequencyStr>(@(annually|yearly|monthly|weekly|daily|hourly|reboot))|(@every (\d+(ns|us|µs|ms|s|m|h))+)) ?|((?P<minute>(\d+,)+\d+|(\d+(\/|-)\d+)|\d+|\*|\*/\d+){1})(?: )((?P<hour>(\d+,)+\d+|(\d+(\/|-)\d+)|\d+|\*|\*/\d+){1})(?: )((?P<day>(\d+,)+\d+|(\d+(\/|-)\d+)|\d+|\*|\*/\d+){1})(?: )((?P<month>(\d+,)+\d+|(\d+(\/|-)\d+)|\d+|\*|\*/\d+){1})(?: )((?P<weekday>(\d+,)+\d+|(\d+(\/|-)\d+)|\d+|\*|\*/\d+){1})(?: )?)$`
 
 type CronSchedule string
 
@@ -36,13 +36,13 @@ func NewCronSchedulePanic(value string) CronSchedule {
 }
 
 func shouldHaveAtSign(value string) bool {
-	cronScheduleCustomFrequencyRegex := `^((@?(annually|yearly|monthly|weekly|daily|hourly|reboot))|(@every (\d+(?:ns|us|µs|ms|s|m|h))+))$`
-	frequencyRegex := regexp.MustCompile(cronScheduleCustomFrequencyRegex)
+	cronPredefinedScheduleRegex := `^((@?(annually|yearly|monthly|weekly|daily|hourly|reboot))|(@every (\d+(?:ns|us|µs|ms|s|m|h))+))$`
+	frequencyRegex := regexp.MustCompile(cronPredefinedScheduleRegex)
 	return frequencyRegex.MatchString(value)
 }
 
 func (schedule CronSchedule) isValid() bool {
-	scheduleRe := regexp.MustCompile(cronScheduleFrequencyRegex)
+	scheduleRe := regexp.MustCompile(cronScheduleRegex)
 	return scheduleRe.MatchString(string(schedule))
 }
 
