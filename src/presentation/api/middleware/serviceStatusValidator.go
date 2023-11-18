97da676c8f10e9ac56ad54d23804e707591c3336
ntorga <northontorga@gmail.com>
2023-11-18 12:15:56 -0300

    fix: move check services to helper pkg

--- src/presentation/api/middleware/serviceStatusValidator.go
+++ src/presentation/api/middleware/serviceStatusValidator.go
@@ -4,13 +4,13 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
-	"github.com/speedianet/os/src/presentation/shared"
+	sharedHelper "github.com/speedianet/os/src/presentation/shared/helper"
 )
 
 func ServiceStatusValidator(serviceNameStr string) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
-			err := shared.CheckServices(serviceNameStr)
+			err := sharedHelper.CheckServices(serviceNameStr)
 			if err != nil {
 				return echo.NewHTTPError(http.StatusBadRequest, map[string]interface{}{
 					"status": http.StatusBadRequest,
