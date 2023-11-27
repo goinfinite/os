cb3278c6fd0268812fe9e9ae231e5ad4b0c887ca
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2023-11-27 16:16:42 -0300

    feat: add UnixFileExtension VO and test

---
+++ src/domain/valueObject/unixFileExtension.go
@@ -0,0 +1,44 @@
+package valueObject
+
+import (
+	"errors"
+	"regexp"
+	"strings"
+)
+
+const unixFileExtensionRegexExpression = `^[\w\_\-]{1,15}$`
+
+type UnixFileExtension string
+
+func NewUnixFileExtension(value string) (UnixFileExtension, error) {
+	if strings.HasPrefix(value, ".") {
+		value, _ = strings.CutPrefix(value, ".")
+	}
+
+	unixFileExtension := UnixFileExtension(value)
+	if !unixFileExtension.isValid() {
+		return "", errors.New("InvalidUnixFileExtension")
+	}
+	return unixFileExtension, nil
+}
+
+func NewUnixFileExtensionPanic(value string) UnixFileExtension {
+	unixFileExtension, err := NewUnixFileExtension(value)
+	if err != nil {
+		panic(err)
+	}
+	return unixFileExtension
+}
+
+func (unixFileExtension UnixFileExtension) isValid() bool {
+	unixFileExtensionRegexRegex := regexp.MustCompile(unixFileExtensionRegexExpression)
+	isValidFormat := unixFileExtensionRegexRegex.MatchString(string(unixFileExtension))
+
+	isEmpty := string(unixFileExtension) == ""
+
+	return isValidFormat && !isEmpty
+}
+
+func (unixFileExtension UnixFileExtension) String() string {
+	return string(unixFileExtension)
+}
---
+++ src/domain/valueObject/unixFileExtension_test.go
@@ -0,0 +1,41 @@
+package valueObject
+
+import "testing"
+
+func TestUnixFileExtension(t *testing.T) {
+	t.Run("ValidUnixFileExtension", func(t *testing.T) {
+		validUnixFileExtensions := []string{
+			".png",
+			"png",
+			".c",
+			"c",
+			".ecelp4800",
+			".n-gage",
+			".application",
+			".fe_launch",
+			".cdbcmsg",
+		}
+		for _, extension := range validUnixFileExtensions {
+			_, err := NewUnixFileExtension(extension)
+			if err != nil {
+				t.Errorf("Expected no error for %s, got %v", extension, err)
+			}
+		}
+	})
+
+	t.Run("InvalidUnixFileExtension", func(t *testing.T) {
+		invalidUnixFileExtensions := []string{
+			"",
+			"file.php?blabla",
+			"@<php52.sandbox.ntorga.com>.php",
+			"../file.php",
+			"hello10/info.php",
+		}
+		for _, extension := range invalidUnixFileExtensions {
+			_, err := NewUnixFileExtension(extension)
+			if err == nil {
+				t.Errorf("Expected error for %s, got nil", extension)
+			}
+		}
+	})
+}
