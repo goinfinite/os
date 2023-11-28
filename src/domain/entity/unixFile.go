2f39ecdca4103480c73a46ffc130953218bfa2e7
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2023-11-28 12:37:53 -0300

    feat: add UnixFile entity

---
+++ src/domain/entity/unixFile.go
@@ -0,0 +1,45 @@
+package entity
+
+import "github.com/speedianet/os/src/domain/valueObject"
+
+type UnixFile struct {
+	Uid         valueObject.UnixUid             `json:"uid"`
+	Gid         valueObject.UnixGid             `json:"gid"`
+	MimeType    valueObject.MimeType            `json:"mimeType"`
+	Name        valueObject.UnixFileName        `json:"name"`
+	Path        valueObject.UnixFilePath        `json:"path"`
+	Extension   valueObject.UnixFileExtension   `json:"extension"`
+	Permissions valueObject.UnixFilePermissions `json:"permissions"`
+	Size        valueObject.Byte                `json:"size"`
+	UpdatedAt   valueObject.UnixTime            `json:"updatedAt"`
+	Owner       valueObject.Username            `json:"owner"`
+	Group       valueObject.GroupId             `json:"group"`
+}
+
+func NewUnixFile(
+	Uid valueObject.UnixUid,
+	Gid valueObject.UnixGid,
+	MimeType valueObject.MimeType,
+	Name valueObject.UnixFileName,
+	Path valueObject.UnixFilePath,
+	Extension valueObject.UnixFileExtension,
+	Permissions valueObject.UnixFilePermissions,
+	Size valueObject.Byte,
+	UpdatedAt valueObject.UnixTime,
+	Owner valueObject.Username,
+	Group valueObject.GroupId,
+) UnixFile {
+	return UnixFile{
+		Uid:         Uid,
+		Gid:         Gid,
+		MimeType:    MimeType,
+		Name:        Name,
+		Path:        Path,
+		Extension:   Extension,
+		Permissions: Permissions,
+		Size:        Size,
+		UpdatedAt:   UpdatedAt,
+		Owner:       Owner,
+		Group:       Group,
+	}
+}
