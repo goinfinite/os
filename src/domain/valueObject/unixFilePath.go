4040fa77d819961e5c13ce97b89b56a98de10a20
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2023-11-28 16:39:28 -0300

    feat: enhance UnixFilePath VO

--- src/domain/valueObject/unixFilePath.go
+++ src/domain/valueObject/unixFilePath.go
@@ -2,18 +2,32 @@ package valueObject
 
 import (
 	"errors"
+	"path/filepath"
 	"regexp"
+	"strings"
 )
 
-const unixFilePathRegexExpression = `^\/(?:[\w\p{Latin}\. \-]+\/)*[\w\p{Latin}\. \-]+$`
+const unixFilePathRegexExpression = `^\/?[^\n\r\t\f\0\?\[\]\<\>]+$`
+const unixFileRelativePathRegexExpression = `(\.\.\/)|^\.\/|^\/\.\/`
 
 type UnixFilePath string
 
 func NewUnixFilePath(value string) (UnixFilePath, error) {
+	isFileName := len(value) > 0 && !strings.HasSuffix(value, "/")
+	if isFileName {
+		value = "/" + value
+	}
+
 	unixFilePath := UnixFilePath(value)
+
 	if !unixFilePath.isValid() {
 		return "", errors.New("InvalidUnixFilePath")
 	}
+
+	if unixFilePath.isRelative() {
+		return "", errors.New("RelativePathNotAllowed")
+	}
+
 	return unixFilePath, nil
 }
 
@@ -28,6 +42,33 @@ func NewUnixFilePathPanic(value string) UnixFilePath {
 func (unixFilePath UnixFilePath) isValid() bool {
 	unixFilePathRegex := regexp.MustCompile(unixFilePathRegexExpression)
 	return unixFilePathRegex.MatchString(string(unixFilePath))
+}
+
+func (unixFilePath UnixFilePath) isRelative() bool {
+	unixFileRelativePathRegex := regexp.MustCompile(unixFileRelativePathRegexExpression)
+	return unixFileRelativePathRegex.MatchString(string(unixFilePath))
+}
+
+func (unixFilePath UnixFilePath) GetFileName() (UnixFileName, error) {
+	var unixFileName UnixFileName
+
+	isDir := strings.HasSuffix(string(unixFilePath), "/")
+	if isDir {
+		return unixFileName, errors.New("UnableToGetFileName")
+	}
+
+	unixFileBase := filepath.Base(string(unixFilePath))
+	unixFileName, err := NewUnixFileName(unixFileBase)
+
+	return unixFileName, err
+}
+
+func (unixFilePath UnixFilePath) GetFileExtension() (UnixFileExtension, error) {
+	return NewUnixFileExtension(filepath.Ext(string(unixFilePath)))
+}
+
+func (unixFilePath UnixFilePath) GetFileDir() (UnixFilePath, error) {
+	return NewUnixFilePath(filepath.Dir(string(unixFilePath)))
 }
 
 func (unixFilePath UnixFilePath) String() string {
--- src/domain/valueObject/unixFilePath_test.go
+++ src/domain/valueObject/unixFilePath_test.go
@@ -5,15 +5,40 @@ import "testing"
 func TestUnixFilePath(t *testing.T) {
 	t.Run("ValidUnixFilePath", func(t *testing.T) {
 		validUnixFilePaths := []string{
-			"/speedia/ssl_crt.pem",
-			"/speedia/ssl_key.pem",
-			"/usr/local/test.sh",
-			"/speedia/ssl_crt",
+			"/",
+			"/root",
+			"/root/",
+			"/home/sandbox/file.php",
+			"/home/sandbox/file",
+			"/home/sandbox/file with space.php",
+			"/home/100sandbox/file.php",
+			"/home/100sandbox/Imagem - Sem Título.jpg",
+			"/home/100sandbox/Imagem - Sem Título & BW.jpg",
+			"/home/100sandbox/Imagem - Sem Título # BW.jpg",
+			"/home/@directory/file.gif",
+			"file.php",
 		}
-		for _, name := range validUnixFilePaths {
-			_, err := NewUnixFilePath(name)
+		for _, filePath := range validUnixFilePaths {
+			_, err := NewUnixFilePath(filePath)
 			if err != nil {
-				t.Errorf("Expected no error for %s, got %v", name, err)
+				t.Errorf("Expected no error for %s, got %v", filePath, err)
+			}
+		}
+	})
+
+	t.Run("ValidUnixFilePath", func(t *testing.T) {
+		invalidUnixFilePaths := []string{
+			"",
+			"/home/user/file.php?blabla",
+			"/home/sandbox/domains/@<php52.sandbox.ntorga.com>",
+			"../file.php",
+			"./file.php",
+			"/home/../file.php",
+		}
+		for _, filePath := range invalidUnixFilePaths {
+			_, err := NewUnixFilePath(filePath)
+			if err == nil {
+				t.Errorf("Expected error for %s, got nil", filePath)
 			}
 		}
 	})
