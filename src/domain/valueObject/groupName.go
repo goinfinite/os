edfe4186246e9971c7be46d524ae34ede8ab39c1
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2023-11-28 17:55:19 -0300

    feat: add GroupName VO and test

---
+++ src/domain/valueObject/groupName.go
@@ -0,0 +1,49 @@
+package valueObject
+
+import (
+	"errors"
+	"regexp"
+)
+
+const groupNameRegexExpression = `^[a-zA-Z0-9_]*$`
+
+type GroupName string
+
+func NewGroupName(value string) (GroupName, error) {
+	if len(value) < 1 {
+		return "", errors.New("InvalidGroupName")
+	}
+
+	groupName := GroupName(value)
+
+	if groupName.isGroupTooBig() {
+		return "", errors.New("InvalidGroupName")
+	}
+
+	if !groupName.isGroupAlphaNumericOnly() {
+		return "", errors.New("InvalidGroupName")
+	}
+
+	return groupName, nil
+}
+
+func NewGroupNamePanic(value string) GroupName {
+	groupName, err := NewGroupName(value)
+	if err != nil {
+		panic(err)
+	}
+	return groupName
+}
+
+func (groupName GroupName) isGroupTooBig() bool {
+	return len(groupName) > 96
+}
+
+func (groupName GroupName) isGroupAlphaNumericOnly() bool {
+	groupNameRegex := regexp.MustCompile(groupNameRegexExpression)
+	return groupNameRegex.MatchString(string(groupName))
+}
+
+func (groupName GroupName) String() string {
+	return string(groupName)
+}
