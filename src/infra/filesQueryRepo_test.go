ef02890377b350ac1831e0e37c440a4de407497f
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2023-12-05 15:10:11 -0300

    feat: add invalid call as a test case in FilesQueryRepo test

--- src/infra/filesQueryRepo_test.go
+++ src/infra/filesQueryRepo_test.go
@@ -8,6 +8,7 @@ import (
 
 func TestFilesQueryRepo(t *testing.T) {
 	unixFilePath, _ := valueObject.NewUnixFilePath("/etc/timezone")
+	invalidUnixPath, _ := valueObject.NewUnixFilePath("/aaa/bbb/ccc")
 
 	filesQueryRepo := FilesQueryRepo{}
 
@@ -15,6 +16,13 @@ func TestFilesQueryRepo(t *testing.T) {
 		_, err := filesQueryRepo.Get(unixFilePath)
 		if err != nil {
 			t.Errorf("FilesQueryRepo.Get() should not return: %s", err)
+		}
+	})
+
+	t.Run("InvalidGet", func(t *testing.T) {
+		_, err := filesQueryRepo.Get(invalidUnixPath)
+		if err == nil {
+			t.Errorf("FilesQueryRepo.Get() should throw an error")
 		}
 	})
 
