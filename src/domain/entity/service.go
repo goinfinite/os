c7f1c8dd94f3df1f8a536709294c63aa883ecbe5
ntorga <northontorga@gmail.com>
2023-12-05 17:25:12 -0300

    feat: add version to svc entity

--- src/domain/entity/service.go
+++ src/domain/entity/service.go
@@ -3,20 +3,22 @@ package entity
 import "github.com/speedianet/os/src/domain/valueObject"
 
 type Service struct {
-	Name            valueObject.ServiceName   `json:"name"`
-	Type            valueObject.ServiceType   `json:"type"`
-	Status          valueObject.ServiceStatus `json:"status"`
-	Command         *valueObject.UnixCommand  `json:"command,omitempty"`
-	Ports           []valueObject.NetworkPort `json:"ports,omitempty"`
-	Pids            []uint32                  `json:"pids,omitempty"`
-	UptimeSecs      *int64                    `json:"uptimeSecs,omitempty"`
-	CpuUsagePercent *float64                  `json:"cpuUsagePercent,omitempty"`
-	MemUsagePercent *float32                  `json:"memUsagePercent,omitempty"`
+	Name            valueObject.ServiceName    `json:"name"`
+	Type            valueObject.ServiceType    `json:"type"`
+	Version         valueObject.ServiceVersion `json:"version"`
+	Status          valueObject.ServiceStatus  `json:"status"`
+	Command         *valueObject.UnixCommand   `json:"command,omitempty"`
+	Ports           []valueObject.NetworkPort  `json:"ports,omitempty"`
+	Pids            []uint32                   `json:"pids,omitempty"`
+	UptimeSecs      *int64                     `json:"uptimeSecs,omitempty"`
+	CpuUsagePercent *float64                   `json:"cpuUsagePercent,omitempty"`
+	MemUsagePercent *float32                   `json:"memUsagePercent,omitempty"`
 }
 
 func NewService(
 	name valueObject.ServiceName,
 	svcType valueObject.ServiceType,
+	version valueObject.ServiceVersion,
 	status valueObject.ServiceStatus,
 	command *valueObject.UnixCommand,
 	ports []valueObject.NetworkPort,
@@ -28,6 +30,7 @@ func NewService(
 	return Service{
 		Name:            name,
 		Type:            svcType,
+		Version:         version,
 		Status:          status,
 		Command:         command,
 		Ports:           ports,
