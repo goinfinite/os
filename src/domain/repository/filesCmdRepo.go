0895749d2f500528ad5ac06fff80c59bc2ff650d
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2023-12-06 12:02:08 -0300

    feat: add UpdatePermissions method to FilesCmdRepo and add dir creation to Add method

--- src/domain/repository/filesCmdRepo.go
+++ src/domain/repository/filesCmdRepo.go
@@ -1,7 +1,11 @@
 package repository
 
-import "github.com/speedianet/os/src/domain/dto"
+import (
+	"github.com/speedianet/os/src/domain/dto"
+	"github.com/speedianet/os/src/domain/valueObject"
+)
 
 type FilesCmdRepo interface {
 	Add(dto.AddUnixFile) error
+	UpdatePermissions(valueObject.UnixFilePath, valueObject.UnixFilePermissions) error
 }
--- src/infra/filesCmdRepo.go
+++ src/infra/filesCmdRepo.go
@@ -6,21 +6,37 @@ import (
 	"os"
 
 	"github.com/speedianet/os/src/domain/dto"
+	"github.com/speedianet/os/src/domain/valueObject"
 )
 
 type FilesCmdRepo struct {
 }
 
 func (repo FilesCmdRepo) Add(addUnixFile dto.AddUnixFile) error {
-	filePathStr := addUnixFile.Path.String()
+	if !addUnixFile.Type.IsDir() {
+		_, err := os.Create(addUnixFile.Path.String())
+		if err != nil {
+			log.Printf("CreateUnixFileError: %s", err)
+			return errors.New("CreateUnixFileError")
+		}
 
-	_, err := os.Create(filePathStr)
-	if err != nil {
-		log.Printf("CreateUnixFileError: %s", err)
-		return errors.New("CreateUnixFileError")
+		return repo.UpdatePermissions(addUnixFile.Path, addUnixFile.Permissions)
 	}
 
-	err = os.Chmod(filePathStr, addUnixFile.Permissions.GetFileMode())
+	err := os.MkdirAll(addUnixFile.Path.String(), addUnixFile.Permissions.GetFileMode())
+	if err != nil {
+		log.Printf("CreateUnixDirectoryError: %s", err)
+		return errors.New("CreateUnixDirectoryError")
+	}
+
+	return nil
+}
+
+func (repo FilesCmdRepo) UpdatePermissions(
+	unixFilePath valueObject.UnixFilePath,
+	unixFilePermissions valueObject.UnixFilePermissions,
+) error {
+	err := os.Chmod(unixFilePath.String(), unixFilePermissions.GetFileMode())
 	if err != nil {
 		log.Printf("ChmodUnixFileError: %s", err)
 		return errors.New("ChmodUnixFileError")
