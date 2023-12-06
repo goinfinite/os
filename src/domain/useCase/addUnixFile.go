27b5cbac830b260818755b4964113559cb1063c1
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2023-12-06 13:26:19 -0300

    feat: add custom log to Files and Directory in AddUnixFile use case

--- src/domain/useCase/addUnixFile.go
+++ src/domain/useCase/addUnixFile.go
@@ -13,22 +13,24 @@ func AddUnixFile(
 	filesCmdRepo repository.FilesCmdRepo,
 	addUnixFile dto.AddUnixFile,
 ) error {
-	unixFiles, err := filesQueryRepo.Get(addUnixFile.Path)
-	if err != nil {
-		return err
+	unixFiles, _ := filesQueryRepo.Get(addUnixFile.Path)
+
+	inodeType := "File"
+	if addUnixFile.Type.IsDir() {
+		inodeType = "Directory"
 	}
 
 	if len(unixFiles) > 0 {
-		return errors.New("FileAlreadyExists")
+		return errors.New(inodeType + "AlreadyExists")
 	}
 
-	err = filesCmdRepo.Add(addUnixFile)
+	err := filesCmdRepo.Add(addUnixFile)
 	if err != nil {
-		return errors.New("AddFileError")
+		return errors.New("Add" + inodeType + "Error")
 	}
 
 	fileName, _ := addUnixFile.Path.GetFileName()
-	log.Printf("File '%s' added to '%s'.", fileName.String(), addUnixFile.Path)
+	log.Printf("%s '%s' added to '%s'.", inodeType, fileName.String(), addUnixFile.Path)
 
 	return nil
 }
