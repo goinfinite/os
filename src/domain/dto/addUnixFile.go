a00a704eaf1faf06281cf09da082c79defc3b455
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2023-12-06 11:37:52 -0300

    refact: reduce AddUnixFile DTO props to just only Path, Permissions and Type (directory or file)

--- src/domain/dto/addUnixFile.go
+++ src/domain/dto/addUnixFile.go
@@ -3,22 +3,19 @@ package dto
 import "github.com/speedianet/os/src/domain/valueObject"
 
 type AddUnixFile struct {
-	MimeType    valueObject.MimeType            `json:"mimeType"`
-	Name        valueObject.UnixFileName        `json:"name"`
 	Path        valueObject.UnixFilePath        `json:"path"`
 	Permissions valueObject.UnixFilePermissions `json:"permissions"`
+	Type        valueObject.UnixFileType        `json:"type"`
 }
 
 func NewAddUnixFile(
-	MimeType valueObject.MimeType,
-	Name valueObject.UnixFileName,
 	Path valueObject.UnixFilePath,
 	Permissions valueObject.UnixFilePermissions,
+	Type valueObject.UnixFileType,
 ) AddUnixFile {
 	return AddUnixFile{
-		MimeType:    MimeType,
-		Name:        Name,
 		Path:        Path,
 		Permissions: Permissions,
+		Type:        Type,
 	}
 }
