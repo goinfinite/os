ed88609c70e721d909ed19456750028c06c18201
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2023-12-07 13:53:38 -0300

    feat: add IsDir method to UnixFilePath VO

--- src/domain/valueObject/unixFilePath.go
+++ src/domain/valueObject/unixFilePath.go
@@ -2,6 +2,7 @@ package valueObject
 
 import (
 	"errors"
+	"os"
 	"path/filepath"
 	"regexp"
 	"strings"
@@ -46,6 +47,15 @@ func (unixFilePath UnixFilePath) isRelative() bool {
 
 	unixFileRelativePathRegex := regexp.MustCompile(unixFileRelativePathRegexExpression)
 	return isOnlyFileName || unixFileRelativePathRegex.MatchString(unixFilePathStr)
+}
+
+func (unixFilePath UnixFilePath) IsDir() (bool, error) {
+	pathInfo, err := os.Stat(string(unixFilePath))
+	if err != nil {
+		return false, err
+	}
+
+	return pathInfo.IsDir(), nil
 }
 
 func (unixFilePath UnixFilePath) GetFileName() (UnixFileName, error) {
