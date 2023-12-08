b3e446d79f5051b6c42b600f10353fd48377e708
ntorga <northontorga@gmail.com>
2023-12-08 15:35:15 -0300

    feat: add delete svc use case

---
+++ src/domain/useCase/deleteService.go
@@ -0,0 +1,30 @@
+package useCase
+
+import (
+	"errors"
+	"log"
+
+	"github.com/speedianet/os/src/domain/repository"
+	"github.com/speedianet/os/src/domain/valueObject"
+)
+
+func DeleteService(
+	queryRepo repository.ServicesQueryRepo,
+	cmdRepo repository.ServicesCmdRepo,
+	svcName valueObject.ServiceName,
+) error {
+	_, err := queryRepo.GetByName(svcName)
+	if err != nil {
+		return errors.New("ServiceNotFound")
+	}
+
+	err = cmdRepo.Uninstall(svcName)
+	if err != nil {
+		log.Printf("DeleteServiceError: %v", err)
+		return errors.New("DeleteServiceInfraError")
+	}
+
+	log.Printf("Service '%v' deleted.", svcName.String())
+
+	return nil
+}
