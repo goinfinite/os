9a80518a549514f39b32c6ae69819c44bc7830b4
ntorga <northontorga@gmail.com>
2023-12-08 15:17:55 -0300

    fix: prevent solo svc version update

--- src/domain/useCase/updateService.go
+++ src/domain/useCase/updateService.go
@@ -4,29 +4,25 @@ import (
 	"errors"
 
 	"github.com/speedianet/os/src/domain/dto"
+	"github.com/speedianet/os/src/domain/entity"
 	"github.com/speedianet/os/src/domain/repository"
 )
 
 func updateServiceStatus(
-	servicesQueryRepo repository.ServicesQueryRepo,
 	servicesCmdRepo repository.ServicesCmdRepo,
+	serviceEntity entity.Service,
 	updateDto dto.UpdateService,
 ) error {
-	currentSvcStatus, err := servicesQueryRepo.GetByName(updateDto.Name)
-	if err != nil {
-		return err
-	}
-
-	if currentSvcStatus.Status.String() == updateDto.Status.String() {
+	if serviceEntity.Status.String() == updateDto.Status.String() {
 		return errors.New("ServiceStatusAlreadySet")
 	}
 
-	isInstalled := currentSvcStatus.Status.String() != "uninstalled"
+	isInstalled := serviceEntity.Status.String() != "uninstalled"
 	if !isInstalled {
 		return errors.New("ServiceNotInstalled")
 	}
 
-	isSystemService := currentSvcStatus.Type.String() == "system"
+	isSystemService := serviceEntity.Type.String() == "system"
 	shouldUninstall := updateDto.Status.String() == "uninstalled"
 	if isSystemService && shouldUninstall {
 		return errors.New("SystemServicesCannotBeUninstalled")
@@ -49,16 +45,26 @@ func UpdateService(
 	servicesCmdRepo repository.ServicesCmdRepo,
 	updateDto dto.UpdateService,
 ) error {
-	var err error
+	serviceEntity, err := servicesQueryRepo.GetByName(updateDto.Name)
+	if err != nil {
+		return err
+	}
+
 	if updateDto.Status != nil {
 		err = updateServiceStatus(
-			servicesQueryRepo,
 			servicesCmdRepo,
+			serviceEntity,
 			updateDto,
 		)
 		if err != nil {
 			return err
 		}
+	}
+
+	isSoloService := serviceEntity.Type.String() == "solo"
+	shouldUpdateVersion := updateDto.Version != nil
+	if isSoloService && shouldUpdateVersion {
+		return errors.New("SoloServicesVersionCannotBeChanged")
 	}
 
 	return servicesCmdRepo.Update(updateDto)
