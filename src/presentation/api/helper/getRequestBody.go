2914f3f93b4e8e9d068e4c11d904b8b5b472f29a
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2023-12-12 14:29:42 -0300

    feat: add support to multipart/form-data in OS API

--- src/presentation/api/helper/getRequestBody.go
+++ src/presentation/api/helper/getRequestBody.go
@@ -1,6 +1,7 @@
 package apiHelper
 
 import (
+	"mime/multipart"
 	"net/http"
 	"strings"
 
@@ -17,7 +18,7 @@ func GetRequestBody(c echo.Context) (map[string]interface{}, error) {
 		if err := c.Bind(&requestData); err != nil {
 			return nil, echo.NewHTTPError(http.StatusBadRequest, "InvalidJsonBody")
 		}
-	case strings.HasPrefix(contentType, "application/x-www-form-urlencoded"), strings.HasPrefix(contentType, "multipart/form-data"):
+	case strings.HasPrefix(contentType, "application/x-www-form-urlencoded"):
 		formData, err := c.FormParams()
 		if err != nil {
 			return nil, echo.NewHTTPError(http.StatusBadRequest, "InvalidFormData")
@@ -26,6 +27,29 @@ func GetRequestBody(c echo.Context) (map[string]interface{}, error) {
 			if len(v) > 0 {
 				requestData[k] = v[0]
 			}
+		}
+	case strings.HasPrefix(contentType, "multipart/form-data"):
+		multipartFormData, err := c.MultipartForm()
+		if err != nil {
+			return nil, echo.NewHTTPError(http.StatusBadRequest, "InvalidMultipartFormData")
+		}
+
+		for valueK, valueV := range multipartFormData.Value {
+			if valueV != nil && len(valueV) > 0 {
+				requestData[valueK] = valueV[0]
+			}
+		}
+
+		if len(multipartFormData.File) > 0 {
+			requestDataFiles := map[string]*multipart.FileHeader{}
+
+			for fileK, fileV := range multipartFormData.File {
+				if fileV != nil && len(fileV) > 0 {
+					requestDataFiles[fileK] = fileV[0]
+				}
+			}
+
+			requestData["files"] = requestDataFiles
 		}
 	default:
 		return nil, echo.NewHTTPError(http.StatusBadRequest, "InvalidContentType")
