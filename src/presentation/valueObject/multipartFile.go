5095101549768e4b4148c8481d19059ae9b78290
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2023-12-12 14:23:18 -0300

    feat: add MultipartFile presentation VO

---
+++ src/presentation/valueObject/multipartFile.go
@@ -0,0 +1,18 @@
+package presentationValueObject
+
+import "mime/multipart"
+
+type MultipartFile struct {
+	CustomName          string
+	MultipartFileHeader *multipart.FileHeader
+}
+
+func NewMultipartFile(
+	customName string,
+	multipartFileHeader *multipart.FileHeader,
+) MultipartFile {
+	return MultipartFile{
+		CustomName:          customName,
+		MultipartFileHeader: multipartFileHeader,
+	}
+}
