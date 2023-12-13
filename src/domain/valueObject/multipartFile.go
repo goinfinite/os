659f0bb0fdd0e640178ad97285b8c4248106c1b1
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2023-12-13 15:58:03 -0300

    refact: change MultipartFile VO accept values and validations

--- src/domain/valueObject/multipartFile.go
+++ src/domain/valueObject/multipartFile.go
@@ -1,18 +1,51 @@
 package valueObject
 
-import "mime/multipart"
+import (
+	"errors"
+	"mime/multipart"
+)
 
 type MultipartFile struct {
-	CustomName          string
-	MultipartFileHeader *multipart.FileHeader
+	name UnixFileName
+	size Byte
+	Open func() (multipart.File, error)
 }
 
-func NewMultipartFile(
-	customName string,
-	multipartFileHeader *multipart.FileHeader,
-) MultipartFile {
-	return MultipartFile{
-		CustomName:          customName,
-		MultipartFileHeader: multipartFileHeader,
+func NewMultipartFile(value *multipart.FileHeader) (MultipartFile, error) {
+	fileName, err := NewUnixFileName(value.Filename)
+	if err != nil {
+		return MultipartFile{}, errors.New("InvalidFileName")
 	}
+
+	fileSize := Byte(value.Size)
+	if isTooBig(fileSize) {
+		return MultipartFile{}, errors.New("FileIsTooBig")
+	}
+
+	return MultipartFile{
+		name: fileName,
+		size: fileSize,
+		Open: value.Open,
+	}, nil
+}
+
+func NewMultipartFilePanic(value *multipart.FileHeader) MultipartFile {
+	multipartFile, err := NewMultipartFile(value)
+	if err != nil {
+		panic(err)
+	}
+
+	return multipartFile
+}
+
+func isTooBig(fileSize Byte) bool {
+	return fileSize.ToGiB() > 5
+}
+
+func (multipartFile MultipartFile) GetFileName() UnixFileName {
+	return multipartFile.name
+}
+
+func (multipartFile MultipartFile) GetFileSize() Byte {
+	return multipartFile.size
 }
