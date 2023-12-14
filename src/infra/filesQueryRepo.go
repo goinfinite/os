4e9296ebca9708d3c4b1a9fa5d532ce2e901171c
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2023-12-14 17:07:38 -0300

    feat: add UnixFileFactory to infra/factory and implements as a call in FilesQueryRepo

--- src/infra/filesQueryRepo.go
+++ src/infra/filesQueryRepo.go
@@ -2,131 +2,15 @@ package infra
 
 import (
 	"errors"
-	"fmt"
-	"io/fs"
 	"log"
-	"mime"
 	"os"
-	"os/user"
-	"path/filepath"
-	"strings"
-	"syscall"
 
 	"github.com/speedianet/os/src/domain/entity"
 	"github.com/speedianet/os/src/domain/valueObject"
+	infraFactory "github.com/speedianet/os/src/infra/factory"
 )
 
 type FilesQueryRepo struct{}
-
-func (repo FilesQueryRepo) unixFileFactory(
-	filePath valueObject.UnixFilePath,
-	fileInfo fs.FileInfo,
-) (entity.UnixFile, error) {
-	var unixFile entity.UnixFile
-
-	fileSysInfo := fileInfo.Sys().(*syscall.Stat_t)
-
-	fileUidStr := fmt.Sprint(fileSysInfo.Uid)
-	unixFileUid, err := valueObject.NewUnixUid(fileSysInfo.Uid)
-	if err != nil {
-		return unixFile, err
-	}
-
-	fileOwner, err := user.LookupId(fileUidStr)
-	if err != nil {
-		log.Printf("UnableToGetFileGroupName: %s", err)
-		return unixFile, errors.New("UnableToGetFileGroupName")
-	}
-
-	unixFileUsername, err := valueObject.NewUsername(fileOwner.Username)
-	if err != nil {
-		return unixFile, err
-	}
-
-	fileGidStr := fmt.Sprint(fileSysInfo.Gid)
-	unixFileGid, err := valueObject.NewGroupId(fileSysInfo.Gid)
-	if err != nil {
-		return unixFile, err
-	}
-
-	fileGroupName, err := user.LookupGroupId(fileGidStr)
-	if err != nil {
-		log.Printf("UnableToGetFileGroupName: %s", err)
-		return unixFile, errors.New("UnableToGetFileGroupName")
-	}
-
-	unixFileGroup, err := valueObject.NewGroupName(fileGroupName.Name)
-	if err != nil {
-		return unixFile, err
-	}
-
-	fileAbsPathStr := filePath.String() + "/" + fileInfo.Name()
-	unixFileAbsPath, err := filepath.Abs(fileAbsPathStr)
-	if err != nil {
-		log.Printf("UnableToGetFileAbsolutePath: %s", err)
-		return unixFile, errors.New("UnableToGetFileAbsolutePath")
-	}
-
-	unixFilePath, err := valueObject.NewUnixFilePath(unixFileAbsPath)
-	if err != nil {
-		return unixFile, err
-	}
-
-	unixFileName, err := unixFilePath.GetFileName()
-	if err != nil {
-		return unixFile, err
-	}
-
-	unixFileExtension, err := unixFilePath.GetFileExtension()
-	if err != nil {
-		return unixFile, err
-	}
-
-	unixFileMimeType, _ := valueObject.NewMimeType("directory")
-
-	if !fileInfo.IsDir() {
-		mimeType := "generic"
-
-		mimeTypeWithCharset := mime.TypeByExtension("." + unixFileExtension.String())
-		if len(mimeTypeWithCharset) > 1 {
-			mimeTypeOnly := strings.Split(mimeTypeWithCharset, ";")[0]
-			mimeType = mimeTypeOnly
-		}
-
-		unixFileMimeType, err = valueObject.NewMimeType(mimeType)
-		if err != nil {
-			log.Print(err)
-			return unixFile, err
-		}
-	}
-
-	filePermissions := fileInfo.Mode().Perm()
-	filePermissionsStr := fmt.Sprintf("%o", filePermissions)
-	unixFilePermissions, err := valueObject.NewUnixFilePermissions(filePermissionsStr)
-	if err != nil {
-		log.Print(err)
-		return unixFile, err
-	}
-
-	unixFileSize := valueObject.Byte(fileInfo.Size())
-	unixFileUpdatedAt := valueObject.UnixTime(fileInfo.ModTime().Unix())
-
-	unixFile = entity.NewUnixFile(
-		unixFileUid,
-		unixFileUsername,
-		unixFileGid,
-		unixFileGroup,
-		unixFileMimeType,
-		unixFileName,
-		unixFilePath,
-		&unixFileExtension,
-		unixFilePermissions,
-		unixFileSize,
-		unixFileUpdatedAt,
-	)
-
-	return unixFile, nil
-}
 
 func (repo FilesQueryRepo) Get(
 	unixFilePath valueObject.UnixFilePath,
@@ -139,7 +23,7 @@ func (repo FilesQueryRepo) Get(
 	}
 
 	if !filePathInfo.IsDir() {
-		unixFile, err := repo.unixFileFactory(unixFilePath, filePathInfo)
+		unixFile, err := infraFactory.UnixFileFactory(unixFilePath, filePathInfo)
 		if err == nil {
 			unixFileList = append(unixFileList, unixFile)
 		}
@@ -155,7 +39,7 @@ func (repo FilesQueryRepo) Get(
 
 	for _, dirEntry := range dirEntriesToAnalyzeList {
 		inodeInfo, _ := dirEntry.Info()
-		unixFile, err := repo.unixFileFactory(unixFilePath, inodeInfo)
+		unixFile, err := infraFactory.UnixFileFactory(unixFilePath, inodeInfo)
 		if err != nil {
 			continue
 		}
