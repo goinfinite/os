6336696241c7ef4b5f79d7ddf1d7fd4fa0ceaab8
ntorga <northontorga@gmail.com>
2023-12-14 13:50:50 -0300

    feat: add delete vhost domain

--- src/domain/repository/virtualHostCmdRepo.go
+++ src/domain/repository/virtualHostCmdRepo.go
@@ -2,8 +2,10 @@ package repository
 
 import (
 	"github.com/speedianet/os/src/domain/dto"
+	"github.com/speedianet/os/src/domain/entity"
 )
 
 type VirtualHostCmdRepo interface {
 	Add(addDto dto.AddVirtualHost) error
+	Delete(vhost entity.VirtualHost) error
 }
---
+++ src/domain/useCase/deleteVirtualHost.go
@@ -0,0 +1,36 @@
+package useCase
+
+import (
+	"errors"
+	"log"
+
+	"github.com/speedianet/os/src/domain/repository"
+	"github.com/speedianet/os/src/domain/valueObject"
+)
+
+func DeleteVirtualHost(
+	queryRepo repository.VirtualHostQueryRepo,
+	cmdRepo repository.VirtualHostCmdRepo,
+	primaryHostname valueObject.Fqdn,
+	hostname valueObject.Fqdn,
+) error {
+	isPrimaryHostname := hostname.String() == primaryHostname.String()
+	if isPrimaryHostname {
+		return errors.New("PrimaryVirtualHostCannotBeDeleted")
+	}
+
+	vhost, err := queryRepo.GetByHostname(hostname)
+	if err != nil {
+		return errors.New("VirtualHostNotFound")
+	}
+
+	err = cmdRepo.Delete(vhost)
+	if err != nil {
+		log.Printf("DeleteVirtualHostError: %v", err)
+		return errors.New("DeleteVirtualHostInfraError")
+	}
+
+	log.Printf("VirtualHost '%v' deleted.", hostname)
+
+	return nil
+}
