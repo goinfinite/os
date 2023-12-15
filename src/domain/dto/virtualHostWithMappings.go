aa4b3fa629a538dd8b97145885b8c076e04e0438
ntorga <northontorga@gmail.com>
2023-12-15 17:13:18 -0300

    fix: transform mapping into entity

--- src/domain/dto/virtualHostWithMappings.go
+++ src/domain/dto/virtualHostWithMappings.go
@@ -2,17 +2,16 @@ package dto
 
 import (
 	"github.com/speedianet/os/src/domain/entity"
-	"github.com/speedianet/os/src/domain/valueObject"
 )
 
 type VirtualHostWithMappings struct {
 	entity.VirtualHost
-	Mappings []valueObject.Mapping `json:"mappings,omitempty"`
+	Mappings []entity.Mapping `json:"mappings,omitempty"`
 }
 
 func NewVirtualHostWithMappings(
 	vhost entity.VirtualHost,
-	mappings []valueObject.Mapping,
+	mappings []entity.Mapping,
 ) VirtualHostWithMappings {
 	return VirtualHostWithMappings{
 		VirtualHost: vhost,
