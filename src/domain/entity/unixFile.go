dd487fbb7626a7ce39726838b87a03b193a5f71a
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2023-12-20 18:15:27 -0300

    fix: change priority order in UnixFile struct and constructor

--- src/domain/entity/unixFile.go
+++ src/domain/entity/unixFile.go
@@ -7,45 +7,45 @@ import (
 )
 
 type UnixFile struct {
+	Name        valueObject.UnixFileName        `json:"name"`
+	Path        valueObject.UnixFilePath        `json:"path"`
+	MimeType    valueObject.MimeType            `json:"mimeType"`
+	Permissions valueObject.UnixFilePermissions `json:"permissions"`
+	Size        valueObject.Byte                `json:"size"`
+	Extension   *valueObject.UnixFileExtension  `json:"extension"`
 	Uid         valueObject.UnixUid             `json:"uid"`
 	Owner       valueObject.Username            `json:"owner"`
 	Gid         valueObject.GroupId             `json:"gid"`
 	Group       valueObject.GroupName           `json:"group"`
-	MimeType    valueObject.MimeType            `json:"mimeType"`
-	Name        valueObject.UnixFileName        `json:"name"`
-	Path        valueObject.UnixFilePath        `json:"path"`
-	Extension   *valueObject.UnixFileExtension  `json:"extension"`
-	Permissions valueObject.UnixFilePermissions `json:"permissions"`
-	Size        valueObject.Byte                `json:"size"`
 	UpdatedAt   valueObject.UnixTime            `json:"updatedAt"`
 	Stream      *os.File                        `json:"-"`
 }
 
 func NewUnixFile(
+	name valueObject.UnixFileName,
+	path valueObject.UnixFilePath,
+	mimeType valueObject.MimeType,
+	permissions valueObject.UnixFilePermissions,
+	size valueObject.Byte,
+	extension *valueObject.UnixFileExtension,
 	uid valueObject.UnixUid,
 	owner valueObject.Username,
 	gid valueObject.GroupId,
 	group valueObject.GroupName,
-	mimeType valueObject.MimeType,
-	name valueObject.UnixFileName,
-	path valueObject.UnixFilePath,
-	extension *valueObject.UnixFileExtension,
-	permissions valueObject.UnixFilePermissions,
-	size valueObject.Byte,
 	updatedAt valueObject.UnixTime,
 	stream *os.File,
 ) UnixFile {
 	return UnixFile{
+		Name:        name,
+		Path:        path,
+		MimeType:    mimeType,
+		Permissions: permissions,
+		Size:        size,
+		Extension:   extension,
 		Uid:         uid,
 		Owner:       owner,
 		Gid:         gid,
 		Group:       group,
-		MimeType:    mimeType,
-		Name:        name,
-		Path:        path,
-		Extension:   extension,
-		Permissions: permissions,
-		Size:        size,
 		UpdatedAt:   updatedAt,
 		Stream:      stream,
 	}
