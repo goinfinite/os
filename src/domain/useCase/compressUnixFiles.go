db8d6ae4d21d0bf1986167eefdb8955da32a5981
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2023-12-21 03:11:01 -0300

    fix: remove all the report logic from CompressUnixFiles use case and leave the repository do the job

--- src/domain/useCase/compressUnixFiles.go
+++ src/domain/useCase/compressUnixFiles.go
@@ -6,107 +6,30 @@ import (
 
 	"github.com/speedianet/os/src/domain/dto"
 	"github.com/speedianet/os/src/domain/repository"
-	"github.com/speedianet/os/src/domain/valueObject"
 )
-
-type CompressionProcessFailure struct {
-	FilePath string `json:"filePath"`
-	Reason   string `json:"reason"`
-}
-
-type CompressionProcessInfo struct {
-	Success     []string                    `json:"success"`
-	Failure     []CompressionProcessFailure `json:"failure"`
-	Destination string                      `json:"destination"`
-}
-
-func compressionProcessFailureFactory(
-	filePath valueObject.UnixFilePath,
-	errMessage string,
-) CompressionProcessFailure {
-	return CompressionProcessFailure{
-		FilePath: filePath.String(),
-		Reason:   errMessage,
-	}
-}
 
 func CompressUnixFiles(
 	filesQueryRepo repository.FilesQueryRepo,
 	filesCmdRepo repository.FilesCmdRepo,
 	compressUnixFiles dto.CompressUnixFiles,
-) (CompressionProcessInfo, error) {
+) (dto.CompressionProcessReport, error) {
 	fileDestinationPath := compressUnixFiles.DestinationPath
 
-	compressionProcessInfo := CompressionProcessInfo{
-		Success:     []string{},
-		Failure:     []CompressionProcessFailure{},
-		Destination: fileDestinationPath.String(),
-	}
-
-	unixFileExists, err := filesQueryRepo.Exists(fileDestinationPath)
+	compressionProcessReport, err := filesCmdRepo.Compress(compressUnixFiles)
 	if err != nil {
-		return compressionProcessInfo, err
+		return compressionProcessReport, err
 	}
 
-	if unixFileExists {
-		return compressionProcessInfo, errors.New("PathAlreadyExists")
-	}
-
-	var filesToCompress []valueObject.UnixFilePath
-	for _, filePath := range compressUnixFiles.Paths {
-		unixDestinationFileExists, err := filesQueryRepo.Exists(filePath)
-		if !unixDestinationFileExists {
-			errMessage := "PathDoesNotExists"
-			if err != nil {
-				errMessage = err.Error()
-			}
-
-			compressionProcessFailure := compressionProcessFailureFactory(filePath, errMessage)
-			compressionProcessInfo.Failure = append(
-				compressionProcessInfo.Failure,
-				compressionProcessFailure,
-			)
-
-			continue
-		}
-
-		filesToCompress = append(filesToCompress, filePath)
-	}
-
-	allPathsFailedInCompression := len(compressionProcessInfo.Failure) == len(compressUnixFiles.Paths)
+	allPathsFailedInCompression := len(compressionProcessReport.Failure) == len(compressUnixFiles.Paths)
 	if allPathsFailedInCompression {
 		log.Printf(
 			"UnableToCompressFilesAndDirectories: File compressed %s wasn't created.",
 			fileDestinationPath,
 		)
-		return compressionProcessInfo, errors.New("UnableToCompressFilesAndDirectories")
-	}
-
-	err = filesCmdRepo.Compress(
-		filesToCompress,
-		fileDestinationPath,
-		compressUnixFiles.CompressionType,
-	)
-	if err != nil {
-		for _, filePath := range filesToCompress {
-			compressionProcessFailure := compressionProcessFailureFactory(filePath, err.Error())
-			compressionProcessInfo.Failure = append(
-				compressionProcessInfo.Failure,
-				compressionProcessFailure,
-			)
-		}
-
-		return compressionProcessInfo, errors.New("UnableToCompressFilesAndDirectories")
-	}
-
-	for _, filePath := range filesToCompress {
-		compressionProcessInfo.Success = append(
-			compressionProcessInfo.Success,
-			filePath.String(),
-		)
+		return compressionProcessReport, errors.New("UnableToCompressFilesAndDirectories")
 	}
 
 	log.Printf("File compressed %s created.", fileDestinationPath)
 
-	return compressionProcessInfo, nil
+	return compressionProcessReport, nil
 }
