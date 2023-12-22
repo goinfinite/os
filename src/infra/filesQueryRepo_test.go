59cbae432ce44779b687b87264c650daa725d3ad
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2023-12-22 18:52:23 -0300

    feat: add GetOnly method to FilesQueryRepo test case

--- src/infra/filesQueryRepo_test.go
+++ src/infra/filesQueryRepo_test.go
@@ -1,16 +1,22 @@
 package infra
 
 import (
+	"fmt"
+	"os/user"
 	"testing"
 
 	"github.com/speedianet/os/src/domain/valueObject"
 )
 
 func TestFilesQueryRepo(t *testing.T) {
-	unixFilePath, _ := valueObject.NewUnixFilePath("/etc/timezone")
-	invalidUnixPath, _ := valueObject.NewUnixFilePath("/aaa/bbb/ccc")
-
 	filesQueryRepo := FilesQueryRepo{}
+
+	currentUser, _ := user.Current()
+	fileBasePathStr := fmt.Sprintf("/home/%s", currentUser.Username)
+
+	unixFilePath, _ := valueObject.NewUnixFilePath(fileBasePathStr + "/.gitconfig")
+	invalidUnixPath, _ := valueObject.NewUnixFilePath("/aaa/bbb/ccc")
+	unixDirPath, _ := valueObject.NewUnixFilePath(fileBasePathStr + "/Downloads")
 
 	t.Run("Get", func(t *testing.T) {
 		_, err := filesQueryRepo.Get(unixFilePath)
@@ -27,10 +33,16 @@ func TestFilesQueryRepo(t *testing.T) {
 	})
 
 	t.Run("Get (many)", func(t *testing.T) {
-		unixFilePath, _ := valueObject.NewUnixFilePath("/etc/ssl")
-		_, err := filesQueryRepo.Get(unixFilePath)
+		_, err := filesQueryRepo.Get(unixDirPath)
 		if err != nil {
 			t.Errorf("FilesQueryRepo.Get() should not return: %s", err)
+		}
+	})
+
+	t.Run("GetOnly", func(t *testing.T) {
+		_, err := filesQueryRepo.GetOnly(unixDirPath)
+		if err != nil {
+			t.Errorf("FilesQueryRepo.GetOnly() should not return: %s", err)
 		}
 	})
 }
