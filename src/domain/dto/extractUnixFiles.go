a8d8e676d55ee2444400442dfad0fde3802799f1
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2023-12-26 12:33:31 -0300

    fix: change Path attribute name to SourcePath in ExtractUnixFiles DTO

--- src/domain/dto/extractUnixFiles.go
+++ src/domain/dto/extractUnixFiles.go
@@ -3,16 +3,16 @@ package dto
 import "github.com/speedianet/os/src/domain/valueObject"
 
 type ExtractUnixFiles struct {
-	Path            valueObject.UnixFilePath `json:"path"`
+	SourcePath      valueObject.UnixFilePath `json:"sourcePath"`
 	DestinationPath valueObject.UnixFilePath `json:"destinationPath"`
 }
 
 func NewExtractUnixFiles(
-	path valueObject.UnixFilePath,
+	sourcePath valueObject.UnixFilePath,
 	destinationPath valueObject.UnixFilePath,
 ) ExtractUnixFiles {
 	return ExtractUnixFiles{
-		Path:            path,
+		SourcePath:      sourcePath,
 		DestinationPath: destinationPath,
 	}
 }
