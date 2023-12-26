02a77d62f4fa82b942b765a707d57067eeb7c503
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2023-12-26 12:35:26 -0300

    fix: change Path attribute name to SourcePath in UpdateUnixFileContent DTO

--- src/domain/dto/updateUnixFileContent.go
+++ src/domain/dto/updateUnixFileContent.go
@@ -3,16 +3,16 @@ package dto
 import "github.com/speedianet/os/src/domain/valueObject"
 
 type UpdateUnixFileContent struct {
-	Path    valueObject.UnixFilePath   `json:"path"`
-	Content valueObject.EncodedContent `json:"content"`
+	SourcePath valueObject.UnixFilePath   `json:"sourcePath"`
+	Content    valueObject.EncodedContent `json:"content"`
 }
 
 func NewUpdateUnixFileContent(
-	path valueObject.UnixFilePath,
+	sourcePath valueObject.UnixFilePath,
 	content valueObject.EncodedContent,
 ) UpdateUnixFileContent {
 	return UpdateUnixFileContent{
-		Path:    path,
-		Content: content,
+		SourcePath: sourcePath,
+		Content:    content,
 	}
 }
