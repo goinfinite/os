6289c8e05b8ad5c2b3e69c3f8d6fac502d5297dd
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-01-02 11:08:34 -0300

    fix: remove isValid from FileProcessingFailure VO and add substring logic to get only the first 256 chars from the value

--- src/domain/valueObject/fileProcessingFailure.go
+++ src/domain/valueObject/fileProcessingFailure.go
@@ -1,17 +1,13 @@
 package valueObject
 
-import (
-	"errors"
-)
-
 type FileProcessingFailure string
 
 func NewFileProcessingFailure(value string) (FileProcessingFailure, error) {
-	fileProcessingFailure := FileProcessingFailure(value)
-	if !fileProcessingFailure.isValid() {
-		return "", errors.New("InvalidFileProcessingFailure")
-	}
-	return fileProcessingFailure, nil
+	maxProcessingFailureSize := 256
+
+	maxProcessingFailureSizeIndex := maxProcessingFailureSize - 1
+	partialProcessingFailure := value[:maxProcessingFailureSizeIndex]
+	return FileProcessingFailure(partialProcessingFailure), nil
 }
 
 func NewFileProcessingFailurePanic(value string) FileProcessingFailure {
@@ -20,12 +16,6 @@ func NewFileProcessingFailurePanic(value string) FileProcessingFailure {
 		panic(err)
 	}
 	return fileProcessingFailure
-}
-
-func (fileProcessingFailure FileProcessingFailure) isValid() bool {
-	isTooShort := len(string(fileProcessingFailure)) < 2
-	isTooLong := len(string(fileProcessingFailure)) > 512
-	return !isTooShort && !isTooLong
 }
 
 func (fileProcessingFailure FileProcessingFailure) String() string {
