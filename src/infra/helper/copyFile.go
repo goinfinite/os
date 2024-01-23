8980ce7e8ec3c1b55587bcfc9d6ff21616da4ae5
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-01-23 14:03:52 -0300

    refact: improve error messages in CopyFile helper

--- src/infra/helper/copyFile.go
+++ src/infra/helper/copyFile.go
@@ -3,7 +3,6 @@ package infraHelper
 import (
 	"errors"
 	"io"
-	"log"
 	"os"
 )
 
@@ -12,20 +11,19 @@ func CopyFile(srcPath string, dstPath string) error {
 
 	srcFile, err := os.OpenFile(dstPath, fileFlags, 0644)
 	if err != nil {
-		return errors.New("OpenSourceFileError: " + err.Error())
+		return errors.New("CopyFileError: " + err.Error())
 	}
 	defer srcFile.Close()
 
 	dstFile, err := os.OpenFile(dstPath, fileFlags, 0644)
 	if err != nil {
-		return errors.New("OpenDestinationFileError: " + err.Error())
+		return errors.New("CopyFileError: " + err.Error())
 	}
 	defer dstFile.Close()
 
 	_, err = io.Copy(dstFile, srcFile)
 	if err != nil {
-		log.Printf("CopyFileError: %s", err)
-		return errors.New("CopyFileError")
+		return errors.New("CopyFileError: " + err.Error())
 	}
 
 	return nil
