e3ab38cc8a3b8730ff5848fac72423a6b1bcd595
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-01-26 13:31:09 -0300

    refact: remove infra helper logs

--- src/infra/helper/getFileContent.go
+++ src/infra/helper/getFileContent.go
@@ -2,21 +2,18 @@ package infraHelper
 
 import (
 	"errors"
-	"log"
 	"os"
 )
 
 func GetFileContent(filePath string) (string, error) {
 	_, err := os.Stat(filePath)
 	if err != nil {
-		log.Printf("FailedToGetFileContent: %v", err)
-		return "", errors.New("FailedToGetFileContent")
+		return "", errors.New("FailedToGetFileContent: " + err.Error())
 	}
 
 	fileContentBytes, err := os.ReadFile(filePath)
 	if err != nil {
-		log.Printf("FailedToGetFileContent: %v", err)
-		return "", errors.New("FailedToGetFileContent")
+		return "", errors.New("FailedToGetFileContent: " + err.Error())
 	}
 	fileContentStr := string(fileContentBytes)
 
