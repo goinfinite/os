8b8fadbd7251ef4e8aa9ffb42421ea70bf60cfca
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-02-05 12:35:31 -0300

    refact: improve error message in os.Stat in GetFileContent infra helper

--- src/infra/helper/getFileContent.go
+++ src/infra/helper/getFileContent.go
@@ -13,7 +13,7 @@ func GetFileContent(filePath string) (string, error) {
 
 	_, err := os.Stat(filePath)
 	if err != nil {
-		return "", errors.New("FailedToGetFileContent: " + err.Error())
+		return "", errors.New("FailedToGetFileInfo: " + err.Error())
 	}
 
 	fileContentBytes, err := os.ReadFile(filePath)
