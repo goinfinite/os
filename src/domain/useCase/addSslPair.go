bc01ac3bae8fc133b38eae7ce019508488c15eeb
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-02-09 16:13:58 -0300

    feat: add vhost existence validation using vhostQueryRepo .Get() func and slices.Contains()

--- src/domain/useCase/addSslPair.go
+++ src/domain/useCase/addSslPair.go
@@ -3,6 +3,7 @@ package useCase
 import (
 	"errors"
 	"log"
+	"slices"
 
 	"github.com/speedianet/os/src/domain/dto"
 	"github.com/speedianet/os/src/domain/repository"
@@ -13,15 +14,37 @@ func AddSslPair(
 	vhostQueryRepo repository.VirtualHostQueryRepo,
 	addSslPair dto.AddSslPair,
 ) error {
+	vhosts, err := vhostQueryRepo.Get()
+	if err != nil {
+		log.Printf("FailedToGetVhosts: %s", err.Error())
+		return errors.New("FailedToGetVhostsInfraError")
+	}
+
+	if len(vhosts) == 0 {
+		log.Printf("VhostsNotFound")
+		return errors.New("VhostsNotFound")
+	}
+
+	existingVhosts := []string{}
+	for _, vhost := range vhosts {
+		existingVhosts = append(existingVhosts, vhost.Hostname.String())
+	}
+
+	shouldReturnVhostError := false
+	if len(addSslPair.VirtualHosts) == 1 {
+		shouldReturnVhostError = true
+	}
+
 	for _, vhost := range addSslPair.VirtualHosts {
-		_, err := vhostQueryRepo.GetByHostname(vhost)
-		if err != nil {
-			log.Printf("OneOfTheVhostsDoesNotExists: %s", vhost.String())
-			return errors.New("OneOfTheVhostsDoesNotExists")
+		if !slices.Contains(existingVhosts, vhost.String()) {
+			log.Printf("VhostDoesNotExists: %s", vhost.String())
+			if shouldReturnVhostError {
+				return errors.New("VhostDoesNotExists")
+			}
 		}
 	}
 
-	err := sslCmdRepo.Add(addSslPair)
+	err = sslCmdRepo.Add(addSslPair)
 	if err != nil {
 		log.Printf("AddSslPairError: %s", err)
 		return errors.New("AddSslPairInfraError")
