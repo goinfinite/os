ab2f157b47618241b3fdb75010e0d3c845bfcffb
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-02-21 12:31:46 -0300

    feat: add remove wildcard flow from hostname in CreateVirtualHost

--- src/domain/useCase/createVirtualHost.go
+++ src/domain/useCase/createVirtualHost.go
@@ -3,9 +3,11 @@ package useCase
 import (
 	"errors"
 	"log"
+	"strings"
 
 	"github.com/speedianet/os/src/domain/dto"
 	"github.com/speedianet/os/src/domain/repository"
+	"github.com/speedianet/os/src/domain/valueObject"
 )
 
 func CreateVirtualHost(
@@ -21,6 +23,18 @@ func CreateVirtualHost(
 	isAlias := addVirtualHost.Type.String() == "alias"
 	if isAlias && addVirtualHost.ParentHostname == nil {
 		return errors.New("AliasMustHaveParentHostname")
+	}
+
+	hostnameStr := addVirtualHost.Hostname.String()
+	hasWildcardInHostname := strings.HasPrefix(hostnameStr, "*.")
+	if hasWildcardInHostname {
+		hostnameWithoutWildcardStr := strings.Replace(hostnameStr, "*.", "", 1)
+		hostnameWithoutWildcard, err := valueObject.NewFqdn(hostnameWithoutWildcardStr)
+		if err != nil {
+			return errors.New("FailedToRemoveWildcardFromHostname: " + err.Error())
+		}
+
+		addVirtualHost.Hostname = hostnameWithoutWildcard
 	}
 
 	err = vhostCmdRepo.Create(addVirtualHost)
