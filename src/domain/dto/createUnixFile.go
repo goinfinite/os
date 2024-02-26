5fa3f46bef4ed7360dd073c76479049e48db7e9f
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-02-26 12:23:15 -0300

    feat: turn permissions to a pointer in CreateUnixFile DTO

--- src/domain/dto/createUnixFile.go
+++ src/domain/dto/createUnixFile.go
@@ -3,14 +3,14 @@ package dto
 import "github.com/speedianet/os/src/domain/valueObject"
 
 type CreateUnixFile struct {
-	FilePath    valueObject.UnixFilePath        `json:"filePath"`
-	Permissions valueObject.UnixFilePermissions `json:"permissions"`
-	MimeType    valueObject.MimeType            `json:"mimeType"`
+	FilePath    valueObject.UnixFilePath         `json:"filePath"`
+	Permissions *valueObject.UnixFilePermissions `json:"permissions"`
+	MimeType    valueObject.MimeType             `json:"mimeType"`
 }
 
 func NewCreateUnixFile(
 	filePath valueObject.UnixFilePath,
-	permissions valueObject.UnixFilePermissions,
+	permissions *valueObject.UnixFilePermissions,
 	mimeType valueObject.MimeType,
 ) CreateUnixFile {
 	return CreateUnixFile{
