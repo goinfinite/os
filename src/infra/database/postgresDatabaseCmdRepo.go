2afa78b8a31904c32333d6a70e4dfcbea22c354b
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-02-26 04:23:38 -0300

    feat: add the right implementation to postgres cmd repo

--- src/infra/database/postgresDatabaseCmdRepo.go
+++ src/infra/database/postgresDatabaseCmdRepo.go
@@ -1,8 +1,6 @@
 package databaseInfra
 
 import (
-	"strings"
-
 	"github.com/speedianet/os/src/domain/dto"
 	"github.com/speedianet/os/src/domain/valueObject"
 )
@@ -12,7 +10,8 @@ type PostgresDatabaseCmdRepo struct {
 
 func (repo PostgresDatabaseCmdRepo) Add(dbName valueObject.DatabaseName) error {
 	_, err := PostgresqlCmd(
-		"CREATE DATABASE " + dbName.String(),
+		"CREATE DATABASE "+dbName.String(),
+		nil,
 	)
 
 	return err
@@ -20,31 +19,98 @@ func (repo PostgresDatabaseCmdRepo) Add(dbName valueObject.DatabaseName) error {
 
 func (repo PostgresDatabaseCmdRepo) Delete(dbName valueObject.DatabaseName) error {
 	_, err := PostgresqlCmd(
-		"DROP DATABASE " + dbName.String(),
+		"DROP DATABASE "+dbName.String(),
+		nil,
+	)
+
+	return err
+}
+
+func (repo PostgresDatabaseCmdRepo) addPermissionsToUser(
+	dbName valueObject.DatabaseName,
+	dbUser valueObject.DatabaseUsername,
+) error {
+	_, err := PostgresqlCmd(
+		"GRANT ALL PRIVILEGES ON DATABASE "+dbName.String()+
+			" TO "+dbUser.String(),
+		nil,
+	)
+	if err != nil {
+		return err
+	}
+
+	_, err = PostgresqlCmd("GRANT ALL ON ALL TABLES IN SCHEMA public TO "+dbUser.String(), nil)
+	if err != nil {
+		return err
+	}
+
+	_, err = PostgresqlCmd("GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO "+dbUser.String(), nil)
+	return err
+}
+
+func (repo PostgresDatabaseCmdRepo) setUserDefaultPermissions(
+	dbName valueObject.DatabaseName,
+	dbUser valueObject.DatabaseUsername,
+) error {
+	dbNameStr := dbName.String()
+
+	_, err := PostgresqlCmd(
+		"ALTER DEFAULT PRIVILEGES IN SCHEMA public "+
+			"GRANT ALL ON TABLES TO "+dbUser.String(),
+		&dbNameStr,
+	)
+	if err != nil {
+		return err
+	}
+
+	_, err = PostgresqlCmd(
+		"ALTER DEFAULT PRIVILEGES IN SCHEMA public "+
+			"GRANT ALL ON SEQUENCES TO "+dbUser.String(),
+		&dbNameStr,
 	)
 
 	return err
 }
 
 func (repo PostgresDatabaseCmdRepo) AddUser(addDatabaseUser dto.AddDatabaseUser) error {
-	privilegesStrList := make([]string, len(addDatabaseUser.Privileges))
-	for i, privilege := range addDatabaseUser.Privileges {
-		privilegesStrList[i] = privilege.String()
+	_, err := PostgresqlCmd(
+		"CREATE USER "+addDatabaseUser.Username.String()+
+			" WITH PASSWORD '"+addDatabaseUser.Password.String()+"'",
+		nil,
+	)
+	if err != nil {
+		return err
 	}
-	privilegesStr := strings.Join(privilegesStrList, ", ")
+
+	err = repo.addPermissionsToUser(addDatabaseUser.DatabaseName, addDatabaseUser.Username)
+	if err != nil {
+		return err
+	}
+
+	err = repo.setUserDefaultPermissions(addDatabaseUser.DatabaseName, addDatabaseUser.Username)
+	return err
+}
+
+func (repo PostgresDatabaseCmdRepo) revokeUserDefaultPermissions(
+	dbName valueObject.DatabaseName,
+	dbUser valueObject.DatabaseUsername,
+) error {
+	dbNameStr := dbName.String()
 
 	_, err := PostgresqlCmd(
-		"GRANT " +
-			privilegesStr +
-			" ON " +
-			addDatabaseUser.DatabaseName.String() +
-			".* TO '" +
-			addDatabaseUser.Username.String() + "'@'%' " +
-			"IDENTIFIED BY '" +
-			addDatabaseUser.Password.String() +
-			"';",
+		"ALTER DEFAULT PRIVILEGES IN SCHEMA public "+
+			"REVOKE ALL ON TABLES FROM "+dbUser.String(),
+		&dbNameStr,
 	)
+	if err != nil {
+		return err
+	}
 
+	_, err = PostgresqlCmd(
+		"ALTER DEFAULT PRIVILEGES IN SCHEMA public "+
+			"REVOKE ALL ON SEQUENCES FROM "+dbUser.String(),
+		&dbNameStr,
+	)
 	return err
 }
 
@@ -53,12 +119,18 @@ func (repo PostgresDatabaseCmdRepo) DeleteUser(
 	dbUser valueObject.DatabaseUsername,
 ) error {
 	_, err := PostgresqlCmd(
-		"REVOKE ALL PRIVILEGES ON " +
-			dbName.String() +
-			".* FROM '" +
-			dbUser.String() +
-			"'@'%';",
+		"REVOKE ALL ON DATABASE "+dbName.String()+" FROM "+dbUser.String(),
+		nil,
 	)
+	if err != nil {
+		return err
+	}
 
+	err = repo.revokeUserDefaultPermissions(dbName, dbUser)
+	if err != nil {
+		return err
+	}
+
+	_, err = PostgresqlCmd("DROP USER "+dbUser.String(), nil)
 	return err
 }
