c493bebeebf4524635e53262e35daf4b7c3e2566
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-02-28 18:41:14 -0300

    refact: recreate shared helper to CheckServiceAvailability instead CheckServices

---
+++ src/presentation/shared/helper/checkServiceAvailability.go
@@ -0,0 +1,39 @@
+package sharedHelper
+
+import (
+	"slices"
+
+	servicesInfra "github.com/speedianet/os/src/infra/services"
+)
+
+func CheckServiceAvailability(svcName string, requiredSvcNames []string) {
+	servicesQueryRepo := servicesInfra.ServicesQueryRepo{}
+	availableSvcs, err := servicesQueryRepo.Get()
+	if err != nil {
+		panic("FailedToGetAvailableServices: " + err.Error())
+	}
+
+	serviceIsRunning := false
+	for _, availableSvc := range availableSvcs {
+		availableSvcName := availableSvc.Name.String()
+
+		if !slices.Contains(requiredSvcNames, availableSvcName) {
+			continue
+		}
+
+		if availableSvcName != svcName {
+			continue
+		}
+
+		if availableSvc.Status.String() != "running" {
+			continue
+		}
+
+		serviceIsRunning = true
+		break
+	}
+
+	if !serviceIsRunning {
+		panic("ServiceUnavailable: " + svcName)
+	}
+}
