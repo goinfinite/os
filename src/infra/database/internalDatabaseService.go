47aff49e8a2c10138f44fcf467271bce7d48dd9a
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-03-03 23:40:06 -0300

    fix: remove unused methods from InternalDatabaseService

--- src/infra/database/internalDatabaseService.go
+++ src/infra/database/internalDatabaseService.go
@@ -2,7 +2,6 @@ package databaseInfra
 
 import (
 	"errors"
-	"reflect"
 
 	"github.com/glebarez/sqlite"
 	"gorm.io/gorm"
@@ -23,76 +22,5 @@ func NewInternalDatabaseService() (*InternalDatabaseService, error) {
 		return nil, errors.New("DatabaseConnectionError")
 	}
 
-	internalDbSvc := &InternalDatabaseService{Handler: ormSvc}
-	err = internalDbSvc.dbMigrate()
-	if err != nil {
-		return nil, err
-	}
-
-	return internalDbSvc, nil
-}
-
-func (internalDbSvc InternalDatabaseService) isTableEmpty(model interface{}) (bool, error) {
-	var count int64
-	err := internalDbSvc.Handler.Model(&model).Count(&count).Error
-	if err != nil {
-		return false, err
-	}
-
-	return count == 0, nil
-}
-
-func (internalDbSvc InternalDatabaseService) seedDatabase(seedModels ...interface{}) error {
-	for _, seedModel := range seedModels {
-		isTableEmpty, err := internalDbSvc.isTableEmpty(seedModel)
-		if err != nil {
-			return err
-		}
-
-		if !isTableEmpty {
-			continue
-		}
-
-		seedModelType := reflect.TypeOf(seedModel).Elem()
-		seedModelFieldsAndMethods := reflect.ValueOf(seedModel)
-
-		seedModelInitialEntriesMethod := seedModelFieldsAndMethods.MethodByName(
-			"InitialEntries",
-		)
-		seedModelInitialEntriesMethodResults := seedModelInitialEntriesMethod.Call(
-			[]reflect.Value{},
-		)
-		initialEntries := seedModelInitialEntriesMethodResults[0].Interface()
-
-		for _, entry := range initialEntries.([]interface{}) {
-			entryInnerStructure := reflect.ValueOf(entry)
-
-			entryFormatHandlerWillAccept := reflect.New(seedModelType)
-			entryFormatHandlerWillAccept.Elem().Set(entryInnerStructure)
-			adjustedEntry := entryFormatHandlerWillAccept.Interface()
-
-			err = internalDbSvc.Handler.Create(adjustedEntry).Error
-			if err != nil {
-				return err
-			}
-		}
-	}
-
-	return nil
-}
-
-func (internalDbSvc InternalDatabaseService) dbMigrate() error {
-	err := internalDbSvc.Handler.AutoMigrate()
-	if err != nil {
-		return errors.New("DatabaseMigrationError")
-	}
-
-	modelsWithInitialEntries := []interface{}{}
-
-	err = internalDbSvc.seedDatabase(modelsWithInitialEntries...)
-	if err != nil {
-		return errors.New("AddDefaultDatabaseEntriesError")
-	}
-
-	return nil
+	return &InternalDatabaseService{Handler: ormSvc}, nil
 }
