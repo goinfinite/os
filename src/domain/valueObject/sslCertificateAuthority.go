4003ff0b96171afaeec2b9e05cfc35bad71a5096
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-03-15 14:47:22 -0300

    feat: add SslCertificateAuthority VO

---
+++ src/domain/valueObject/sslCertificateAuthority.go
@@ -0,0 +1,37 @@
+package valueObject
+
+import (
+	"errors"
+	"regexp"
+)
+
+const sslCertificateAuthorityRegex string = `^[A-z\.\,\s\'\(\)]{1,50}$`
+
+type SslCertificateAuthority string
+
+func NewSslCertificateAuthority(value string) (SslCertificateAuthority, error) {
+	sca := SslCertificateAuthority(value)
+
+	if !sca.isValid() {
+		return "", errors.New("InvalidSslCertificateAuthority")
+	}
+
+	return SslCertificateAuthority(sca), nil
+}
+
+func NewSslCertificateAuthorityPanic(value string) SslCertificateAuthority {
+	sn, err := NewSslCertificateAuthority(value)
+	if err != nil {
+		panic(err)
+	}
+	return sn
+}
+
+func (sca SslCertificateAuthority) isValid() bool {
+	compiledScaRegex := regexp.MustCompile(sslCertificateAuthorityRegex)
+	return compiledScaRegex.MatchString(string(sca))
+}
+
+func (sca SslCertificateAuthority) String() string {
+	return string(sca)
+}
---
+++ src/domain/valueObject/sslCertificateAuthority_test.go
@@ -0,0 +1,36 @@
+package valueObject
+
+import "testing"
+
+func TestSslCertificateAuthority(t *testing.T) {
+	t.Run("ValidSslCertificateAuthority", func(t *testing.T) {
+		validsSslCertificateAuthority := []string{
+			"IdenTrust",
+			"DigiCert Group",
+			"Sectigo (Comodo Cybersecurity)",
+			"GlobalSign",
+			"Let's Encrypt",
+			"GoDaddy Group",
+		}
+		for _, sslCertificateAuthority := range validsSslCertificateAuthority {
+			_, err := NewSslCertificateAuthority(sslCertificateAuthority)
+			if err != nil {
+				t.Errorf("Expected no error for %s, got %s", sslCertificateAuthority, err.Error())
+			}
+		}
+	})
+
+	t.Run("InvalidSslCertificateAuthority", func(t *testing.T) {
+		invalidsSslCertificateAuthority := []string{
+			"",
+			"Nitro Auth@rity",
+			"Super long certificate authority, because I don't know",
+		}
+		for _, sslCertificateAuthority := range invalidsSslCertificateAuthority {
+			_, err := NewSslCertificateAuthority(sslCertificateAuthority)
+			if err == nil {
+				t.Errorf("Expected error for %s, got nil", sslCertificateAuthority)
+			}
+		}
+	})
+}
