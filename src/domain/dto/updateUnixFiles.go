b9beca88e40bab1e44f227ba3f23c7e0c14ff3ab
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-03-15 14:36:28 -0300

    refact: change DTO name from UpdateUnixFile to UpdateUnixFiles

--- src/domain/dto/updateUnixFiles.go
+++ src/domain/dto/updateUnixFiles.go
@@ -2,20 +2,20 @@ package dto
 
 import "github.com/speedianet/os/src/domain/valueObject"
 
-type UpdateUnixFile struct {
+type UpdateUnixFiles struct {
 	SourcePaths     []valueObject.UnixFilePath       `json:"sourcePaths"`
 	DestinationPath *valueObject.UnixFilePath        `json:"destinationPath"`
 	Permissions     *valueObject.UnixFilePermissions `json:"permissions"`
 	EncodedContent  *valueObject.EncodedContent      `json:"encodedContent"`
 }
 
-func NewUpdateUnixFile(
+func NewUpdateUnixFiles(
 	sourcePaths []valueObject.UnixFilePath,
 	destinationPath *valueObject.UnixFilePath,
 	permissions *valueObject.UnixFilePermissions,
 	encodedContent *valueObject.EncodedContent,
-) UpdateUnixFile {
-	return UpdateUnixFile{
+) UpdateUnixFiles {
+	return UpdateUnixFiles{
 		SourcePaths:     sourcePaths,
 		DestinationPath: destinationPath,
 		Permissions:     permissions,
