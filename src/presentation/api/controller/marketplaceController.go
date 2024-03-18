a9d26e4bedc91a065c5ab3fe8ddd512a2aa870ba
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-03-18 14:56:13 -0300

    feat: implements infra and domain  structureto GetCatalogController

--- src/presentation/api/controller/marketplaceController.go
+++ src/presentation/api/controller/marketplaceController.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/speedianet/os/src/domain/useCase"
+	mktplaceInfra "github.com/speedianet/os/src/infra/mktplace"
 	apiHelper "github.com/speedianet/os/src/presentation/api/helper"
 )
 
@@ -17,5 +19,11 @@ import (
 // @Success      200 {string} "AllCatalogProducts"
 // @Router       /marketplace/catalog/ [get]
 func GetCatalogController(c echo.Context) error {
+	mktplaceQueryRepo := mktplaceInfra.MarketplaceQueryRepo{}
+	err := useCase.GetMarketplaceCatalog(mktplaceQueryRepo)
+	if err != nil {
+		return apiHelper.ResponseWrapper(c, http.StatusInternalServerError, err.Error())
+	}
+
 	return apiHelper.ResponseWrapper(c, http.StatusOK, "AllCatalogProducts")
 }
