63fa2131e7a6d51132ee284cf301cfcc4e186432
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-03-18 17:06:17 -0300

    feat: add GenerateString test helper

--- src/devUtils/testHelpers.go
+++ src/devUtils/testHelpers.go
@@ -1,12 +1,19 @@
 package testHelpers
 
 import (
+	"encoding/base64"
 	"fmt"
 	"path"
 	"runtime"
 
 	"github.com/joho/godotenv"
 )
+
+func GenerateString(desiredSize int) string {
+	desiredByteSize := uint((float64(desiredSize) / 4) * 3)
+	randomBytes := make([]byte, desiredByteSize)
+	return base64.StdEncoding.EncodeToString(randomBytes)
+}
 
 func LoadEnvVars() {
 	_, fileDirectory, _, _ := runtime.Caller(0)
