794ac2ee03c273bb212acde5c6b55cec833e3a9a
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-03-26 18:47:36 -0300

    feat: add UnmarshalJSON method to MktplaceItemInstallStep VO

--- src/domain/valueObject/mktplaceItemInstallStep.go
+++ src/domain/valueObject/mktplaceItemInstallStep.go
@@ -1,33 +1,49 @@
 package valueObject
 
-import "errors"
+import (
+	"errors"
+	"strings"
+)
 
 type MktplaceItemInstallStep string
 
 func NewMktplaceItemInstallStep(value string) (MktplaceItemInstallStep, error) {
-	mkplaceItemInstallStep := MktplaceItemInstallStep(value)
-	if !mkplaceItemInstallStep.isValid() {
+	mpis := MktplaceItemInstallStep(value)
+	if !mpis.isValid() {
 		return "", errors.New("InvalidMarketplaceItemInstallStep")
 	}
 
-	return mkplaceItemInstallStep, nil
+	return mpis, nil
 }
 
 func NewMktplaceItemInstallStepPanic(value string) MktplaceItemInstallStep {
-	mkplaceItemInstallStep, err := NewMktplaceItemInstallStep(value)
+	mpis, err := NewMktplaceItemInstallStep(value)
 	if err != nil {
 		panic(err)
 	}
 
-	return mkplaceItemInstallStep
+	return mpis
 }
 
-func (mkplaceItemInstallStep MktplaceItemInstallStep) isValid() bool {
-	isTooShort := len(string(mkplaceItemInstallStep)) < 1
-	isTooLong := len(string(mkplaceItemInstallStep)) > 512
+func (mpis MktplaceItemInstallStep) isValid() bool {
+	isTooShort := len(string(mpis)) < 1
+	isTooLong := len(string(mpis)) > 512
 	return !isTooShort && !isTooLong
 }
 
-func (mkplaceItemInstallStep MktplaceItemInstallStep) String() string {
-	return string(mkplaceItemInstallStep)
+func (mpis MktplaceItemInstallStep) String() string {
+	return string(mpis)
+}
+
+func (mpisPtr *MktplaceItemInstallStep) UnmarshalJSON(value []byte) error {
+	valueStr := string(value)
+	unquotedValue := strings.Trim(valueStr, "\"")
+
+	mpis, err := NewMktplaceItemInstallStep(unquotedValue)
+	if err != nil {
+		return err
+	}
+
+	*mpisPtr = mpis
+	return nil
 }
