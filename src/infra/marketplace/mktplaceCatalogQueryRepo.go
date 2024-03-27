11ef5e09c9873c624b2879ef78decc38da8ac61a
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-03-27 19:24:20 -0300

    feat: add factory and all the other code that search in assets and create MktplaceCatalogItem

--- src/infra/marketplace/mktplaceCatalogQueryRepo.go
+++ src/infra/marketplace/mktplaceCatalogQueryRepo.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"errors"
 	"io/fs"
+	"log"
 
 	"github.com/speedianet/os/src/domain/entity"
 	"github.com/speedianet/os/src/domain/valueObject"
@@ -15,13 +16,12 @@ var assets embed.FS
 
 type MktplaceCatalogQueryRepo struct{}
 
-func (repo MktplaceCatalogQueryRepo) getMarketplaceCatalogItemFromName(
-	mktplaceItemName valueObject.MktplaceItemName,
+func (repo MktplaceCatalogQueryRepo) getMktCatalogItemFromFilePath(
+	mktplaceItemFilePath valueObject.UnixFilePath,
 ) (entity.MarketplaceCatalogItem, error) {
 	var mktplaceCatalogItem entity.MarketplaceCatalogItem
 
-	mktplaceItemFilePath := "assets/" + mktplaceItemName.String() + ".json"
-	mktplaceCatalogItemFile, err := assets.Open(mktplaceItemFilePath)
+	mktplaceCatalogItemFile, err := assets.Open(mktplaceItemFilePath.String())
 	if err != nil {
 		return mktplaceCatalogItem, errors.New(
 			"FailedToOpenMktCatalogItemFile: " + err.Error(),
@@ -43,13 +43,34 @@ func (repo MktplaceCatalogQueryRepo) getMarketplaceCatalogItemFromName(
 func (repo MktplaceCatalogQueryRepo) GetItems() ([]entity.MarketplaceCatalogItem, error) {
 	mktplaceCatalogItems := []entity.MarketplaceCatalogItem{}
 
-	assetsEntries, err := fs.ReadDir(assets, "assets")
+	mktplaceItemFiles, err := fs.ReadDir(assets, "assets")
 	if err != nil {
 		return mktplaceCatalogItems, errors.New("FailedToGetMktAssetsFiles: " + err.Error())
 	}
 
-	if len(assetsEntries) == 0 {
+	if len(mktplaceItemFiles) == 0 {
 		return mktplaceCatalogItems, errors.New("MktAssetsEmpty")
+	}
+
+	for mktplaceItemFileIndex, mktplaceItemFile := range mktplaceItemFiles {
+		mktplaceItemFilePathStr := "assets/" + mktplaceItemFile.Name()
+		mktplaceItemFilePath, err := valueObject.NewUnixFilePath(mktplaceItemFilePathStr)
+		if err != nil {
+			log.Printf("%s : %s", err.Error(), mktplaceItemFilePathStr)
+			continue
+		}
+
+		mktplaceCatalogItem, err := repo.getMktCatalogItemFromFilePath(mktplaceItemFilePath)
+		if err != nil {
+			log.Printf("FailedToGetMktCatalogItem: %s", err.Error())
+			continue
+		}
+
+		mktplaceItemIdInt := mktplaceItemFileIndex + 1
+		mktplaceItemId, _ := valueObject.NewMktplaceItemId(mktplaceItemIdInt)
+		mktplaceCatalogItem.Id = mktplaceItemId
+
+		mktplaceCatalogItems = append(mktplaceCatalogItems, mktplaceCatalogItem)
 	}
 
 	return mktplaceCatalogItems, nil
