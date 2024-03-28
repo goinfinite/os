2542651b04518d4667ce7bae2c025294f4b8c8de
ntorga <northontorga@gmail.com>
2024-03-28 10:20:24 -0300

    test: is domain mapped to server

--- src/infra/vhost/virtualHostQueryRepo_test.go
+++ src/infra/vhost/virtualHostQueryRepo_test.go
@@ -4,16 +4,16 @@ import (
 	"testing"
 
 	testHelpers "github.com/speedianet/os/src/devUtils"
+	"github.com/speedianet/os/src/domain/valueObject"
 	infraHelper "github.com/speedianet/os/src/infra/helper"
 )
 
 func TestVirtualHostQueryRepo(t *testing.T) {
+	vhostQueryRepo := VirtualHostQueryRepo{}
 	testHelpers.LoadEnvVars()
 
 	t.Run("GetVirtualHosts", func(t *testing.T) {
-		repo := VirtualHostQueryRepo{}
-		vhosts, err := repo.Get()
-
+		vhosts, err := vhostQueryRepo.Get()
 		if err != nil {
 			t.Errorf("ExpectingNoErrorButGot: %v", err)
 		}
@@ -39,15 +39,30 @@ location / {
 			t.Errorf("UpdateMappingFileError: %s", err.Error())
 		}
 
-		repo := VirtualHostQueryRepo{}
-		vhosts, err := repo.GetWithMappings()
-
+		vhosts, err := vhostQueryRepo.GetWithMappings()
 		if err != nil {
 			t.Errorf("ExpectingNoErrorButGot: %v", err)
 		}
 
 		if len(vhosts) == 0 {
 			t.Errorf("ExpectingNonEmptySliceButGot: %v", vhosts)
+		}
+	})
+
+	t.Run("TestDomainNotMappedToServer", func(t *testing.T) {
+		primaryVhost, err := infraHelper.GetPrimaryVirtualHost()
+		if err != nil {
+			t.Errorf("GetPrimaryVirtualHostError: %s", err.Error())
+		}
+		randomHash := valueObject.NewHashPanic("randomhash")
+
+		isDomainMapped := vhostQueryRepo.IsDomainMappedToServer(
+			primaryVhost,
+			randomHash,
+		)
+
+		if isDomainMapped {
+			t.Errorf("ExpectingFalseButGot: %v", isDomainMapped)
 		}
 	})
 }
