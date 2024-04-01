0158f910add05ce3a570370e20ee3da86a9ddcd1
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-04-01 20:56:29 -0300

    feat: implements all PersistentDatabaseService

--- src/infra/internalDatabase/persistentDatabaseService.go
+++ src/infra/internalDatabase/persistentDatabaseService.go
@@ -2,8 +2,10 @@ package internalDbInfra
 
 import (
 	"errors"
+	"reflect"
 
 	"github.com/glebarez/sqlite"
+	dbModel "github.com/speedianet/os/src/infra/internalDatabase/model"
 	"gorm.io/gorm"
 )
 
@@ -22,5 +24,78 @@ func NewPersistentDatabaseService() (*PersistentDatabaseService, error) {
 		return nil, errors.New("DatabaseConnectionError")
 	}
 
-	return &PersistentDatabaseService{Handler: ormSvc}, nil
+	dbSvc := &PersistentDatabaseService{Handler: ormSvc}
+	err = dbSvc.dbMigrate()
+	if err != nil {
+		return nil, err
+	}
+
+	return dbSvc, nil
+}
+
+func (dbSvc *PersistentDatabaseService) isTableEmpty(model interface{}) (bool, error) {
+	var count int64
+	err := dbSvc.Handler.Model(&model).Count(&count).Error
+	if err != nil {
+		return false, err
+	}
+
+	return count == 0, nil
+}
+
+func (dbSvc *PersistentDatabaseService) seedDatabase(seedModels ...interface{}) error {
+	for _, seedModel := range seedModels {
+		isTableEmpty, err := dbSvc.isTableEmpty(seedModel)
+		if err != nil {
+			return err
+		}
+
+		if !isTableEmpty {
+			continue
+		}
+
+		seedModelType := reflect.TypeOf(seedModel).Elem()
+		seedModelFieldsAndMethods := reflect.ValueOf(seedModel)
+
+		seedModelInitialEntriesMethod := seedModelFieldsAndMethods.MethodByName(
+			"InitialEntries",
+		)
+		seedModelInitialEntriesMethodResults := seedModelInitialEntriesMethod.Call(
+			[]reflect.Value{},
+		)
+		initialEntries := seedModelInitialEntriesMethodResults[0].Interface()
+
+		for _, entry := range initialEntries.([]interface{}) {
+			entryInnerStructure := reflect.ValueOf(entry)
+
+			entryFormatHandlerWillAccept := reflect.New(seedModelType)
+			entryFormatHandlerWillAccept.Elem().Set(entryInnerStructure)
+			adjustedEntry := entryFormatHandlerWillAccept.Interface()
+
+			err = dbSvc.Handler.Create(adjustedEntry).Error
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func (dbSvc *PersistentDatabaseService) dbMigrate() error {
+	err := dbSvc.Handler.AutoMigrate(
+		&dbModel.MarketplaceInstalledItem{},
+	)
+	if err != nil {
+		return errors.New("DatabaseMigrationError")
+	}
+
+	modelsWithInitialEntries := []interface{}{}
+
+	err = dbSvc.seedDatabase(modelsWithInitialEntries...)
+	if err != nil {
+		return errors.New("CreateDefaultDatabaseEntriesError")
+	}
+
+	return nil
 }
