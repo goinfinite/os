be222be47d0dd6b27c3d3987fe2e9e2e0f7254df
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-04-01 06:11:18 -0300

    feat: add removeMariaDb function to uninstall MariaDB properly

--- src/infra/services/uninstall.go
+++ src/infra/services/uninstall.go
@@ -1,9 +1,60 @@
 package servicesInfra
 
 import (
+	"os"
+	"strings"
+	"time"
+
 	"github.com/speedianet/os/src/domain/valueObject"
 	infraHelper "github.com/speedianet/os/src/infra/helper"
 )
+
+func purgePkgs(packages []string) error {
+	purgePackages := append([]string{"purge", "-y"}, packages...)
+	_, err := infraHelper.RunCmd("apt-get", purgePackages...)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func removeMariaDb() error {
+	mariaDbPidFilePath := "/run/mysqld/mysqld.pid"
+	mariaDbPid, err := infraHelper.GetFileContent(mariaDbPidFilePath)
+	if err != nil {
+		return err
+	}
+
+	mariaDbPidWithoutBreakLine := strings.Trim(mariaDbPid, "\n")
+	_, err = infraHelper.RunCmd(
+		"kill",
+		"-15",
+		mariaDbPidWithoutBreakLine,
+	)
+	if err != nil {
+		return err
+	}
+
+	time.Sleep(2 * time.Second)
+
+	_, err = infraHelper.RunCmd(
+		"mariadbd-safe",
+		"stop",
+	)
+	if err != nil {
+		return err
+	}
+
+	mariaDbDataDirPath := "/var/lib/mysql"
+	err = os.RemoveAll(mariaDbDataDirPath)
+	if err != nil {
+		return err
+	}
+
+	packages := append(MariaDbPackages, "mariadb*", "mysql*")
+	return purgePkgs(packages)
+}
 
 func Uninstall(name valueObject.ServiceName) error {
 	err := SupervisordFacade{}.RemoveConf(name)
@@ -11,23 +62,15 @@ func Uninstall(name valueObject.ServiceName) error {
 		return err
 	}
 
-	var packages []string
 	switch name.String() {
 	case "php":
-		packages = append(OlsPackages, "lsphp*")
+		packages := append(OlsPackages, "lsphp*")
+		return purgePkgs(packages)
 	case "mariadb":
-		packages = MariaDbPackages
+		return removeMariaDb()
 	case "redis":
-		packages = RedisPackages
+		return purgePkgs(RedisPackages)
 	default:
 		return nil
 	}
-
-	purgePackages := append([]string{"purge", "-y"}, packages...)
-	_, err = infraHelper.RunCmd("apt-get", purgePackages...)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
