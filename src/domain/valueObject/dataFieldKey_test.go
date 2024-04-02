110b2fbd729b544912a0f716b955ad24475e63df
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-04-02 14:02:21 -0300

    feat: add UnmarshalJSON valid and invalid test case to DataFieldKey VO

--- src/domain/valueObject/dataFieldKey_test.go
+++ src/domain/valueObject/dataFieldKey_test.go
@@ -1,6 +1,8 @@
 package valueObject
 
 import (
+	"encoding/json"
+	"strings"
 	"testing"
 
 	testHelpers "github.com/speedianet/os/src/devUtils"
@@ -26,7 +28,7 @@ func TestDataFieldKey(t *testing.T) {
 		invalidLength := 40
 		invalidDataFieldKeys := []string{
 			"",
-			"./teste",
+			"./test",
 			testHelpers.GenerateString(invalidLength),
 		}
 
@@ -35,6 +37,53 @@ func TestDataFieldKey(t *testing.T) {
 			if err == nil {
 				t.Errorf("Expected error for %s, got nil", dfk)
 			}
+		}
+	})
+
+	t.Run("ValidUnmarshalJSON", func(t *testing.T) {
+		var testStruct struct {
+			DataToTest DataFieldKey
+		}
+
+		dataToTest := "SomeNiceDataFieldKey"
+		mapToTest := map[string]string{
+			"dataToTest": dataToTest,
+		}
+		mapBytesToTest, _ := json.Marshal(mapToTest)
+
+		reader := strings.NewReader(string(mapBytesToTest))
+		jsonDecoder := json.NewDecoder(reader)
+		err := jsonDecoder.Decode(&testStruct)
+		if err != nil {
+			t.Fatalf("Expected no error on UnmarshalJSON valid test, got %s", err.Error())
+		}
+
+		dataToTestFromStructStr := testStruct.DataToTest.String()
+		if dataToTestFromStructStr != dataToTest {
+			t.Errorf(
+				"VO data '%s' after UnmarshalJSON is not the same as the original data '%s'",
+				dataToTestFromStructStr,
+				dataToTest,
+			)
+		}
+	})
+
+	t.Run("InvalidUnmarshalJSON", func(t *testing.T) {
+		var testStruct struct {
+			DataToTest DataFieldKey
+		}
+
+		dataToTest := ""
+		mapToTest := map[string]string{
+			"dataToTest": dataToTest,
+		}
+		mapBytesToTest, _ := json.Marshal(mapToTest)
+
+		reader := strings.NewReader(string(mapBytesToTest))
+		jsonDecoder := json.NewDecoder(reader)
+		err := jsonDecoder.Decode(&testStruct)
+		if err == nil {
+			t.Fatal("Expected error on UnmarshalJSON invalid test, got nil")
 		}
 	})
 }
