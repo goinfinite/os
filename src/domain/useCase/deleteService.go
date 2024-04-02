13adb83d7cd1c99a8edcb38d1487b6631b8f2d7e
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-04-02 12:11:24 -0300

    fix: remove comments from UC

--- src/domain/useCase/deleteService.go
+++ src/domain/useCase/deleteService.go
@@ -8,54 +8,6 @@ import (
 	"github.com/speedianet/os/src/domain/valueObject"
 )
 
-/*func (uc DeleteService) deleteSvcAutoMapping(
-	svcName valueObject.ServiceName,
-) error {
-	vhostsWithMappings, err := uc.vhostQueryRepo.GetWithMappings()
-	if err != nil {
-		return err
-	}
-
-	if len(vhostsWithMappings) == 0 {
-		return nil
-	}
-
-	primaryVhostWithMapping := vhostsWithMappings[0]
-	if len(primaryVhostWithMapping.Mappings) == 0 {
-		return nil
-	}
-
-	var mappingToDelete entity.Mapping
-	for _, primaryVhostMapping := range primaryVhostWithMapping.Mappings {
-		if primaryVhostMapping.TargetType.String() != "service" {
-			continue
-		}
-
-		targetServiceName := primaryVhostMapping.TargetServiceName
-		if targetServiceName == nil {
-			continue
-		}
-
-		if targetServiceName.String() != svcName.String() {
-			continue
-		}
-
-		mappingToDelete = primaryVhostMapping
-	}
-
-	hasMappingToDelete := mappingToDelete.Hostname != ""
-	if !hasMappingToDelete {
-		return nil
-	}
-
-	err = uc.vhostCmdRepo.DeleteMapping(mappingToDelete)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}*/
-
 func DeleteService(
 	queryRepo repository.ServicesQueryRepo,
 	cmdRepo repository.ServicesCmdRepo,
