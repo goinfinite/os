d750e744d395e034cec8c5043caa9988bdce3f5d
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-04-03 03:51:15 -0300

    feat: implements all installation to mktplaceCatalogCmdRepo, but it need to create mappings, so it's no finished

--- src/infra/marketplace/mktplaceCatalogCmdRepo.go
+++ src/infra/marketplace/mktplaceCatalogCmdRepo.go
@@ -1,8 +1,17 @@
 package mktplaceInfra
 
 import (
+	"errors"
+	"strings"
+	"time"
+
 	"github.com/speedianet/os/src/domain/dto"
+	"github.com/speedianet/os/src/domain/entity"
+	"github.com/speedianet/os/src/domain/valueObject"
+	infraHelper "github.com/speedianet/os/src/infra/helper"
 	internalDbInfra "github.com/speedianet/os/src/infra/internalDatabase"
+	dbModel "github.com/speedianet/os/src/infra/internalDatabase/model"
+	servicesInfra "github.com/speedianet/os/src/infra/services"
 )
 
 type MktplaceCatalogCmdRepo struct {
@@ -21,8 +30,95 @@ func NewMktplaceCatalogCmdRepo(
 	}
 }
 
+func (repo *MktplaceCatalogCmdRepo) getDataFieldsAsMap(
+	dataFields []valueObject.DataField,
+) map[string]string {
+	dataFieldMap := map[string]string{}
+
+	for _, dataField := range dataFields {
+		dataFieldMap[dataField.Key.String()] = dataField.Value.String()
+	}
+
+	return dataFieldMap
+}
+
 func (repo *MktplaceCatalogCmdRepo) InstallItem(
 	installMktplaceCatalogItem dto.InstallMarketplaceCatalogItem,
 ) error {
+	mktplaceCatalogItem, err := repo.queryRepo.GetItemById(
+		installMktplaceCatalogItem.Id,
+	)
+	if err != nil {
+		return errors.New("MktplaceCatalogItemNotFound")
+	}
+
+	for _, requiredSvcName := range mktplaceCatalogItem.Services {
+		requiredSvcAutoCreateMapping := false
+		requiredService := dto.NewCreateInstallableService(
+			requiredSvcName,
+			nil,
+			nil,
+			nil,
+			requiredSvcAutoCreateMapping,
+		)
+
+		err := servicesInfra.CreateInstallable(requiredService)
+		if err != nil {
+			return errors.New("InstallRequiredService: " + err.Error())
+		}
+	}
+
+	// Criar os mappings que o mktplaceCatalogItem exige.
+
+	dataFieldsMap := repo.getDataFieldsAsMap(installMktplaceCatalogItem.DataFields)
+	for _, cmdStep := range mktplaceCatalogItem.CmdSteps {
+		cmdStepStr := cmdStep.String()
+		cmdStepRequiredDataField, err := infraHelper.GetRegexFirstGroup(
+			cmdStepStr,
+			`%(.+)%`,
+		)
+		if err == nil {
+			requiredDataFieldValue := dataFieldsMap[cmdStepRequiredDataField]
+			cmdStepWithDataField := strings.ReplaceAll(
+				cmdStepStr,
+				"%"+cmdStepRequiredDataField+"%",
+				requiredDataFieldValue,
+			)
+			cmdStepStr = cmdStepWithDataField
+		}
+
+		_, err = infraHelper.RunCmdWithSubShell(cmdStepStr)
+		if err != nil {
+			return errors.New("RunCmdStepError: " + err.Error())
+		}
+	}
+
+	nowUnixTime := time.Now().Unix()
+	createdAt := valueObject.UnixTime(nowUnixTime)
+	updatedAt := valueObject.UnixTime(nowUnixTime)
+
+	mktplaceInstalledItem := entity.NewMarketplaceInstalledItem(
+		mktplaceCatalogItem.Id,
+		mktplaceCatalogItem.Name,
+		mktplaceCatalogItem.Type,
+		installMktplaceCatalogItem.RootDirectory,
+		mktplaceCatalogItem.Services,
+		[]entity.Mapping{},
+		mktplaceCatalogItem.AvatarUrl,
+		createdAt,
+		updatedAt,
+	)
+
+	mktplaceInstalledItemModel, err := dbModel.MarketplaceInstalledItem{}.ToModel(
+		mktplaceInstalledItem,
+	)
+	if err != nil {
+		return err
+	}
+
+	err = repo.persistentDbSvc.Handler.Create(&mktplaceInstalledItemModel).Error
+	if err != nil {
+		return err
+	}
 	return nil
 }
