05beffe700bd14c4e60b1650cb537bad35fce289
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-04-05 03:36:20 -0300

    feat: add entity constructor

--- src/domain/entity/marketplaceCatalogItem.go
+++ src/domain/entity/marketplaceCatalogItem.go
@@ -17,3 +17,31 @@ type MarketplaceCatalogItem struct {
 	AvatarUrl          valueObject.Url                          `json:"avatarUrl" yaml:"avatarUrl"`
 	ScreenshotUrls     []valueObject.Url                        `json:"screenshotUrls" yaml:"screenshotUrls"`
 }
+
+func NewMarketplaceCatalogItem(
+	id valueObject.MarketplaceItemId,
+	itemName valueObject.MarketplaceItemName,
+	itemType valueObject.MarketplaceItemType,
+	description valueObject.MarketplaceItemDescription,
+	serviceNames []valueObject.ServiceName,
+	mappings []MarketplaceMapping,
+	dataFields []valueObject.MarketplaceItemDataField,
+	cmdSteps []valueObject.MarketplaceItemInstallStep,
+	estimatedSizeBytes valueObject.Byte,
+	avatarUrl valueObject.Url,
+	screenshotUrls []valueObject.Url,
+) MarketplaceCatalogItem {
+	return MarketplaceCatalogItem{
+		Id:                 id,
+		Name:               itemName,
+		Type:               itemType,
+		Description:        description,
+		ServiceNames:       serviceNames,
+		Mappings:           mappings,
+		DataFields:         dataFields,
+		CmdSteps:           cmdSteps,
+		EstimatedSizeBytes: estimatedSizeBytes,
+		AvatarUrl:          avatarUrl,
+		ScreenshotUrls:     screenshotUrls,
+	}
+}
