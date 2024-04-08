9966482b1fbfa9744a28bcc4d80495c742e1685e
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-04-08 12:43:15 -0300

    feat: add isRequired and defaultValue to DataField

--- src/domain/valueObject/marketplaceItemDataField.go
+++ src/domain/valueObject/marketplaceItemDataField.go
@@ -1,42 +1,22 @@
 package valueObject
 
-import (
-	"gopkg.in/yaml.v3"
-)
-
 type MarketplaceItemDataField struct {
-	Key   DataFieldKey   `json:"key"`
-	Value DataFieldValue `json:"value"`
+	Key          DataFieldKey    `json:"key"`
+	Value        DataFieldValue  `json:"value"`
+	IsRequired   bool            `json:"isRequired"`
+	DefaultValue *DataFieldValue `json:"defaultValue,omitempty"`
 }
 
 func NewMarketplaceItemDataField(
 	key DataFieldKey,
 	value DataFieldValue,
+	isRequired bool,
+	defaultValue *DataFieldValue,
 ) MarketplaceItemDataField {
 	return MarketplaceItemDataField{
-		Key:   key,
-		Value: value,
+		Key:          key,
+		Value:        value,
+		IsRequired:   isRequired,
+		DefaultValue: defaultValue,
 	}
-}
-
-func (dfPtr *MarketplaceItemDataField) UnmarshalYAML(value *yaml.Node) error {
-	var valuesMap map[string]string
-	err := value.Decode(&valuesMap)
-	if err != nil {
-		return err
-	}
-
-	dfKey, err := NewDataFieldKey(valuesMap["key"])
-	if err != nil {
-		return err
-	}
-
-	dfValue, err := NewDataFieldValue(valuesMap["value"])
-	if err != nil {
-		return err
-	}
-
-	*dfPtr = NewMarketplaceItemDataField(dfKey, dfValue)
-
-	return nil
 }
