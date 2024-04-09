ea01383eaadbead0864ef9d2b6f1f4f48e059196
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-04-09 19:27:40 -0300

    fix: back to mappingPath instead mp

--- src/domain/valueObject/mappingPath.go
+++ src/domain/valueObject/mappingPath.go
@@ -10,26 +10,26 @@ const mappingPathRegex string = `^[^\s<>;'":#{}?\[\]]{1,512}$`
 type MappingPath string
 
 func NewMappingPath(value string) (MappingPath, error) {
-	mp := MappingPath(value)
-	if !mp.isValid() {
+	mappingPath := MappingPath(value)
+	if !mappingPath.isValid() {
 		return "", errors.New("InvalidMappingPath")
 	}
-	return mp, nil
+	return mappingPath, nil
 }
 
 func NewMappingPathPanic(value string) MappingPath {
-	mp, err := NewMappingPath(value)
+	mappingPath, err := NewMappingPath(value)
 	if err != nil {
 		panic(err)
 	}
-	return mp
+	return mappingPath
 }
 
-func (mp MappingPath) isValid() bool {
+func (mappingPath MappingPath) isValid() bool {
 	re := regexp.MustCompile(mappingPathRegex)
-	return re.MatchString(string(mp))
+	return re.MatchString(string(mappingPath))
 }
 
-func (mp MappingPath) String() string {
-	return string(mp)
+func (mappingPath MappingPath) String() string {
+	return string(mappingPath)
 }
