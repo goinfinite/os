b65ca38ae1c212b3004a15348ab81b135bab78ee
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-04-16 18:28:01 -0300

    feat: implements GetInstalledItemById and UninstallItem to DeleteMarketplaceInstalledItem UC

--- src/domain/useCase/deleteMarketplaceInstalledItem.go
+++ src/domain/useCase/deleteMarketplaceInstalledItem.go
@@ -1,6 +1,9 @@
 package useCase
 
 import (
+	"errors"
+	"log"
+
 	"github.com/speedianet/os/src/domain/repository"
 	"github.com/speedianet/os/src/domain/valueObject"
 )
@@ -10,5 +13,16 @@ func DeleteMarketplaceInstalledItem(
 	marketplaceCmdRepo repository.MarketplaceCmdRepo,
 	installedId valueObject.MarketplaceInstalledItemId,
 ) error {
+	_, err := marketplaceQueryRepo.GetInstalledItemById(installedId)
+	if err != nil {
+		return errors.New("MarketplaceInstalledItemNotFound")
+	}
+
+	err = marketplaceCmdRepo.UninstallItem(installedId)
+	if err != nil {
+		log.Printf("UninstallMarketplaceItemError: %s", err.Error())
+		return errors.New("UninstallMarketplaceItemInfraError")
+	}
+
 	return nil
 }
