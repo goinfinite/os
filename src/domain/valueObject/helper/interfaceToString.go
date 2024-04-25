b4a3a3d16906f4c6d1334e6e64a9a5b16d99568d
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-04-25 13:13:27 -0300

    fix: remove negative values validation from InterfaceToString helper

--- src/domain/valueObject/helper/interfaceToString.go
+++ src/domain/valueObject/helper/interfaceToString.go
@@ -15,18 +15,12 @@ func InterfaceToString(input interface{}) (string, error) {
 		output = v
 	case int, int8, int16, int32, int64:
 		intValue := reflect.ValueOf(v).Int()
-		if intValue < 0 {
-			err = defaultErr
-		}
 		output = strconv.FormatInt(intValue, 10)
 	case uint, uint8, uint16, uint32, uint64:
 		uintValue := reflect.ValueOf(v).Uint()
 		output = strconv.FormatUint(uintValue, 10)
 	case float32, float64:
 		floatValue := reflect.ValueOf(v).Float()
-		if floatValue < 0 {
-			err = defaultErr
-		}
 		output = strconv.FormatFloat(floatValue, 'f', -1, 64)
 	case bool:
 		boolValue := reflect.ValueOf(v).Bool()
