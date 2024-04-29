1ef4da3e07ebfd734ad0a4cc02f8bcfcdc98d15c
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-04-29 07:36:52 -0300

    refact: remove mapping methods from VhodCmdRepo

--- src/domain/repository/virtualHostCmdRepo.go
+++ src/domain/repository/virtualHostCmdRepo.go
@@ -3,13 +3,9 @@ package repository
 import (
 	"github.com/speedianet/os/src/domain/dto"
 	"github.com/speedianet/os/src/domain/entity"
-	"github.com/speedianet/os/src/domain/valueObject"
 )
 
 type VirtualHostCmdRepo interface {
 	Create(createDto dto.CreateVirtualHost) error
 	Delete(vhost entity.VirtualHost) error
-	DeleteMapping(mapping entity.Mapping) error
-	DeleteAutoMapping(serviceName valueObject.ServiceName) error
-	RecreateMapping(mapping entity.Mapping) error
 }
