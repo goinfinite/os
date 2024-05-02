2429f6cfe15b9aad220d8c99203dff365cf32325
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-05-02 16:26:28 -0300

    feat: add Label, HtmlType and Options attributes to MarketplaceCatalogItemDataField VO

--- src/domain/valueObject/marketplaceCatalogItemDataField.go
+++ src/domain/valueObject/marketplaceCatalogItemDataField.go
@@ -1,19 +1,28 @@
 package valueObject
 
 type MarketplaceCatalogItemDataField struct {
-	Key          DataFieldKey    `json:"key"`
-	DefaultValue *DataFieldValue `json:"defaultValue,omitempty"`
-	IsRequired   bool            `json:"isRequired"`
+	Key          DataFieldKey     `json:"key"`
+	Label        DataFieldLabel   `json:"label"`
+	HtmlType     HtmlType         `json:"type"`
+	DefaultValue *DataFieldValue  `json:"defaultValue,omitempty"`
+	Options      []DataFieldValue `json:"options"`
+	IsRequired   bool             `json:"isRequired"`
 }
 
 func NewMarketplaceCatalogItemDataField(
 	key DataFieldKey,
+	label DataFieldLabel,
+	htmlType HtmlType,
 	defaultValue *DataFieldValue,
+	options []DataFieldValue,
 	isRequired bool,
 ) (MarketplaceCatalogItemDataField, error) {
 	return MarketplaceCatalogItemDataField{
 		Key:          key,
+		Label:        label,
+		HtmlType:     htmlType,
 		DefaultValue: defaultValue,
+		Options:      options,
 		IsRequired:   isRequired,
 	}, nil
 }
