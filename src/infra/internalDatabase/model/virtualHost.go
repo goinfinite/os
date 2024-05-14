b45b424ee6f9d735a68382f9db107ade1d11c1c7
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-05-14 07:52:35 -0300

    feat: add ToEntity method to VirtualHost model

--- src/infra/internalDatabase/model/virtualHost.go
+++ src/infra/internalDatabase/model/virtualHost.go
@@ -1,5 +1,10 @@
 package dbModel
 
+import (
+	"github.com/speedianet/os/src/domain/entity"
+	"github.com/speedianet/os/src/domain/valueObject"
+)
+
 type VirtualHost struct {
 	ID             uint   `gorm:"primarykey"`
 	Hostname       string `gorm:"not null"`
@@ -7,4 +12,55 @@ type VirtualHost struct {
 	RootDirectory  string `gorm:"not null"`
 	ParentHostname *string
 	Mappings       []Mapping
+}
+
+func (model VirtualHost) ToEntity() (vhost entity.VirtualHost, err error) {
+	id, err := valueObject.NewVirtualHostId(model.ID)
+	if err != nil {
+		return vhost, err
+	}
+
+	hostname, err := valueObject.NewFqdn(model.Hostname)
+	if err != nil {
+		return vhost, err
+	}
+
+	vhostType, err := valueObject.NewVirtualHostType(model.Type)
+	if err != nil {
+		return vhost, err
+	}
+
+	rootDir, err := valueObject.NewUnixFilePath(model.RootDirectory)
+	if err != nil {
+		return vhost, err
+	}
+
+	var parentHostnamePtr *valueObject.Fqdn
+	if model.ParentHostname == nil {
+		parentHostname, err := valueObject.NewFqdn(*model.ParentHostname)
+		if err != nil {
+			return vhost, err
+		}
+		parentHostnamePtr = &parentHostname
+	}
+
+	mappings := []entity.Mapping{}
+	if len(model.Mappings) > 0 {
+		for _, mappingModel := range model.Mappings {
+			mapping, err := mappingModel.ToEntity()
+			if err != nil {
+				return vhost, err
+			}
+			mappings = append(mappings, mapping)
+		}
+	}
+
+	return entity.NewVirtualHost(
+		id,
+		hostname,
+		vhostType,
+		rootDir,
+		parentHostnamePtr,
+		mappings,
+	), nil
 }
