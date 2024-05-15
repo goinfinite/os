2c2f0aafe4cb1fbec4d9664cab9dc257fda3ea36
ntorga <northontorga@gmail.com>
2024-05-15 15:17:45 -0300

    fix: hide ip addr from dto

--- src/domain/dto/login.go
+++ src/domain/dto/login.go
@@ -5,7 +5,7 @@ import "github.com/speedianet/os/src/domain/valueObject"
 type Login struct {
 	Username  valueObject.Username  `json:"username"`
 	Password  valueObject.Password  `json:"password"`
-	IpAddress valueObject.IpAddress `json:"ipAddress"`
+	IpAddress valueObject.IpAddress `json:"-"`
 }
 
 func NewLogin(
