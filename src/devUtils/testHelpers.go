e32499716292e5321f7354da842d50abcbcb2d65
ntorga <northontorga@gmail.com>
2024-05-15 18:17:29 -0300

    fix: remove db svc due to import cycle on vos

--- src/devUtils/testHelpers.go
+++ src/devUtils/testHelpers.go
@@ -6,7 +6,6 @@ import (
 	"runtime"
 
 	"github.com/joho/godotenv"
-	internalDbInfra "github.com/speedianet/os/src/infra/internalDatabase"
 )
 
 func GenerateString(desiredSize int) string {
@@ -25,12 +24,4 @@ func LoadEnvVars() {
 	if loadEnvErr != nil {
 		panic("LoadingEnvFileError: " + loadEnvErr.Error())
 	}
-}
-
-func GetPersistentDbSvc() *internalDbInfra.PersistentDatabaseService {
-	persistentDbSvc, err := internalDbInfra.NewPersistentDatabaseService()
-	if err != nil {
-		panic("GetPersistentDbSvcError: " + err.Error())
-	}
-	return persistentDbSvc
 }
