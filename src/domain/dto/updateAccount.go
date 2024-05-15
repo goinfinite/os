bd43e6af6a74036c6dab6a3d95f10dcea4d62e9f
ntorga <northontorga@gmail.com>
2024-05-15 14:47:35 -0300

    fix: turn id optional on dto

--- src/domain/dto/updateAccount.go
+++ src/domain/dto/updateAccount.go
@@ -3,14 +3,14 @@ package dto
 import "github.com/speedianet/os/src/domain/valueObject"
 
 type UpdateAccount struct {
-	Id                 valueObject.AccountId `json:"id"`
-	Username           *valueObject.Username `json:"username"`
-	Password           *valueObject.Password `json:"password"`
-	ShouldUpdateApiKey *bool                 `json:"shouldUpdateApiKey"`
+	Id                 *valueObject.AccountId `json:"id"`
+	Username           *valueObject.Username  `json:"username"`
+	Password           *valueObject.Password  `json:"password"`
+	ShouldUpdateApiKey *bool                  `json:"shouldUpdateApiKey"`
 }
 
 func NewUpdateAccount(
-	id valueObject.AccountId,
+	id *valueObject.AccountId,
 	username *valueObject.Username,
 	password *valueObject.Password,
 	shouldUpdateApiKey *bool,
