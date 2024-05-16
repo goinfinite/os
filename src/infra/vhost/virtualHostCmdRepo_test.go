6999b186cc3a2cd279ea48ba1c7e36dd081d5355
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-05-16 18:15:46 -0300

    refact: remove test cases from virtual host infra and change to a TODO task

--- src/infra/vhost/virtualHostCmdRepo_test.go
+++ src/infra/vhost/virtualHostCmdRepo_test.go
@@ -2,60 +2,8 @@ package vhostInfra
 
 import (
 	"testing"
-
-	testHelpers "github.com/speedianet/os/src/devUtils"
-	"github.com/speedianet/os/src/domain/dto"
-	"github.com/speedianet/os/src/domain/valueObject"
 )
 
 func TestVirtualHostCmdRepo(t *testing.T) {
-	testHelpers.LoadEnvVars()
-	t.Run("CreateAlias", func(t *testing.T) {
-		parentDomain := valueObject.NewFqdnPanic("speedia.net")
-
-		createDto := dto.NewCreateVirtualHost(
-			valueObject.NewFqdnPanic("speedia.com"),
-			valueObject.NewVirtualHostTypePanic("alias"),
-			&parentDomain,
-		)
-
-		err := VirtualHostCmdRepo{}.Create(createDto)
-
-		if err != nil {
-			t.Errorf("ExpectedNoErrorButGot: %v", err)
-		}
-	})
-
-	t.Run("CreateTopLevel", func(t *testing.T) {
-		createDto := dto.NewCreateVirtualHost(
-			valueObject.NewFqdnPanic("speedia.org"),
-			valueObject.NewVirtualHostTypePanic("top-level"),
-			nil,
-		)
-
-		err := VirtualHostCmdRepo{}.Create(createDto)
-
-		if err != nil {
-			t.Errorf("ExpectedNoErrorButGot: %v", err)
-		}
-	})
-
-	t.Run("DeleteTopLevelAndAliases", func(t *testing.T) {
-		hostnames := []valueObject.Fqdn{
-			valueObject.NewFqdnPanic("speedia.com"),
-			valueObject.NewFqdnPanic("speedia.org"),
-		}
-
-		for _, hostname := range hostnames {
-			vhostEntity, err := VirtualHostQueryRepo{}.GetByHostname(hostname)
-			if err != nil {
-				t.Errorf("ExpectedNoErrorButGot: %v", err)
-			}
-
-			err = VirtualHostCmdRepo{}.Delete(vhostEntity)
-			if err != nil {
-				t.Errorf("ExpectedNoErrorButGot: %v", err)
-			}
-		}
-	})
+	// TODO: refact tests
 }
--- src/infra/vhost/virtualHostQueryRepo_test.go
+++ src/infra/vhost/virtualHostQueryRepo_test.go
@@ -2,22 +2,8 @@ package vhostInfra
 
 import (
 	"testing"
-
-	testHelpers "github.com/speedianet/os/src/devUtils"
 )
 
 func TestVirtualHostQueryRepo(t *testing.T) {
-	vhostQueryRepo := VirtualHostQueryRepo{}
-	testHelpers.LoadEnvVars()
-
-	t.Run("GetVirtualHosts", func(t *testing.T) {
-		vhosts, err := vhostQueryRepo.Get()
-		if err != nil {
-			t.Errorf("ExpectingNoErrorButGot: %v", err)
-		}
-
-		if len(vhosts) == 0 {
-			t.Errorf("ExpectingNonEmptySliceButGot: %v", vhosts)
-		}
-	})
+	// TODO: refact tests
 }
