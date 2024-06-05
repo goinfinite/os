68ec3b995696284254917cef97fec20cb5e4d72e
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-06-04 22:38:42 -0300

    feat: add virtualHostQueryRepo test case

--- src/infra/vhost/virtualHostQueryRepo_test.go
+++ src/infra/vhost/virtualHostQueryRepo_test.go
@@ -2,8 +2,50 @@ package vhostInfra
 
 import (
 	"testing"
+
+	testHelpers "github.com/speedianet/os/src/devUtils"
+	infraHelper "github.com/speedianet/os/src/infra/helper"
+	internalDbInfra "github.com/speedianet/os/src/infra/internalDatabase"
 )
 
 func TestVirtualHostQueryRepo(t *testing.T) {
-	// TODO: refact tests
+	testHelpers.LoadEnvVars()
+
+	persistentDbSvc, _ := internalDbInfra.NewPersistentDatabaseService()
+	vhostQueryRepo := NewVirtualHostQueryRepo(persistentDbSvc)
+
+	t.Run("Read", func(t *testing.T) {
+		vhosts, err := vhostQueryRepo.Read()
+		if err != nil {
+			t.Errorf("ExpectingNoErrorButGot: %v", err)
+		}
+
+		if len(vhosts) > 0 {
+			t.Errorf("ExpectingEmptySliceButGot: %v", len(vhosts))
+		}
+	})
+
+	t.Run("ReadByHostname", func(t *testing.T) {
+		hostname, _ := infraHelper.GetPrimaryVirtualHost()
+		vhost, err := vhostQueryRepo.ReadByHostname(hostname)
+		if err != nil && err.Error() != "VhostNotFound" {
+			t.Errorf("ExpectingNoErrorButGot: %v", err)
+		}
+
+		if vhost.Hostname != "" {
+			t.Errorf("ExpectingEmptyVhostButGot: %v", vhost)
+		}
+	})
+
+	t.Run("ReadAliasesByHostname", func(t *testing.T) {
+		hostname, _ := infraHelper.GetPrimaryVirtualHost()
+		aliases, err := vhostQueryRepo.ReadAliasesByHostname(hostname)
+		if err != nil && err.Error() != "VhostNotFound" {
+			t.Errorf("ExpectingNoErrorButGot: %v", err)
+		}
+
+		if len(aliases) > 0 {
+			t.Errorf("ExpectingEmptySliceButGot: %v", len(aliases))
+		}
+	})
 }
