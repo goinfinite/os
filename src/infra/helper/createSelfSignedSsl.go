517b136980a24d1213fe8e867aa95cddfd289b64
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-06-06 16:06:06 -0300

    feat: using conf file to generate self signed SSL with aliases

--- src/infra/helper/createSelfSignedSsl.go
+++ src/infra/helper/createSelfSignedSsl.go
@@ -1,15 +1,97 @@
 package infraHelper
 
 import (
-	"fmt"
+	"errors"
+	"os"
+	"strings"
+	"text/template"
 )
 
-func CreateSelfSignedSsl(dirPath string, virtualHost string) error {
+func selfSignedConfFileFactory(
+	virtualHost string,
+	aliases []string,
+) (string, error) {
+	altNames := []string{virtualHost}
+	altNames = append(altNames, aliases...)
+
+	valuesToInterpolate := map[string]interface{}{
+		"VirtualHost": virtualHost,
+		"AltNames":    altNames,
+	}
+
+	selfSignedConfFileTemplate := `[ req ]
+default_bits = 2048
+distinguished_name = req_distinguished_name
+x509_extensions = v3_req
+prompt = no
+
+[ req_distinguished_name ]
+C = US
+ST = California
+L = Los Angeles
+CN = {{ .VirtualHost }}
+
+[ v3_req ]
+subjectAltName = @alt_names
+
+[ alt_names ]
+{{- $lastDnsIndex := 0 }}
+{{- range $altName := .AltNames }}
+{{- $dnsIndex := increaseIndex $lastDnsIndex }}
+DNS.{{ $dnsIndex }} = {{ $altName }}
+{{- $wwwDnsIndex := increaseIndex $dnsIndex }}
+DNS.{{ $wwwDnsIndex }} = www.{{ $altName }}
+{{- $lastDnsIndex = $wwwDnsIndex }}
+{{- end }}
+`
+
+	selfSignedConfFileTemplatePtr := template.New("selfSignedConfFile")
+	selfSignedConfFileTemplatePtr = selfSignedConfFileTemplatePtr.Funcs(
+		template.FuncMap{
+			"increaseIndex": func(currentIndex int) int {
+				return currentIndex + 1
+			},
+		},
+	)
+
+	selfSignedConfFileTemplatePtr, err := selfSignedConfFileTemplatePtr.Parse(selfSignedConfFileTemplate)
+	if err != nil {
+		return "", errors.New("TemplateParsingError: " + err.Error())
+	}
+
+	var selfSignedConfFileContent strings.Builder
+	err = selfSignedConfFileTemplatePtr.Execute(
+		&selfSignedConfFileContent,
+		valuesToInterpolate,
+	)
+	if err != nil {
+		return "", errors.New("TemplateExecutionError: " + err.Error())
+	}
+
+	return selfSignedConfFileContent.String(), nil
+}
+
+func CreateSelfSignedSsl(
+	dirPath string,
+	virtualHost string,
+	aliases []string,
+) error {
+	selfSignedConfContent, err := selfSignedConfFileFactory(virtualHost, aliases)
+	if err != nil {
+		return errors.New("GenerateSelfSignedConfFileError: " + err.Error())
+	}
+
+	selfSignedConfFilePath := "/tmp/" + virtualHost + "_selfSignedSsl.conf"
+	shouldOverwrite := true
+	err = UpdateFile(selfSignedConfFilePath, selfSignedConfContent, shouldOverwrite)
+	if err != nil {
+		return errors.New("GenerateSelfSignedConfFileError: " + err.Error())
+	}
+
 	vhostCertKeyFilePath := dirPath + "/" + virtualHost + ".key"
 	vhostCertFilePath := dirPath + "/" + virtualHost + ".crt"
 
-	// TODO: how to generate a self signed with aliases
-	_, err := RunCmd(
+	_, err = RunCmd(
 		"openssl",
 		"req",
 		"-x509",
@@ -22,11 +104,16 @@ func CreateSelfSignedSsl(dirPath string, virtualHost string) error {
 		vhostCertKeyFilePath,
 		"-out",
 		vhostCertFilePath,
-		"-subj",
-		"/C=US/ST=California/L=LosAngeles/O=Acme/CN="+virtualHost,
+		"-config",
+		selfSignedConfFilePath,
 	)
 	if err != nil {
-		return fmt.Errorf("CreateSelfSignedSslFailed (%s): %s", virtualHost, err.Error())
+		return errors.New("CreateSelfSignedSslFailed (" + virtualHost + "): " + err.Error())
+	}
+
+	err = os.Remove(selfSignedConfFilePath)
+	if err != nil {
+		return errors.New("DeleteSelfSignedConfFileError: " + err.Error())
 	}
 
 	return nil
