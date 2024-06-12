4599c8b722740a33c9f92c8ec65a3e26d2abe684
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-06-12 19:07:12 -0300

    feat: add vhostCmdRepo infra test

---
+++ src/infra/vhost/virtualHostCmdRepo_test.go
@@ -0,0 +1,43 @@
+package vhostInfra
+
+import (
+	"testing"
+
+	testHelpers "github.com/speedianet/os/src/devUtils"
+	"github.com/speedianet/os/src/domain/dto"
+	"github.com/speedianet/os/src/domain/valueObject"
+	infraHelper "github.com/speedianet/os/src/infra/helper"
+	internalDbInfra "github.com/speedianet/os/src/infra/internalDatabase"
+)
+
+func TestVirtualHostCmdRepo(t *testing.T) {
+	testHelpers.LoadEnvVars()
+
+	persistentDbSvc, _ := internalDbInfra.NewPersistentDatabaseService()
+	vhostCmdRepo := NewVirtualHostCmdRepo(persistentDbSvc)
+	vhostQueryRepo := NewVirtualHostQueryRepo(persistentDbSvc)
+
+	vhostHostname, _ := infraHelper.GetPrimaryVirtualHost()
+
+	t.Run("Create", func(t *testing.T) {
+		vhostType := valueObject.NewVirtualHostTypePanic("top-level")
+		createDto := dto.NewCreateVirtualHost(vhostHostname, vhostType, nil)
+
+		err := vhostCmdRepo.Create(createDto)
+		if err != nil {
+			t.Errorf("ExpectingNoErrorButGot: %v", err)
+		}
+	})
+
+	t.Run("Delete", func(t *testing.T) {
+		vhost, err := vhostQueryRepo.ReadByHostname(vhostHostname)
+		if err != nil {
+			t.Errorf("ExpectingNoErrorButGot: %v", err)
+		}
+
+		err = vhostCmdRepo.Delete(vhost)
+		if err != nil {
+			t.Errorf("ExpectingNoErrorButGot: %v", err)
+		}
+	})
+}
