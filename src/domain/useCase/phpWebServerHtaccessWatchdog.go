7f9830e364fc25118044544795077385735c6d43
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-06-27 18:20:35 -0300

    feat: implements PhpWebServerHtaccessWatchdog use case using runtime query and cmd repository

--- src/domain/useCase/phpWebServerHtaccessWatchdog.go
+++ src/domain/useCase/phpWebServerHtaccessWatchdog.go
@@ -1,6 +1,10 @@
 package useCase
 
-import "github.com/speedianet/os/src/domain/repository"
+import (
+	"log"
+
+	"github.com/speedianet/os/src/domain/repository"
+)
 
 const PhpWebServerHtaccessValidationsPerHour int = 30
 
@@ -19,4 +23,13 @@ func NewPhpWebServerHtaccessWatchdog(
 	}
 }
 
-func (uc PhpWebServerHtaccessWatchdog) Execute() {}
+func (uc PhpWebServerHtaccessWatchdog) Execute() {
+	if !uc.runtimeQueryRepo.IsHtaccessModifiedRecently() {
+		return
+	}
+
+	err := uc.runtimeCmdRepo.RestartPhp()
+	if err != nil {
+		log.Print(err)
+	}
+}
