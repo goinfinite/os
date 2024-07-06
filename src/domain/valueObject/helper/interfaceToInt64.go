59e74539fc629b8e3e98d8063f9514e5ef318b37
ntorga <northontorga@gmail.com>
2024-07-06 17:58:10 -0300

    feat: add interface to int64 helper

---
+++ src/domain/valueObject/helper/interfaceToInt64.go
@@ -0,0 +1,32 @@
+package voHelper
+
+import (
+	"errors"
+	"reflect"
+	"strconv"
+)
+
+func InterfaceToInt64(input interface{}) (output int64, err error) {
+	var defaultErr error = errors.New("InvalidInput")
+	switch v := input.(type) {
+	case string:
+		output, err = strconv.ParseInt(v, 10, 64)
+	case int, int8, int16, int32, int64:
+		intValue := reflect.ValueOf(v).Int()
+		output = int64(intValue)
+	case uint, uint8, uint16, uint32, uint64:
+		uintValue := reflect.ValueOf(v).Uint()
+		output = int64(uintValue)
+	case float32, float64:
+		floatValue := reflect.ValueOf(v).Float()
+		output = int64(floatValue)
+	default:
+		err = defaultErr
+	}
+
+	if err != nil {
+		return 0, defaultErr
+	}
+
+	return output, nil
+}
