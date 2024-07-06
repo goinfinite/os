c613e773da2316db5593545896d3f47080432fdc
ntorga <northontorga@gmail.com>
2024-07-06 17:05:11 -0300

    fix: rename service to installed service and sync props

---
+++ src/domain/entity/installedService.go
@@ -0,0 +1,57 @@
+package entity
+
+import "github.com/speedianet/os/src/domain/valueObject"
+
+type InstalledService struct {
+	Name             valueObject.ServiceName    `json:"name"`
+	Nature           valueObject.ServiceNature  `json:"nature"`
+	Type             valueObject.ServiceType    `json:"type"`
+	Version          valueObject.ServiceVersion `json:"version"`
+	Command          valueObject.UnixCommand    `json:"command"`
+	Status           valueObject.ServiceStatus  `json:"status"`
+	Envs             []valueObject.ServiceEnv   `json:"envs"`
+	PortBindings     []valueObject.PortBinding  `json:"portBindings"`
+	StartupFile      *valueObject.UnixFilePath  `json:"startupFile"`
+	AutoStart        *bool                      `json:"autoStart"`
+	TimeoutStartSecs *uint                      `json:"timeoutStartSecs"`
+	AutoRestart      *bool                      `json:"autoRestart"`
+	MaxStartRetries  *uint                      `json:"maxStartRetries"`
+	CreatedAt        valueObject.UnixTime       `json:"createdAt"`
+	UpdatedAt        valueObject.UnixTime       `json:"updatedAt"`
+}
+
+func NewInstalledService(
+	name valueObject.ServiceName,
+	nature valueObject.ServiceNature,
+	serviceType valueObject.ServiceType,
+	version valueObject.ServiceVersion,
+	command valueObject.UnixCommand,
+	status valueObject.ServiceStatus,
+	envs []valueObject.ServiceEnv,
+	portBindings []valueObject.PortBinding,
+	startupFile *valueObject.UnixFilePath,
+	autoStart *bool,
+	timeoutStartSecs *uint,
+	autoRestart *bool,
+	maxStartRetries *uint,
+	createdAt valueObject.UnixTime,
+	updatedAt valueObject.UnixTime,
+) InstalledService {
+	return InstalledService{
+		Name:             name,
+		Nature:           nature,
+		Type:             serviceType,
+		Version:          version,
+		Command:          command,
+		Status:           status,
+		Envs:             envs,
+		PortBindings:     portBindings,
+		StartupFile:      startupFile,
+		AutoStart:        autoStart,
+		TimeoutStartSecs: timeoutStartSecs,
+		AutoRestart:      autoRestart,
+		MaxStartRetries:  maxStartRetries,
+		CreatedAt:        createdAt,
+		UpdatedAt:        updatedAt,
+	}
+}
--- src/infra/internalDatabase/model/installedService.go
+++ src/infra/internalDatabase/model/installedService.go
@@ -9,20 +9,20 @@ import (
 )
 
 type InstalledService struct {
-	Name            string `gorm:"primarykey;not null"`
-	Nature          string `gorm:"not null"`
-	Type            string `gorm:"not null"`
-	Version         string `gorm:"not null"`
-	Command         string `gorm:"not null"`
-	AutoStart       bool   `gorm:"not null"`
-	TimeoutStartSec uint   `gorm:"not null"`
-	AutoRestart     bool   `gorm:"not null"`
-	MaxStartRetries uint   `gorm:"not null"`
-	StartupFile     *string
-	Envs            *string
-	PortBindings    *string
-	CreatedAt       time.Time `gorm:"not null"`
-	UpdatedAt       time.Time `gorm:"not null"`
+	Name             string `gorm:"primarykey;not null"`
+	Nature           string `gorm:"not null"`
+	Type             string `gorm:"not null"`
+	Version          string `gorm:"not null"`
+	Command          string `gorm:"not null"`
+	Envs             *string
+	PortBindings     *string
+	StartupFile      *string
+	AutoStart        *bool
+	TimeoutStartSecs *uint
+	AutoRestart      *bool
+	MaxStartRetries  *uint
+	CreatedAt        time.Time `gorm:"not null"`
+	UpdatedAt        time.Time `gorm:"not null"`
 }
 
 func (InstalledService) TableName() string {
@@ -30,48 +30,56 @@ func (InstalledService) TableName() string {
 }
 
 func (InstalledService) InitialEntries() (entries []interface{}, err error) {
+	autoStart := true
+	timeoutStartSecs := uint(10)
+	autoRestart := true
+	maxStartRetries := uint(3)
+
 	osApiPortBindings := "1618/http"
 	osApiInstalledService := InstalledService{
-		Name:            "os-api",
-		Nature:          "solo",
-		Type:            "system",
-		Version:         infraEnvs.SpeediaOsVersion,
-		Command:         "/speedia/os serve",
-		AutoStart:       true,
-		TimeoutStartSec: 10,
-		AutoRestart:     true,
-		MaxStartRetries: 3,
-		StartupFile:     nil,
-		PortBindings:    &osApiPortBindings,
+		Name:             "os-api",
+		Nature:           "solo",
+		Type:             "system",
+		Version:          infraEnvs.SpeediaOsVersion,
+		Command:          "/speedia/os serve",
+		Envs:             nil,
+		PortBindings:     &osApiPortBindings,
+		StartupFile:      nil,
+		AutoStart:        &autoStart,
+		TimeoutStartSecs: &timeoutStartSecs,
+		AutoRestart:      &autoRestart,
+		MaxStartRetries:  &maxStartRetries,
 	}
 
 	cronInstalledService := InstalledService{
-		Name:            "cron",
-		Nature:          "solo",
-		Type:            "system",
-		Version:         "3.0",
-		Command:         "/usr/sbin/cron -f",
-		AutoStart:       true,
-		TimeoutStartSec: 10,
-		AutoRestart:     true,
-		MaxStartRetries: 3,
-		StartupFile:     nil,
-		PortBindings:    nil,
+		Name:             "cron",
+		Nature:           "solo",
+		Type:             "system",
+		Version:          "3.0",
+		Command:          "/usr/sbin/cron -f",
+		Envs:             nil,
+		PortBindings:     nil,
+		StartupFile:      nil,
+		AutoStart:        &autoStart,
+		TimeoutStartSecs: &timeoutStartSecs,
+		AutoRestart:      &autoRestart,
+		MaxStartRetries:  &maxStartRetries,
 	}
 
 	nginxPortBindings := "80/http;443/https"
 	nginxInstalledService := InstalledService{
-		Name:            "nginx",
-		Nature:          "solo",
-		Type:            "system",
-		Version:         "1.24.0",
-		Command:         "/usr/sbin/nginx",
-		AutoStart:       true,
-		TimeoutStartSec: 10,
-		AutoRestart:     true,
-		MaxStartRetries: 3,
-		StartupFile:     nil,
-		PortBindings:    &nginxPortBindings,
+		Name:             "nginx",
+		Nature:           "solo",
+		Type:             "system",
+		Version:          "1.24.0",
+		Command:          "/usr/sbin/nginx",
+		Envs:             nil,
+		PortBindings:     &nginxPortBindings,
+		StartupFile:      nil,
+		AutoStart:        &autoStart,
+		TimeoutStartSecs: &timeoutStartSecs,
+		AutoRestart:      &autoRestart,
+		MaxStartRetries:  &maxStartRetries,
 	}
 
 	return []interface{}{osApiInstalledService, cronInstalledService, nginxInstalledService}, nil
@@ -79,13 +87,8 @@ func (InstalledService) InitialEntries() (entries []interface{}, err error) {
 
 func NewInstalledService(
 	name, nature, serviceType, version, command string,
-	autoStart bool,
-	timeoutStartSec uint,
-	autoRestart bool,
-	maxStartRetries uint,
-	startupFile *string,
-	envs []valueObject.ServiceEnv,
-	portBindings []valueObject.PortBinding,
+	envs []valueObject.ServiceEnv, portBindings []valueObject.PortBinding, startupFile *string,
+	autoStart *bool, timeoutStartSecs *uint, autoRestart *bool, maxStartRetries *uint,
 ) InstalledService {
 	var envsPtr *string
 	if len(envs) > 0 {
@@ -108,17 +111,17 @@ func NewInstalledService(
 	}
 
 	return InstalledService{
-		Name:            name,
-		Nature:          nature,
-		Type:            serviceType,
-		Version:         version,
-		Command:         command,
-		AutoStart:       autoStart,
-		TimeoutStartSec: timeoutStartSec,
-		AutoRestart:     autoRestart,
-		MaxStartRetries: maxStartRetries,
-		StartupFile:     startupFile,
-		Envs:            envsPtr,
-		PortBindings:    portBindingsPtr,
+		Name:             name,
+		Nature:           nature,
+		Type:             serviceType,
+		Version:          version,
+		Command:          command,
+		Envs:             envsPtr,
+		PortBindings:     portBindingsPtr,
+		StartupFile:      startupFile,
+		AutoStart:        autoStart,
+		TimeoutStartSecs: timeoutStartSecs,
+		AutoRestart:      autoRestart,
+		MaxStartRetries:  maxStartRetries,
 	}
 }
