ff1cb0282035ceccf858808d786197cc10639266
ntorga <northontorga@gmail.com>
2024-07-06 17:58:19 -0300

    refactor: unix time vo

--- src/domain/valueObject/unixTime.go
+++ src/domain/valueObject/unixTime.go
@@ -1,25 +1,60 @@
 package valueObject
 
-import "time"
+import (
+	"errors"
+	"strconv"
+	"time"
+
+	voHelper "github.com/speedianet/os/src/domain/valueObject/helper"
+)
 
 type UnixTime int64
 
-func (ut UnixTime) Get() int64 {
-	return time.Unix(int64(ut), 0).UTC().Unix()
+func NewUnixTime(value interface{}) (UnixTime, error) {
+	unixTime, err := voHelper.InterfaceToInt64(value)
+	if err != nil {
+		return 0, errors.New("InvalidUnixTime")
+	}
+
+	return UnixTime(unixTime), nil
 }
 
-func (ut UnixTime) GetRfcDate() string {
-	return time.Unix(int64(ut), 0).UTC().Format(time.RFC3339)
+func NewUnixTimeNow() UnixTime {
+	return UnixTime(time.Now().Unix())
 }
 
-func (ut UnixTime) GetDateOnly() string {
-	return time.Unix(int64(ut), 0).UTC().Format("2006-01-02")
+func NewUnixTimeBeforeNow(duration time.Duration) UnixTime {
+	return UnixTime(time.Now().Add(-duration).Unix())
 }
 
-func (ut UnixTime) GetTimeOnly() string {
-	return time.Unix(int64(ut), 0).UTC().Format("15:04:05")
+func NewUnixTimeAfterNow(duration time.Duration) UnixTime {
+	return UnixTime(time.Now().Add(duration).Unix())
 }
 
-func (ut UnixTime) GetUnixTime() time.Time {
-	return time.Unix(int64(ut), 0)
+func NewUnixTimeWithGoTime(goTime time.Time) UnixTime {
+	return UnixTime(goTime.Unix())
+}
+
+func (vo UnixTime) Read() int64 {
+	return time.Unix(int64(vo), 0).UTC().Unix()
+}
+
+func (vo UnixTime) GetRfcDate() string {
+	return time.Unix(int64(vo), 0).UTC().Format(time.RFC3339)
+}
+
+func (vo UnixTime) GetDateOnly() string {
+	return time.Unix(int64(vo), 0).UTC().Format("2006-01-02")
+}
+
+func (vo UnixTime) GetTimeOnly() string {
+	return time.Unix(int64(vo), 0).UTC().Format("15:04:05")
+}
+
+func (vo UnixTime) GetAsGoTime() time.Time {
+	return time.Unix(int64(vo), 0).UTC()
+}
+
+func (vo UnixTime) String() string {
+	return strconv.FormatInt(int64(vo), 10)
 }
