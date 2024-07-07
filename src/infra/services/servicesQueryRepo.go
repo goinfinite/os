75040d54be15e3567942910d617499ada068eb66
ntorga <northontorga@gmail.com>
2024-07-07 11:32:15 -0300

    feat: impl read with metrics

--- src/infra/services/servicesQueryRepo.go
+++ src/infra/services/servicesQueryRepo.go
@@ -3,10 +3,15 @@ package servicesInfra
 import (
 	"errors"
 	"log"
+	"math"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/speedianet/os/src/domain/dto"
 	"github.com/speedianet/os/src/domain/entity"
 	"github.com/speedianet/os/src/domain/valueObject"
+	infraHelper "github.com/speedianet/os/src/infra/helper"
 	internalDbInfra "github.com/speedianet/os/src/infra/internalDatabase"
 	dbModel "github.com/speedianet/os/src/infra/internalDatabase/model"
 
@@ -70,50 +75,56 @@ func (repo *ServicesQueryRepo) ReadByName(
 	return serviceEntity, nil
 }
 
-func (repo *ServicesQueryRepo) getPpidEntireProcessFamily(
-	ppid int32,
-) ([]*process.Process, error) {
-	ppidProcesses := []*process.Process{}
+func (repo *ServicesQueryRepo) getPidProcessFamily(pid int32) ([]*process.Process, error) {
+	processFamily := []*process.Process{}
 
-	ppidProcess, err := process.NewProcess(ppid)
+	pidProcess, err := process.NewProcess(pid)
 	if err != nil {
-		return ppidProcesses, err
+		return processFamily, err
 	}
 
-	ppidProcesses = append(ppidProcesses, ppidProcess)
+	processFamily = append(processFamily, pidProcess)
 
-	childrenPidProcesses, _ := ppidProcess.Children()
-	if len(childrenPidProcesses) == 0 {
-		return ppidProcesses, nil
+	childrenPidProcesses, err := pidProcess.Children()
+	if err != nil || len(childrenPidProcesses) == 0 {
+		return processFamily, nil
 	}
 
 	for _, childPidProcess := range childrenPidProcesses {
-		grandChildrenPidProcesses, _ := repo.getPpidEntireProcessFamily(
+		grandChildrenPidProcesses, err := repo.getPidProcessFamily(
 			childPidProcess.Pid,
 		)
-		if len(grandChildrenPidProcesses) == 0 {
+		if err != nil || len(grandChildrenPidProcesses) == 0 {
 			continue
 		}
 
-		ppidProcesses = append(ppidProcesses, grandChildrenPidProcesses...)
+		processFamily = append(processFamily, grandChildrenPidProcesses...)
 	}
 
-	return ppidProcesses, nil
+	return processFamily, nil
 }
 
-func (repo *ServicesQueryRepo) getSupervisordServiceMetrics(
+func (repo *ServicesQueryRepo) getPidMetrics(
 	mainPid int32,
-	uptimeSecs int64,
-) (valueObject.ServiceMetrics, error) {
-	supervisordServiceMetrics := valueObject.ServiceMetrics{}
+) (serviceMetrics valueObject.ServiceMetrics, err error) {
+	pidProcesses, err := repo.getPidProcessFamily(mainPid)
+	if err != nil {
+		return serviceMetrics, err
+	}
+
+	if len(pidProcesses) == 0 {
+		return serviceMetrics, nil
+	}
+
+	uptimeMilliseconds, err := pidProcesses[0].CreateTime()
+	if err != nil {
+		return serviceMetrics, err
+	}
+	nowMilliseconds := time.Now().UTC().UnixMilli()
+	uptimeSecs := (nowMilliseconds - uptimeMilliseconds) / 1000
 
 	cpuPercent := float64(0.0)
 	memPercent := float32(0.0)
-
-	pidProcesses, err := repo.getPpidEntireProcessFamily(mainPid)
-	if err != nil {
-		return supervisordServiceMetrics, err
-	}
 
 	pids := []uint32{}
 	for _, process := range pidProcesses {
@@ -133,11 +144,11 @@ func (repo *ServicesQueryRepo) getSupervisordServiceMetrics(
 		pids = append(pids, uint32(process.Pid))
 	}
 
-	serviceMetrics := valueObject.NewServiceMetrics(
-		pids,
-		uptimeSecs,
-		cpuPercent,
-		memPercent,
+	cpuPercent = math.Round(cpuPercent*100) / 100
+	memPercent = float32(math.Round(float64(memPercent)*100) / 100)
+
+	serviceMetrics = valueObject.NewServiceMetrics(
+		pids, uptimeSecs, cpuPercent, memPercent,
 	)
 
 	return serviceMetrics, nil
@@ -145,6 +156,82 @@ func (repo *ServicesQueryRepo) getSupervisordServiceMetrics(
 
 func (repo *ServicesQueryRepo) ReadWithMetrics() ([]dto.InstalledServiceWithMetrics, error) {
 	servicesWithMetrics := []dto.InstalledServiceWithMetrics{}
+
+	servicesEntities, err := repo.Read()
+	if err != nil {
+		return servicesWithMetrics, err
+	}
+	serviceNameServiceEntityMap := map[string]entity.InstalledService{}
+	for _, serviceEntity := range servicesEntities {
+		serviceNameServiceEntityMap[serviceEntity.Name.String()] = serviceEntity
+	}
+
+	supervisorStatus, _ := infraHelper.RunCmd("supervisorctl", "status")
+	if len(supervisorStatus) == 0 {
+		return servicesWithMetrics, errors.New("GetSupervisorStatusError")
+	}
+
+	// # supervisorctl status
+	// cron                             RUNNING   pid 2, uptime 0:00:11
+	// nginx                            RUNNING   pid 24, uptime 0:00:10
+	// os-api                           RUNNING   pid 3, uptime 0:00:11
+	supervisorStatusLines := strings.Split(supervisorStatus, "\n")
+	if len(supervisorStatusLines) == 0 {
+		return servicesWithMetrics, errors.New("SupervisorStatusEmpty")
+	}
+
+	for _, supervisorStatusLine := range supervisorStatusLines {
+		if supervisorStatusLine == "" {
+			continue
+		}
+
+		supervisorStatusLineParts := strings.Fields(supervisorStatusLine)
+		if len(supervisorStatusLineParts) != 6 {
+			continue
+		}
+
+		rawServiceName := supervisorStatusLineParts[0]
+		serviceName, err := valueObject.NewServiceName(rawServiceName)
+		if err != nil {
+			continue
+		}
+
+		serviceEntity, exists := serviceNameServiceEntityMap[serviceName.String()]
+		if !exists {
+			continue
+		}
+
+		rawServiceStatus := supervisorStatusLineParts[1]
+		serviceStatus, err := valueObject.NewServiceStatus(rawServiceStatus)
+		if err != nil {
+			continue
+		}
+
+		if serviceStatus.String() != "running" {
+			serviceWithMetrics := dto.NewInstalledServiceWithMetrics(serviceEntity, nil)
+			servicesWithMetrics = append(servicesWithMetrics, serviceWithMetrics)
+			continue
+		}
+
+		rawServicePid := supervisorStatusLineParts[3]
+		rawServicePid = strings.Trim(rawServicePid, ",")
+		servicePidInt, err := strconv.ParseInt(rawServicePid, 10, 32)
+		if err != nil {
+			continue
+		}
+
+		serviceMetrics, err := repo.getPidMetrics(int32(servicePidInt))
+		if err != nil {
+			continue
+		}
+
+		serviceWithMetrics := dto.NewInstalledServiceWithMetrics(
+			serviceEntity, &serviceMetrics,
+		)
+
+		servicesWithMetrics = append(servicesWithMetrics, serviceWithMetrics)
+	}
+
 	return servicesWithMetrics, nil
 }
 
