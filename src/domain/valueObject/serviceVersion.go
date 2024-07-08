2cf44a98852e8d77652ea21d2411a9a7877f8141
ntorga <northontorga@gmail.com>
2024-07-08 12:44:13 -0300

    refact: service version vo

--- src/domain/valueObject/serviceVersion.go
+++ src/domain/valueObject/serviceVersion.go
@@ -3,38 +3,37 @@ package valueObject
 import (
 	"errors"
 	"regexp"
+	"strings"
+
+	voHelper "github.com/speedianet/os/src/domain/valueObject/helper"
 )
 
 const serviceVersionRegex string = `^([\w\_\.\-]{1,64}|[\d\.\_\-]{1,20}|latest|lts|alpha|beta)$`
 
 type ServiceVersion string
 
-func NewServiceVersion(value string) (ServiceVersion, error) {
-	version := ServiceVersion(value)
-	if !version.isValid() {
+func NewServiceVersion(value interface{}) (ServiceVersion, error) {
+	stringValue, err := voHelper.InterfaceToString(value)
+	if err != nil {
+		return "", errors.New("ServiceVersionMustBeString")
+	}
+
+	stringValue = strings.TrimSpace(stringValue)
+
+	re := regexp.MustCompile(serviceVersionRegex)
+	isValid := re.MatchString(stringValue)
+	if !isValid {
 		return "", errors.New("InvalidServiceVersion")
 	}
-	return version, nil
+
+	return ServiceVersion(stringValue), nil
 }
 
-func NewServiceVersionPanic(value string) ServiceVersion {
-	version, err := NewServiceVersion(value)
-	if err != nil {
-		panic(err)
-	}
-	return version
-}
-
-func (version ServiceVersion) isValid() bool {
-	re := regexp.MustCompile(serviceVersionRegex)
-	return re.MatchString(string(version))
-}
-
-func (version ServiceVersion) GetWithoutPunctuation() string {
+func (vo ServiceVersion) GetWithoutPunctuation() string {
 	re := regexp.MustCompile(`[\.\_\-]`)
-	return re.ReplaceAllString(string(version), "")
+	return re.ReplaceAllString(string(vo), "")
 }
 
-func (version ServiceVersion) String() string {
-	return string(version)
+func (vo ServiceVersion) String() string {
+	return string(vo)
 }
