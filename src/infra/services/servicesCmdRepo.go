2e0237c3604b879e1224f247ffa019a7e57ea1c0
ntorga <northontorga@gmail.com>
2024-07-08 16:02:17 -0300

    feat: impl create installable

--- src/infra/services/servicesCmdRepo.go
+++ src/infra/services/servicesCmdRepo.go
@@ -1,6 +1,11 @@
 package servicesInfra
 
 import (
+	"errors"
+	"strconv"
+	"strings"
+
+	"github.com/alessio/shellescape"
 	"github.com/speedianet/os/src/domain/dto"
 	"github.com/speedianet/os/src/domain/valueObject"
 	infraHelper "github.com/speedianet/os/src/infra/helper"
@@ -38,9 +43,120 @@ func (repo *ServicesCmdRepo) Reload() error {
 	return err
 }
 
+func (repo *ServicesCmdRepo) replaceCmdStepsPlaceholders(
+	cmdSteps []valueObject.UnixCommand,
+	placeholders map[string]string,
+) (finalCmdSteps []valueObject.UnixCommand, err error) {
+	for _, cmdStep := range cmdSteps {
+		cmdStepStr := cmdStep.String()
+		stepPlaceholders, _ := infraHelper.GetAllRegexGroupMatches(cmdStepStr, `%(.*?)%`)
+
+		for _, stepPlaceholder := range stepPlaceholders {
+			placeholderValue, exists := placeholders[stepPlaceholder]
+			if !exists {
+				return nil, errors.New("MissingPlaceholder: " + stepPlaceholder)
+			}
+
+			escapedPlaceholderValue := shellescape.Quote(placeholderValue)
+
+			cmdStepStr = strings.ReplaceAll(
+				cmdStepStr, "%"+stepPlaceholder+"%", escapedPlaceholderValue,
+			)
+		}
+
+		finalCmdStep, err := valueObject.NewUnixCommand(cmdStepStr)
+		if err != nil {
+			return nil, errors.New("InvalidCmdStep: " + cmdStepStr)
+		}
+
+		finalCmdSteps = append(finalCmdSteps, finalCmdStep)
+	}
+
+	return finalCmdSteps, nil
+}
+
 func (repo *ServicesCmdRepo) CreateInstallable(
 	createDto dto.CreateInstallableService,
 ) error {
+	servicesQueryRepo := NewServicesQueryRepo(repo.persistentDbSvc)
+	installableService, err := servicesQueryRepo.ReadInstallableByName(createDto.Name)
+	if err != nil {
+		return err
+	}
+
+	serviceVersion := installableService.Versions[0]
+	if createDto.Version != nil {
+		serviceVersion = *createDto.Version
+	}
+
+	primaryHostname, err := infraHelper.GetPrimaryVirtualHost()
+	if err != nil {
+		return err
+	}
+
+	stepsPlaceholders := map[string]string{
+		"randomPassword":  infraHelper.GenPass(16),
+		"version":         serviceVersion.String(),
+		"primaryHostname": primaryHostname.String(),
+	}
+
+	if createDto.StartupFile != nil {
+		stepsPlaceholders["startupFile"] = createDto.StartupFile.String()
+	}
+
+	finalInstallCmdSteps, err := repo.replaceCmdStepsPlaceholders(
+		installableService.InstallCmdSteps, stepsPlaceholders,
+	)
+	if err != nil {
+		return err
+	}
+
+	for stepIndex, cmdStep := range finalInstallCmdSteps {
+		_, err = infraHelper.RunCmdWithSubShell(cmdStep.String())
+		if err != nil {
+			stepIndexStr := strconv.Itoa(stepIndex)
+			return errors.New(
+				"RunCmdStepError (" + stepIndexStr + "): " + err.Error(),
+			)
+		}
+	}
+
+	finalCommand, err := repo.replaceCmdStepsPlaceholders(
+		[]valueObject.UnixCommand{installableService.Command}, stepsPlaceholders,
+	)
+	if err != nil {
+		return err
+	}
+
+	if len(createDto.PortBindings) == 0 {
+		createDto.PortBindings = installableService.PortBindings
+	}
+
+	installedServiceModel := dbModel.NewInstalledService(
+		createDto.Name.String(),
+		installableService.Nature.String(),
+		installableService.Type.String(),
+		serviceVersion.String(),
+		finalCommand[0].String(),
+		createDto.Envs,
+		createDto.PortBindings,
+		nil,
+		createDto.AutoStart,
+		createDto.TimeoutStartSecs,
+		createDto.AutoRestart,
+		createDto.MaxStartRetries,
+	)
+
+	if createDto.StartupFile != nil {
+		startupFileStr := createDto.StartupFile.String()
+		installedServiceModel.StartupFile = &startupFileStr
+	}
+
+	err = repo.persistentDbSvc.Handler.Create(&installedServiceModel).Error
+	if err != nil {
+		return err
+	}
+
 	return repo.Reload()
 }
 
