bad6041aa8d40b3db80cbca8802a2ec97845bc15
ntorga <northontorga@gmail.com>
2024-07-09 17:24:33 -0300

    fix: remove status update from uc

--- src/domain/useCase/updateService.go
+++ src/domain/useCase/updateService.go
@@ -5,41 +5,8 @@ import (
 	"log"
 
 	"github.com/speedianet/os/src/domain/dto"
-	"github.com/speedianet/os/src/domain/entity"
 	"github.com/speedianet/os/src/domain/repository"
 )
-
-func updateServiceStatus(
-	servicesQueryRepo repository.ServicesQueryRepo,
-	servicesCmdRepo repository.ServicesCmdRepo,
-	mappingCmdRepo repository.MappingCmdRepo,
-	serviceEntity entity.InstalledService,
-	updateDto dto.UpdateService,
-) error {
-	if serviceEntity.Status.String() == updateDto.Status.String() {
-		return nil
-	}
-
-	isInstalled := serviceEntity.Status.String() != "uninstalled"
-	if !isInstalled {
-		return errors.New("ServiceNotInstalled")
-	}
-
-	switch updateDto.Status.String() {
-	case "running":
-		return servicesCmdRepo.Start(updateDto.Name)
-	case "stopped":
-		return servicesCmdRepo.Stop(updateDto.Name)
-	case "uninstalled":
-		return DeleteService(
-			servicesQueryRepo, servicesCmdRepo, mappingCmdRepo, updateDto.Name,
-		)
-	case "restarting":
-		return servicesCmdRepo.Restart(updateDto.Name)
-	default:
-		return errors.New("UnknownServiceStatus")
-	}
-}
 
 func UpdateService(
 	servicesQueryRepo repository.ServicesQueryRepo,
@@ -53,35 +20,18 @@ func UpdateService(
 		return err
 	}
 
-	isSoloService := serviceEntity.Type.String() == "solo"
+	isSoloService := serviceEntity.Nature.String() == "solo"
+	isSystemService := serviceEntity.Type.String() == "system"
 	shouldUpdateStatus := updateDto.Status != nil
-	if isSoloService && !shouldUpdateStatus {
-		return errors.New("SoloServicesCanOnlyChangeStatus")
+	if (isSoloService || isSystemService) && !shouldUpdateStatus {
+		return errors.New("OnlyStatusUpdateAllowed")
 	}
 
-	if shouldUpdateStatus {
-		err = updateServiceStatus(
-			servicesQueryRepo,
-			servicesCmdRepo,
-			mappingCmdRepo,
-			serviceEntity,
-			updateDto,
+	shouldDelete := shouldUpdateStatus && updateDto.Status.String() == "uninstalled"
+	if shouldDelete {
+		return DeleteService(
+			servicesQueryRepo, servicesCmdRepo, mappingCmdRepo, updateDto.Name,
 		)
-		if err != nil {
-			log.Printf("UpdateServiceStatusError: %s", err.Error())
-			return errors.New("UpdateServiceStatusInfraError")
-		}
-	}
-
-	shouldUpdateType := updateDto.Type != nil
-	shouldUpdateCommand := updateDto.StartCmd != nil
-	shouldUpdateVersion := updateDto.Version != nil
-	shouldUpdateStartupFile := updateDto.StartupFile != nil
-	portBindingsChanged := len(updateDto.PortBindings) != 0
-	nothingElseChanged := !shouldUpdateType && !shouldUpdateCommand &&
-		!shouldUpdateVersion && !shouldUpdateStartupFile && !portBindingsChanged
-	if nothingElseChanged {
-		return nil
 	}
 
 	err = servicesCmdRepo.Update(updateDto)
