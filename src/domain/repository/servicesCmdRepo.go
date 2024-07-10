aaac4214e06f06d970da15bb10bd04061e764941
ntorga <northontorga@gmail.com>
2024-07-10 16:20:52 -0300

    fix: not use cmd methods

--- src/domain/repository/servicesCmdRepo.go
+++ src/domain/repository/servicesCmdRepo.go
@@ -8,9 +8,6 @@ import (
 type ServicesCmdRepo interface {
 	CreateInstallable(createDto dto.CreateInstallableService) (valueObject.ServiceName, error)
 	CreateCustom(createDto dto.CreateCustomService) error
-	Start(name valueObject.ServiceName) error
-	Stop(name valueObject.ServiceName) error
-	Restart(name valueObject.ServiceName) error
 	Update(updateDto dto.UpdateService) error
 	Delete(name valueObject.ServiceName) error
 }
