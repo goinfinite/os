ae1f2e839d6473ca78512519fe997f448db46d00
ntorga <northontorga@gmail.com>
2024-07-12 18:05:45 -0300

    feat: add service presentation layer

---
+++ src/presentation/api/helper/serviceResponseWrapper.go
@@ -0,0 +1,36 @@
+package apiHelper
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/speedianet/os/src/presentation/service"
+)
+
+type newFormattedResponse struct {
+	Status int         `json:"status"`
+	Body   interface{} `json:"body"`
+}
+
+func ServiceResponseWrapper(
+	c echo.Context,
+	serviceOutput service.ServiceOutput,
+) error {
+	responseStatus := http.StatusOK
+	switch serviceOutput.Status {
+	case service.Created:
+		responseStatus = http.StatusCreated
+	case service.MultiStatus:
+		responseStatus = http.StatusMultiStatus
+	case service.UserError:
+		responseStatus = http.StatusBadRequest
+	case service.InfraError:
+		responseStatus = http.StatusInternalServerError
+	}
+
+	formattedResponse := newFormattedResponse{
+		Status: responseStatus,
+		Body:   serviceOutput.Body,
+	}
+	return c.JSON(responseStatus, formattedResponse)
+}
---
+++ src/presentation/cli/helper/serviceResponseWrapper.go
@@ -0,0 +1,67 @@
+package cliHelper
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/alecthomas/chroma/formatters"
+	"github.com/alecthomas/chroma/lexers"
+	"github.com/alecthomas/chroma/styles"
+	"github.com/speedianet/os/src/presentation/service"
+	"golang.org/x/term"
+)
+
+func ServiceResponseWrapper(serviceOutput service.ServiceOutput) {
+	exitCode := 0
+	switch serviceOutput.Status {
+	case service.MultiStatus:
+		exitCode = 1
+	case service.UserError:
+		exitCode = 1
+	case service.InfraError:
+		exitCode = 1
+	}
+
+	stdoutFileDescriptor := int(os.Stdout.Fd())
+	isNonInteractiveSession := !term.IsTerminal(stdoutFileDescriptor)
+	if isNonInteractiveSession {
+		standardJsonBytes, err := json.Marshal(serviceOutput)
+		if err != nil {
+			fmt.Println("ResponseEncodingError")
+			os.Exit(1)
+		}
+
+		fmt.Println(string(standardJsonBytes))
+		os.Exit(exitCode)
+	}
+
+	prettyJsonBytes, err := json.MarshalIndent(serviceOutput, "", "  ")
+	if err != nil {
+		fmt.Println("PrettyResponseEncodingError")
+		os.Exit(1)
+	}
+
+	syntaxHighlightingLexer := lexers.Get("json")
+	if syntaxHighlightingLexer == nil {
+		syntaxHighlightingLexer = lexers.Fallback
+	}
+
+	shIterator, err := syntaxHighlightingLexer.Tokenise(nil, string(prettyJsonBytes))
+	if err != nil {
+		fmt.Println("SyntaxHighlightingTokenizingError")
+		os.Exit(1)
+	}
+
+	shFormatter := formatters.Get("terminal256")
+	if shFormatter == nil {
+		shFormatter = formatters.Fallback
+	}
+
+	err = shFormatter.Format(os.Stdout, styles.Vulcan, shIterator)
+	if err != nil {
+		fmt.Println("SyntaxHighlightingFormatError")
+		os.Exit(1)
+	}
+	fmt.Println()
+}
