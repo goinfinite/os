3af8946070b9b1ca567246c3096cd9dedf572a83
ntorga <northontorga@gmail.com>
2024-07-13 13:24:59 -0300

    refactor: fqdn vo

--- src/domain/valueObject/fqdn.go
+++ src/domain/valueObject/fqdn.go
@@ -4,36 +4,45 @@ import (
 	"errors"
 	"net"
 	"regexp"
+	"strings"
+
+	voHelper "github.com/speedianet/os/src/domain/valueObject/helper"
 )
 
 const fqdnRegex string = `^((\*\.)?([a-zA-Z0-9_]+[\w-]*\.)*)?([a-zA-Z0-9_][\w-]*[a-zA-Z0-9])\.([a-zA-Z]{2,})$`
 
 type Fqdn string
 
-func NewFqdn(value string) (Fqdn, error) {
-	fqdn := Fqdn(value)
-	if !fqdn.isValid() {
+func NewFqdn(value interface{}) (Fqdn, error) {
+	stringValue, err := voHelper.InterfaceToString(value)
+	if err != nil {
+		return "", errors.New("FqdnMustBeString")
+	}
+
+	stringValue = strings.ToLower(stringValue)
+
+	isIpAddress := net.ParseIP(stringValue) != nil
+	if isIpAddress {
+		return "", errors.New("FqdnCannotBeIpAddress")
+	}
+
+	re := regexp.MustCompile(fqdnRegex)
+	if !re.MatchString(stringValue) {
 		return "", errors.New("InvalidFqdn")
 	}
-	return fqdn, nil
+
+	return Fqdn(stringValue), nil
 }
 
-func NewFqdnPanic(value string) Fqdn {
-	fqdn := Fqdn(value)
-	if !fqdn.isValid() {
-		panic("InvalidFqdn")
+func NewFqdnPanic(value interface{}) Fqdn {
+	vo, err := NewFqdn(value)
+	if err != nil {
+		panic(err)
 	}
-	return fqdn
+
+	return vo
 }
 
-func (fqdn Fqdn) isValid() bool {
-	if net.ParseIP(string(fqdn)) != nil {
-		return false
-	}
-	re := regexp.MustCompile(fqdnRegex)
-	return re.MatchString(string(fqdn))
-}
-
-func (fqdn Fqdn) String() string {
-	return string(fqdn)
+func (vo Fqdn) String() string {
+	return string(vo)
 }
