6d10a7b2de7f7d0245626a6e39edc40c351cc719
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-07-17 17:11:57 -0300

    refact: remove useless panic constructor from AccountId VO

--- src/domain/valueObject/accountId.go
+++ src/domain/valueObject/accountId.go
@@ -10,26 +10,18 @@ import (
 type AccountId uint64
 
 func NewAccountId(value interface{}) (AccountId, error) {
-	accId, err := voHelper.InterfaceToUint64(value)
+	accountId, err := voHelper.InterfaceToUint64(value)
 	if err != nil {
-		return 0, errors.New("InvalidAccountId")
+		return 0, errors.New("AccountIdMustBeInt")
 	}
 
-	return AccountId(accId), nil
+	return AccountId(accountId), nil
 }
 
-func NewAccountIdPanic(value interface{}) AccountId {
-	accId, err := NewAccountId(value)
-	if err != nil {
-		panic(err)
-	}
-	return accId
+func (vo AccountId) Get() uint64 {
+	return uint64(vo)
 }
 
-func (id AccountId) Get() uint64 {
-	return uint64(id)
-}
-
-func (id AccountId) String() string {
-	return strconv.FormatUint(uint64(id), 10)
+func (vo AccountId) String() string {
+	return strconv.FormatUint(uint64(vo), 10)
 }
