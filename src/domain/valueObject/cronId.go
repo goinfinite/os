a0aa773524a326191da9a2a13d3ea5bcd2175de3
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-07-18 16:56:29 -0300

    refact: change CronId VO to use it new pattern

--- src/domain/valueObject/cronId.go
+++ src/domain/valueObject/cronId.go
@@ -2,55 +2,26 @@ package valueObject
 
 import (
 	"errors"
-	"reflect"
 	"strconv"
+
+	voHelper "github.com/speedianet/os/src/domain/valueObject/helper"
 )
 
 type CronId uint64
 
-func NewCronId(value interface{}) (CronId, error) {
-	var cronId uint64
-	var err error
-	switch v := value.(type) {
-	case string:
-		cronId, err = strconv.ParseUint(v, 10, 64)
-	case int, int8, int16, int32, int64:
-		intValue := reflect.ValueOf(v).Int()
-		if intValue < 0 {
-			err = errors.New("InvalidCronId")
-		}
-		cronId = uint64(intValue)
-	case uint, uint8, uint16, uint32, uint64:
-		cronId = uint64(reflect.ValueOf(v).Uint())
-	case float32, float64:
-		floatValue := reflect.ValueOf(v).Float()
-		if floatValue < 0 {
-			err = errors.New("InvalidCronId")
-		}
-		cronId = uint64(floatValue)
-	default:
-		err = errors.New("InvalidCronId")
-	}
-
+func NewCronId(value interface{}) (cronId CronId, err error) {
+	uintValue, err := voHelper.InterfaceToUint64(value)
 	if err != nil {
-		return 0, errors.New("InvalidCronId")
+		return cronId, errors.New("CronIdMustBeInt")
 	}
 
-	return CronId(cronId), nil
+	return CronId(uintValue), nil
 }
 
-func NewCronIdPanic(value interface{}) CronId {
-	cronId, err := NewCronId(value)
-	if err != nil {
-		panic(err)
-	}
-	return cronId
+func (vo CronId) Get() uint64 {
+	return uint64(vo)
 }
 
-func (id CronId) Get() uint64 {
-	return uint64(id)
-}
-
-func (id CronId) String() string {
-	return strconv.FormatUint(uint64(id), 10)
+func (vo CronId) String() string {
+	return strconv.FormatUint(uint64(vo), 10)
 }
