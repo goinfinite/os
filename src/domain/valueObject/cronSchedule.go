7bc725e2670b94f2aeca683ba544975b2742b0cd
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-07-18 15:45:27 -0300

    refact: change CronSchedule VO to use it new pattern

--- src/domain/valueObject/cronSchedule.go
+++ src/domain/valueObject/cronSchedule.go
@@ -4,46 +4,40 @@ import (
 	"errors"
 	"regexp"
 	"strings"
+
+	voHelper "github.com/speedianet/os/src/domain/valueObject/helper"
 )
 
 const cronScheduleRegex string = `^((?P<frequencyStr>(@(annually|yearly|monthly|weekly|daily|hourly|reboot))|(@every (\d+(ns|us|µs|ms|s|m|h))+)) ?|((?P<minute>(\d+,)+\d+|(\d+(\/|-)\d+)|\d+|\*|\*/\d+){1})(?: )((?P<hour>(\d+,)+\d+|(\d+(\/|-)\d+)|\d+|\*|\*/\d+){1})(?: )((?P<day>(\d+,)+\d+|(\d+(\/|-)\d+)|\d+|\*|\*/\d+){1})(?: )((?P<month>(\d+,)+\d+|(\d+(\/|-)\d+)|\d+|\*|\*/\d+){1})(?: )((?P<weekday>(\d+,)+\d+|(\d+(\/|-)\d+)|\d+|\*|\*/\d+){1})(?: )?)$`
 
 type CronSchedule string
 
-func NewCronSchedule(value string) (CronSchedule, error) {
-	if shouldHaveAtSign(value) {
-		hasAtSign := strings.HasPrefix(value, "@")
+func NewCronSchedule(value interface{}) (cronSchedule CronSchedule, err error) {
+	stringValue, err := voHelper.InterfaceToString(value)
+	if err != nil {
+		return cronSchedule, errors.New("CronScheduleMustBeString")
+	}
+
+	if shouldHaveAtSign(stringValue) {
+		hasAtSign := strings.HasPrefix(stringValue, "@")
 		if !hasAtSign {
-			value = "@" + value
+			stringValue = "@" + stringValue
 		}
 	}
 
-	schedule := CronSchedule(value)
-
-	if !schedule.isValid() {
-		return "", errors.New("InvalidCronSchedule")
+	re := regexp.MustCompile(cronScheduleRegex)
+	isValid := re.MatchString(stringValue)
+	if !isValid {
+		return cronSchedule, errors.New("InvalidCronSchedule")
 	}
 
-	return schedule, nil
-}
-
-func NewCronSchedulePanic(value string) CronSchedule {
-	schedule, err := NewCronSchedule(value)
-	if err != nil {
-		panic(err)
-	}
-	return schedule
+	return CronSchedule(stringValue), nil
 }
 
 func shouldHaveAtSign(value string) bool {
 	cronPredefinedScheduleRegex := `^((@?(annually|yearly|monthly|weekly|daily|hourly|reboot))|(@every (\d+(?:ns|us|µs|ms|s|m|h))+))$`
 	frequencyRegex := regexp.MustCompile(cronPredefinedScheduleRegex)
 	return frequencyRegex.MatchString(value)
-}
-
-func (schedule CronSchedule) isValid() bool {
-	scheduleRe := regexp.MustCompile(cronScheduleRegex)
-	return scheduleRe.MatchString(string(schedule))
 }
 
 func (schedule CronSchedule) String() string {
