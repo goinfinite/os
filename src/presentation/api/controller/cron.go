6729c6468609aa398d7fb0131872c583e6d99033
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-07-18 17:50:18 -0300

    refact: implements new Cron service layer to Cron API controller

---
+++ src/presentation/api/controller/cron.go
@@ -0,0 +1,93 @@
+package apiController
+
+import (
+	"github.com/labstack/echo/v4"
+	apiHelper "github.com/speedianet/os/src/presentation/api/helper"
+	"github.com/speedianet/os/src/presentation/service"
+)
+
+type CronController struct {
+	cronService service.CronService
+}
+
+func NewCronController() CronController {
+	return CronController{
+		cronService: service.NewCronService(),
+	}
+}
+
+// ReadCrons	 godoc
+// @Summary      ReadCrons
+// @Description  List crons.
+// @Tags         cron
+// @Accept       json
+// @Produce      json
+// @Security     Bearer
+// @Success      200 {array} entity.Cron
+// @Router       /v1/cron/ [get]
+func (controller CronController) Read(c echo.Context) error {
+	return apiHelper.ServiceResponseWrapper(c, controller.cronService.Read())
+}
+
+// CreateCron    godoc
+// @Summary      CreateNewCron
+// @Description  Create a new cron.
+// @Tags         cron
+// @Accept       json
+// @Produce      json
+// @Security     Bearer
+// @Param        createCronDto 	  body    dto.CreateCron  true  "comment is optional."
+// @Success      201 {object} object{} "CronCreated"
+// @Router       /v1/cron/ [post]
+func (controller CronController) Create(c echo.Context) error {
+	requestBody, err := apiHelper.ReadRequestBody(c)
+	if err != nil {
+		return err
+	}
+
+	return apiHelper.ServiceResponseWrapper(
+		c, controller.cronService.Create(requestBody),
+	)
+}
+
+// UpdateCron godoc
+// @Summary      UpdateCron
+// @Description  Update a cron.
+// @Tags         cron
+// @Accept       json
+// @Produce      json
+// @Security     Bearer
+// @Param        updateCronDto 	  body dto.UpdateCron  true  "Only id is required."
+// @Success      200 {object} object{} "CronUpdated message"
+// @Router       /v1/cron/ [put]
+func (controller CronController) Update(c echo.Context) error {
+	requestBody, err := apiHelper.ReadRequestBody(c)
+	if err != nil {
+		return err
+	}
+
+	return apiHelper.ServiceResponseWrapper(
+		c, controller.cronService.Update(requestBody),
+	)
+}
+
+// DeleteCron	 godoc
+// @Summary      DeleteCron
+// @Description  Delete a cron.
+// @Tags         cron
+// @Accept       json
+// @Produce      json
+// @Security     Bearer
+// @Param        cronId 	  path   string  true  "CronId to delete."
+// @Success      200 {object} object{} "CronDeleted"
+// @Router       /v1/cron/{cronId}/ [delete]
+func (controller CronController) Delete(c echo.Context) error {
+	requestBody := map[string]interface{}{
+		"id": c.Param("cronId"),
+	}
+
+	return apiHelper.ServiceResponseWrapper(
+		c, controller.cronService.Delete(requestBody),
+	)
+
+}
