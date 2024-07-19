9e62e29f05c3be006dfa6c5f32a60754b98c3a01
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-07-19 18:48:27 -0300

    refact: change DatabaseUsername VO to use it new pattern

--- src/domain/valueObject/databaseUsername.go
+++ src/domain/valueObject/databaseUsername.go
@@ -3,33 +3,30 @@ package valueObject
 import (
 	"errors"
 	"regexp"
+
+	voHelper "github.com/speedianet/os/src/domain/valueObject/helper"
 )
 
 const databaseUsernameRegex string = `^\w[\w-]+\w$`
 
 type DatabaseUsername string
 
-func NewDatabaseUsername(value string) (DatabaseUsername, error) {
-	dbUser := DatabaseUsername(value)
-	if !dbUser.isValid() {
-		return "", errors.New("InvalidDatabaseUsername")
+func NewDatabaseUsername(value interface{}) (
+	dbUsername DatabaseUsername, err error,
+) {
+	stringValue, err := voHelper.InterfaceToString(value)
+	if err != nil {
+		return dbUsername, errors.New("DatabaseUsernameMustBeString")
 	}
-	return dbUser, nil
-}
 
-func NewDatabaseUsernamePanic(value string) DatabaseUsername {
-	dbUser := DatabaseUsername(value)
-	if !dbUser.isValid() {
-		panic("InvalidDatabaseUsername")
-	}
-	return dbUser
-}
-
-func (dbUser DatabaseUsername) isValid() bool {
 	re := regexp.MustCompile(databaseUsernameRegex)
-	return re.MatchString(string(dbUser))
+	isValid := re.MatchString(stringValue)
+	if !isValid {
+		return dbUsername, errors.New("InvalidDatabaseUsername")
+	}
+	return DatabaseUsername(stringValue), nil
 }
 
-func (dbUser DatabaseUsername) String() string {
-	return string(dbUser)
+func (vo DatabaseUsername) String() string {
+	return string(vo)
 }
