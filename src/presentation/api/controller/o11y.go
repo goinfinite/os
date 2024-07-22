1380327ccbcb9d022173de9cb58314ae00b5ab0a
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-07-22 02:10:14 -0300

    refact: implements new O11y service layer to O11y API controller

---
+++ src/presentation/api/controller/o11y.go
@@ -0,0 +1,35 @@
+package apiController
+
+import (
+	"github.com/labstack/echo/v4"
+	internalDbInfra "github.com/speedianet/os/src/infra/internalDatabase"
+	apiHelper "github.com/speedianet/os/src/presentation/api/helper"
+	"github.com/speedianet/os/src/presentation/service"
+)
+
+type O11yController struct {
+	transientDbSvc *internalDbInfra.TransientDatabaseService
+	o11yService    *service.O11yService
+}
+
+func NewO11yController(
+	transientDbSvc *internalDbInfra.TransientDatabaseService,
+) *O11yController {
+	return &O11yController{
+		transientDbSvc: transientDbSvc,
+		o11yService:    service.NewO11yService(transientDbSvc),
+	}
+}
+
+// O11yOverview  godoc
+// @Summary      O11yOverview
+// @Description  Show system information and resource usage.
+// @Tags         o11y
+// @Accept       json
+// @Produce      json
+// @Security     Bearer
+// @Success      200 {object} entity.O11yOverview
+// @Router       /v1/o11y/overview/ [get]
+func (controller *O11yController) ReadOverview(c echo.Context) error {
+	return apiHelper.ServiceResponseWrapper(c, controller.o11yService.ReadOverview())
+}
