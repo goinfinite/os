1df0b6122b11495e3d35ad3b834edb4043a3770a
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-07-30 16:19:22 -0300

    feat: add parsers to parse settings and modules to service layer

--- src/presentation/service/runtime.go
+++ src/presentation/service/runtime.go
@@ -1,7 +1,9 @@
 package service
 
 import (
+	"errors"
 	"log"
+	"log/slog"
 
 	"github.com/speedianet/os/src/domain/dto"
 	"github.com/speedianet/os/src/domain/entity"
@@ -48,6 +50,87 @@ func (service *RuntimeService) ReadPhpConfigs(
 	return NewServiceOutput(Success, phpConfigs)
 }
 
+func (service *RuntimeService) parsePhpModules(
+	rawPhpModules interface{},
+) ([]entity.PhpModule, error) {
+	modules := []entity.PhpModule{}
+
+	rawModulesSlice, assertOk := rawPhpModules.([]interface{})
+	if !assertOk {
+		return modules, errors.New("InvalidPhpModules")
+	}
+
+	for _, rawModule := range rawModulesSlice {
+		rawModuleMap, assertOk := rawModule.(map[string]interface{})
+		if !assertOk {
+			slog.Debug("PhpModuleIsNotAnInterface")
+			continue
+		}
+
+		moduleName, err := valueObject.NewPhpModuleName(rawModuleMap["name"])
+		if err != nil {
+			slog.Debug("InvalidPhpModuleName", slog.Any("name", rawModuleMap["name"]))
+			continue
+		}
+
+		moduleStatus, assertOk := rawModuleMap["status"].(bool)
+		if !assertOk {
+			slog.Debug(
+				"InvalidPhpModuleStatus", slog.Any("status", rawModuleMap["status"]),
+			)
+			continue
+		}
+
+		modules = append(modules, entity.NewPhpModule(moduleName, moduleStatus))
+	}
+
+	return modules, nil
+}
+
+func (service *RuntimeService) parsePhpSettings(
+	rawPhpSettings interface{},
+) ([]entity.PhpSetting, error) {
+	settings := []entity.PhpSetting{}
+
+	rawSettingsSlice, assertOk := rawPhpSettings.([]interface{})
+	if !assertOk {
+		return settings, errors.New("InvalidPhpSettings")
+	}
+
+	for _, rawSetting := range rawSettingsSlice {
+		rawSettingMap, assertOk := rawSetting.(map[string]interface{})
+		if !assertOk {
+			slog.Debug("PhpSettingIsNotAnInterface")
+			continue
+		}
+
+		settingName, err := valueObject.NewPhpSettingName(rawSettingMap["name"])
+		if err != nil {
+			slog.Debug(
+				"InvalidPhpSettingName", slog.Any("name", rawSettingMap["name"]),
+			)
+			continue
+		}
+
+		settingValue, err := valueObject.NewPhpSettingValue(rawSettingMap["value"])
+		if err != nil {
+			slog.Debug(
+				"InvalidPhpSettingValue", slog.Any("value", rawSettingMap["value"]),
+			)
+			continue
+		}
+
+		settings = append(
+			settings,
+			entity.NewPhpSetting(
+				settingName, settingValue, []valueObject.PhpSettingOption{},
+			),
+		)
+	}
+
+	return settings, nil
+}
+
 func (service *RuntimeService) UpdatePhpConfigs(
 	input map[string]interface{},
 ) ServiceOutput {
@@ -71,20 +154,18 @@ func (service *RuntimeService) UpdatePhpConfigs(
 	}
 
 	phpModules := []entity.PhpModule{}
-	if _, exists := input["modules"]; exists {
-		var assertOk bool
-		phpModules, assertOk = input["modules"].([]entity.PhpModule)
-		if !assertOk {
-			return NewServiceOutput(UserError, "InvalidPhpModules")
+	if input["modules"] != nil {
+		phpModules, err = service.parsePhpModules(input["modules"])
+		if err != nil {
+			return NewServiceOutput(UserError, err.Error())
 		}
 	}
 
 	phpSettings := []entity.PhpSetting{}
-	if _, exists := input["settings"]; exists {
-		var assertOk bool
-		phpSettings, assertOk = input["settings"].([]entity.PhpSetting)
-		if !assertOk {
-			return NewServiceOutput(UserError, "InvalidPhpSettings")
+	if input["settings"] != nil {
+		phpSettings, err = service.parsePhpSettings(input["settings"])
+		if err != nil {
+			return NewServiceOutput(UserError, err.Error())
 		}
 	}
 
