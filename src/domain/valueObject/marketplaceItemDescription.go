504255962bf32a28745d83283622bc64569b7ccd
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-07-31 13:16:01 -0300

    refact: change MarketplaceItemDescription VO to use it new pattern and add missing unit tests

--- src/domain/valueObject/marketplaceItemDescription.go
+++ src/domain/valueObject/marketplaceItemDescription.go
@@ -2,31 +2,33 @@ package valueObject
 
 import (
 	"errors"
+
+	voHelper "github.com/speedianet/os/src/domain/valueObject/helper"
 )
 
 type MarketplaceItemDescription string
 
-func NewMarketplaceItemDescription(value string) (MarketplaceItemDescription, error) {
-	if len(value) < 2 {
+func NewMarketplaceItemDescription(value interface{}) (
+	marketplaceItemDescription MarketplaceItemDescription, err error,
+) {
+	stringValue, err := voHelper.InterfaceToString(value)
+	if err != nil {
+		return marketplaceItemDescription, errors.New(
+			"MarketplaceItemDescriptionMustBeString",
+		)
+	}
+
+	if len(stringValue) < 2 {
 		return "", errors.New("MarketplaceItemDescriptionTooSmall")
 	}
 
-	if len(value) > 2048 {
+	if len(stringValue) > 2048 {
 		return "", errors.New("MarketplaceItemDescriptionTooBig")
 	}
 
-	return MarketplaceItemDescription(value), nil
+	return MarketplaceItemDescription(stringValue), nil
 }
 
-func NewMarketplaceItemDescriptionPanic(value string) MarketplaceItemDescription {
-	mid, err := NewMarketplaceItemDescription(value)
-	if err != nil {
-		panic(err)
-	}
-
-	return mid
-}
-
-func (mid MarketplaceItemDescription) String() string {
-	return string(mid)
+func (vo MarketplaceItemDescription) String() string {
+	return string(vo)
 }
--- src/domain/valueObject/marketplaceItemDescription_test.go
+++ src/domain/valueObject/marketplaceItemDescription_test.go
@@ -1,12 +1,10 @@
 package valueObject
 
-import (
-	"testing"
-)
+import "testing"
 
 func TestMarketplaceItemDescription(t *testing.T) {
 	t.Run("ValidMarketplaceItemDescription", func(t *testing.T) {
-		validMarketplaceItemDescriptions := []string{
+		validMarketplaceItemDescriptions := []interface{}{
 			"Build and grow your website with the best way to WordPress. Lightning-fast hosting, intuitive, flexible editing, and everything you need to grow your site and audience, baked right in.",
 			"It's comprised of Elasticsearch, Kibana, Beats, and Logstash (also known as the ELK Stack) and more. Reliably and securely take data from any source, in any format, then search, analyze, and visualize.",
 			"RabbitMQ is a reliable and mature messaging and streaming broker, which is easy to deploy on cloud environments, on-premises, and on your local machine. It is currently used by millions worldwide.",
@@ -20,7 +18,7 @@ func TestMarketplaceItemDescription(t *testing.T) {
 	})
 
 	t.Run("InvalidMarketplaceItemDescription", func(t *testing.T) {
-		invalidMarketplaceItemDescriptions := []string{
+		invalidMarketplaceItemDescriptions := []interface{}{
 			"",
 			"a",
 		}
