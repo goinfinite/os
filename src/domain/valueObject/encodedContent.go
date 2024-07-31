480684bdb656d593545af59e5ba0bd7d7465110c
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-07-31 12:08:51 -0300

    refact: change EncodedContent VO to use it new pattern

--- src/domain/valueObject/encodedContent.go
+++ src/domain/valueObject/encodedContent.go
@@ -4,38 +4,34 @@ import (
 	"encoding/base64"
 	"errors"
 	"regexp"
+
+	voHelper "github.com/speedianet/os/src/domain/valueObject/helper"
 )
 
 const encodedContentRegexExpression = `^(?:[A-Za-z0-9+\/]{4})*(?:[A-Za-z0-9+\/]{4}|[A-Za-z0-9+\/]{3}=|[A-Za-z0-9+\/]{2}={2})$`
 
 type EncodedContent string
 
-func NewEncodedContent(value string) (EncodedContent, error) {
-	isEmpty := len(value) < 1
-	if isEmpty {
-		return "", errors.New("InvalidEncodedContent")
-	}
-
-	encodedBaseContentRegex := regexp.MustCompile(encodedContentRegexExpression)
-	isValid := encodedBaseContentRegex.MatchString(value)
-
-	if !isValid {
-		return "", errors.New("InvalidEncodedContent")
-	}
-
-	return EncodedContent(value), nil
-}
-
-func NewEncodedContentPanic(value string) EncodedContent {
-	encodedContent, err := NewEncodedContent(value)
+func NewEncodedContent(value interface{}) (encodedContent EncodedContent, err error) {
+	stringValue, err := voHelper.InterfaceToString(value)
 	if err != nil {
-		panic(err)
+		return encodedContent, errors.New("EncodedContentMustBeString")
 	}
-	return encodedContent
+
+	if len(stringValue) == 0 {
+		return encodedContent, errors.New("EncodedContentEmpty")
+	}
+
+	re := regexp.MustCompile(encodedContentRegexExpression)
+	if !re.MatchString(stringValue) {
+		return encodedContent, errors.New("InvalidEncodedContent")
+	}
+
+	return EncodedContent(stringValue), nil
 }
 
-func (encodedBaseContent EncodedContent) GetDecodedContent() (string, error) {
-	decodedContent, err := base64.StdEncoding.DecodeString(string(encodedBaseContent))
+func (vo EncodedContent) GetDecodedContent() (string, error) {
+	decodedContent, err := base64.StdEncoding.DecodeString(string(vo))
 	if err != nil {
 		return "", err
 	}
@@ -43,6 +39,6 @@ func (encodedBaseContent EncodedContent) GetDecodedContent() (string, error) {
 	return string(decodedContent), nil
 }
 
-func (encodedBaseContent EncodedContent) String() string {
-	return string(encodedBaseContent)
+func (vo EncodedContent) String() string {
+	return string(vo)
 }
