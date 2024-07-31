116bf69043838f8c20d7a7863d484feccb529f19
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-07-31 15:50:06 -0300

    refact: change PortBinding VO to use it new pattern and add missing unit tests

---
+++ src/domain/valueObject/portBinding_test.go
@@ -0,0 +1,31 @@
+package valueObject
+
+import "testing"
+
+func TestPortBinding(t *testing.T) {
+	t.Run("ValidPortBindings", func(t *testing.T) {
+		validPortBindings := []interface{}{
+			80, "80/http", "443/https", "3306/tcp", "8000",
+		}
+
+		for _, portBinding := range validPortBindings {
+			_, err := NewPortBinding(portBinding)
+			if err != nil {
+				t.Errorf("Expected no error for %s, got %v", portBinding, err)
+			}
+		}
+	})
+
+	t.Run("InvalidPortBindings", func(t *testing.T) {
+		invalidPortBindings := []interface{}{
+			"", "8000/",
+		}
+
+		for _, portBinding := range invalidPortBindings {
+			_, err := NewPortBinding(portBinding)
+			if err == nil {
+				t.Errorf("Expected error for %v, got nil", portBinding)
+			}
+		}
+	})
+}
