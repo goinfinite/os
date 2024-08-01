e9260a99038a41f12218d952628dc370a6b753d2
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-08-01 13:26:47 -0300

    feat: add ActivityRecordQueryRepo with Read method and unit test

---
+++ src/infra/activityRecord/activityRecordQueryRepo_test.go
@@ -0,0 +1,22 @@
+package infra
+
+import (
+	"testing"
+
+	testHelpers "github.com/speedianet/os/src/devUtils"
+	"github.com/speedianet/os/src/domain/dto"
+)
+
+func TestActivityRecordQueryRepo(t *testing.T) {
+	testHelpers.LoadEnvVars()
+	trailDbSvc := testHelpers.GetTrailDbSvc()
+	activityRecordQueryRepo := NewActivityRecordQueryRepo(trailDbSvc)
+
+	t.Run("ReadActivityRecordQuery", func(t *testing.T) {
+		readDto := dto.ReadActivityRecords{}
+		_, err := activityRecordQueryRepo.Read(readDto)
+		if err != nil {
+			t.Errorf("ExpectedNoErrorButGot: %v", err)
+		}
+	})
+}
