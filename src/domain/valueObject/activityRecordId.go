40d9baf1fc5bd2ff73af6b08c2bd698e85d03507
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-08-01 12:19:33 -0300

    feat: add ActivityRecordId VO with unit test

---
+++ src/domain/valueObject/activityRecordId.go
@@ -0,0 +1,29 @@
+package valueObject
+
+import (
+	"errors"
+	"strconv"
+
+	voHelper "github.com/speedianet/os/src/domain/valueObject/helper"
+)
+
+type ActivityRecordId uint64
+
+func NewActivityRecordId(value interface{}) (
+	activityRecordId ActivityRecordId, err error,
+) {
+	uintValue, err := voHelper.InterfaceToUint64(value)
+	if err != nil {
+		return activityRecordId, errors.New("InvalidActivityRecordId")
+	}
+
+	return ActivityRecordId(uintValue), nil
+}
+
+func (vo ActivityRecordId) Uint64() uint64 {
+	return uint64(vo)
+}
+
+func (vo ActivityRecordId) String() string {
+	return strconv.FormatUint(uint64(vo), 10)
+}
