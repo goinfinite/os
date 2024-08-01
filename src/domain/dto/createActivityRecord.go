8d33743b78d1916e7d266fc32c3c43ac491e379b
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-08-01 13:00:31 -0300

    feat: add CreateActivityRecord DTO

---
+++ src/domain/dto/createActivityRecord.go
@@ -0,0 +1,16 @@
+package dto
+
+import (
+	"github.com/speedianet/os/src/domain/valueObject"
+)
+
+type CreateActivityRecord struct {
+	Level             valueObject.ActivityRecordLevel    `json:"level"`
+	Code              *valueObject.ActivityRecordCode    `json:"code,omitempty"`
+	Message           *valueObject.ActivityRecordMessage `json:"message,omitempty"`
+	IpAddress         *valueObject.IpAddress             `json:"ipAddress,omitempty"`
+	OperatorAccountId *valueObject.AccountId             `json:"operatorAccountId,omitempty"`
+	TargetAccountId   *valueObject.AccountId             `json:"targetAccountId,omitempty"`
+	Username          *valueObject.Username              `json:"username,omitempty"`
+	MappingId         *valueObject.MappingId             `json:"mappingId,omitempty"`
+}
