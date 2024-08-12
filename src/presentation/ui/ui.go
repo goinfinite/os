b08ffd9828b9e002256e6f032b58a74d382f29d2
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-08-12 14:13:02 -0300

    refact: remove underscore from basePath in UI routes initialization and add authentication UI middleware

--- src/presentation/ui/ui.go
+++ src/presentation/ui/ui.go
@@ -2,13 +2,16 @@ package ui
 
 import (
 	"github.com/labstack/echo/v4"
+	uiMiddleware "github.com/speedianet/os/src/presentation/ui/middleware"
 )
 
 func UiInit(
 	e *echo.Echo,
 ) {
-	basePath := "/_"
+	basePath := ""
 	baseRoute := e.Group(basePath)
+
+	e.Use(uiMiddleware.Authentication())
 
 	router := NewRouter(baseRoute)
 	router.RegisterRoutes()
