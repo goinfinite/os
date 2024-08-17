8b1c928338da25cee8104826fceea58adae69716
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-08-16 21:10:32 -0300

    feat: add ReadWithMappings call to MappingsPresenter and send to page

--- src/presentation/ui/presenter/mappingsPresenter.go
+++ src/presentation/ui/presenter/mappingsPresenter.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/speedianet/os/src/domain/dto"
 	internalDbInfra "github.com/speedianet/os/src/infra/internalDatabase"
 	"github.com/speedianet/os/src/presentation/service"
 	uiHelper "github.com/speedianet/os/src/presentation/ui/helper"
@@ -23,6 +24,16 @@ func NewMappingsPresenter(
 }
 
 func (presenter *MappingsPresenter) Handler(c echo.Context) error {
-	pageContent := page.MappingsIndex()
+	responseOutput := presenter.virtualHostService.ReadWithMappings()
+	if responseOutput.Status != service.Success {
+		return nil
+	}
+
+	vhostsWithMappings, assertOk := responseOutput.Body.([]dto.VirtualHostWithMappings)
+	if !assertOk {
+		return nil
+	}
+
+	pageContent := page.MappingsIndex(vhostsWithMappings)
 	return uiHelper.Render(c, pageContent, http.StatusOK)
 }
