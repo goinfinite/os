4c2afb509be5137b1d0bcc96ca70372e1875be00
ntorga <northontorga@gmail.com>
2024-08-23 19:31:23 -0300

    fix: add jre as alias for java

--- src/domain/valueObject/serviceName.go
+++ src/domain/valueObject/serviceName.go
@@ -24,7 +24,7 @@ var NativeSvcNamesWithAliases = map[string][]string{
 	},
 	"postgresql": {"postgres"},
 	"redis":      {"redis-server"},
-	"java":       {"jdk", "openjdk"},
+	"java":       {"jre", "jdk", "openjdk"},
 }
 
 func NewServiceName(value interface{}) (serviceName ServiceName, err error) {
