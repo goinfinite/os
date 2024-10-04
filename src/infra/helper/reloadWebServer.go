20867e9d5ce878d733b310df13d407f57848eefa
ntorga <northontorga@gmail.com>
2024-10-04 18:13:19 -0300

    fix: improve nginx validation and use full path

--- src/infra/helper/reloadWebServer.go
+++ src/infra/helper/reloadWebServer.go
@@ -1,14 +1,29 @@
 package infraHelper
 
-import "errors"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 func ReloadWebServer() error {
-	_, err := RunCmdWithSubShell(
-		"nginx -t && nginx -s reload && sleep 2",
-	)
+	wsConfigTestResult, err := RunCmd("/usr/sbin/nginx", "-t")
+	if err != nil {
+		if wsConfigTestResult != "" {
+			return errors.New("NginxConfigTestFailed: " + err.Error())
+		}
+
+		if !strings.Contains(wsConfigTestResult, "test is successful") {
+			return errors.New("NginxConfigTestFailed: " + wsConfigTestResult)
+		}
+	}
+
+	_, err = RunCmd("/usr/sbin/nginx", "-s", "reload", "-c", "/etc/nginx/nginx.conf")
 	if err != nil {
 		return errors.New("NginxReloadFailed: " + err.Error())
 	}
+
+	time.Sleep(2 * time.Second)
 
 	return nil
 }
