19acef879d3f85c9e258f18bac229d420ed29b3f
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-10-08 15:03:12 -0300

    feat: add OperatorAccountId and OperatorIpAddress to CreateAccount DTO

--- src/domain/dto/createAccount.go
+++ src/domain/dto/createAccount.go
@@ -3,16 +3,22 @@ package dto
 import "github.com/speedianet/os/src/domain/valueObject"
 
 type CreateAccount struct {
-	Username valueObject.Username `json:"username"`
-	Password valueObject.Password `json:"password"`
+	Username          valueObject.Username  `json:"username"`
+	Password          valueObject.Password  `json:"password"`
+	OperatorAccountId valueObject.AccountId `json:"-"`
+	OperatorIpAddress valueObject.IpAddress `json:"-"`
 }
 
 func NewCreateAccount(
 	username valueObject.Username,
 	password valueObject.Password,
+	operatorAccountId valueObject.AccountId,
+	operatorIpAddress valueObject.IpAddress,
 ) CreateAccount {
 	return CreateAccount{
-		Username: username,
-		Password: password,
+		Username:          username,
+		Password:          password,
+		OperatorAccountId: operatorAccountId,
+		OperatorIpAddress: operatorIpAddress,
 	}
 }
