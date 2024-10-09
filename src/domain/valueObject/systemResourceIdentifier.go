1171dba1b494ac1927a2c38f3e3d0463f6948317
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-10-08 21:25:39 -0300

    feat: add SystemResourceIdentifier VO and unit test

---
+++ src/domain/valueObject/systemResourceIdentifier.go
@@ -0,0 +1,50 @@
+package valueObject
+
+import (
+	"errors"
+	"log/slog"
+	"regexp"
+
+	voHelper "github.com/speedianet/os/src/domain/valueObject/helper"
+)
+
+const systemResourceIdentifierRegex string = `^sri://(?P<accountId>[\d]{1,64}):(?P<resourceType>[\w\_\-]{2,64})\/(?P<resourceId>[\w\_\.\-]{2,256}|\*)$`
+
+type SystemResourceIdentifier string
+
+func NewSystemResourceIdentifier(
+	value interface{},
+) (sri SystemResourceIdentifier, err error) {
+	stringValue, err := voHelper.InterfaceToString(value)
+	if err != nil {
+		return sri, errors.New("SystemResourceIdentifierMustBeString")
+	}
+
+	re := regexp.MustCompile(systemResourceIdentifierRegex)
+	if !re.MatchString(stringValue) {
+		return sri, errors.New("InvalidSystemResourceIdentifier")
+	}
+
+	return SystemResourceIdentifier(stringValue), nil
+}
+
+func NewSystemResourceIdentifierIgnoreError(value interface{}) SystemResourceIdentifier {
+	sri, err := NewSystemResourceIdentifier(value)
+	if err != nil {
+		panicMessage := "UnexpectedSystemResourceIdentifierCreationError"
+		slog.Debug(panicMessage, slog.Any("value", value), slog.Any("error", err))
+		panic(panicMessage)
+	}
+
+	return sri
+}
+
+func NewAccountSri(accountId AccountId) SystemResourceIdentifier {
+	return NewSystemResourceIdentifierIgnoreError(
+		"sri://0:account/" + accountId.String(),
+	)
+}
+
+func (vo SystemResourceIdentifier) String() string {
+	return string(vo)
+}
