de10debb3579b312e6a605cf6654f710cacda97a
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-10-14 20:11:20 -0300

    feat: using authentication service on authentication API controller

--- src/presentation/api/controller/authentication.go
+++ src/presentation/api/controller/authentication.go
@@ -1,32 +1,24 @@
 package apiController
 
 import (
-	"net/http"
-
-	"github.com/goinfinite/os/src/domain/dto"
-	"github.com/goinfinite/os/src/domain/useCase"
-	"github.com/goinfinite/os/src/domain/valueObject"
-	accountInfra "github.com/goinfinite/os/src/infra/account"
-	activityRecordInfra "github.com/goinfinite/os/src/infra/activityRecord"
-	authInfra "github.com/goinfinite/os/src/infra/auth"
 	internalDbInfra "github.com/goinfinite/os/src/infra/internalDatabase"
 	apiHelper "github.com/goinfinite/os/src/presentation/api/helper"
-	serviceHelper "github.com/goinfinite/os/src/presentation/service/helper"
+	"github.com/goinfinite/os/src/presentation/service"
 	"github.com/labstack/echo/v4"
 )
 
-type AuthController struct {
-	persistentDbSvc *internalDbInfra.PersistentDatabaseService
-	trailDbSvc      *internalDbInfra.TrailDatabaseService
+type AuthenticationController struct {
+	authenticationService *service.AuthenticationService
 }
 
-func NewAuthController(
+func NewAuthenticationController(
 	persistentDbSvc *internalDbInfra.PersistentDatabaseService,
 	trailDbSvc *internalDbInfra.TrailDatabaseService,
-) *AuthController {
-	return &AuthController{
-		persistentDbSvc: persistentDbSvc,
-		trailDbSvc:      trailDbSvc,
+) *AuthenticationController {
+	return &AuthenticationController{
+		authenticationService: service.NewAuthenticationService(
+			persistentDbSvc, trailDbSvc,
+		),
 	}
 }
 
@@ -40,53 +32,13 @@ func NewAuthController(
 // @Success      200 {object} entity.AccessToken
 // @Failure      401 {object} string
 // @Router       /v1/auth/login/ [post]
-func (controller *AuthController) Login(c echo.Context) error {
+func (controller *AuthenticationController) Login(c echo.Context) error {
 	requestBody, err := apiHelper.ReadRequestBody(c)
 	if err != nil {
 		return err
 	}
 
-	requiredParams := []string{"username", "password"}
-	err = serviceHelper.RequiredParamsInspector(requestBody, requiredParams)
-	if err != nil {
-		return apiHelper.ResponseWrapper(c, http.StatusBadRequest, err.Error())
-	}
-
-	username, err := valueObject.NewUsername(requestBody["username"])
-	if err != nil {
-		return apiHelper.ResponseWrapper(c, http.StatusBadRequest, err.Error())
-	}
-
-	password, err := valueObject.NewPassword(requestBody["password"])
-	if err != nil {
-		return apiHelper.ResponseWrapper(c, http.StatusBadRequest, err.Error())
-	}
-
-	operatorIpAddress, err := valueObject.NewIpAddress(requestBody["operatorIpAddress"])
-	if err != nil {
-		return apiHelper.ResponseWrapper(c, http.StatusBadRequest, err.Error())
-	}
-
-	dto := dto.NewCreateSessionToken(username, password, operatorIpAddress)
-
-	authQueryRepo := authInfra.NewAuthQueryRepo(controller.persistentDbSvc)
-	authCmdRepo := authInfra.AuthCmdRepo{}
-	accountQueryRepo := accountInfra.NewAccountQueryRepo(controller.persistentDbSvc)
-	activityRecordQueryRepo := activityRecordInfra.NewActivityRecordQueryRepo(
-		controller.trailDbSvc,
+	return apiHelper.ServiceResponseWithIgnoreToastWrapper(
+		c, controller.authenticationService.Login(requestBody),
 	)
-	activityRecordCmdRepo := activityRecordInfra.NewActivityRecordCmdRepo(
-		controller.trailDbSvc,
-	)
-
-	accessToken, err := useCase.CreateSessionToken(
-		authQueryRepo, authCmdRepo, accountQueryRepo, activityRecordQueryRepo,
-		activityRecordCmdRepo, dto,
-	)
-	if err != nil {
-		return apiHelper.ResponseWrapper(c, http.StatusUnauthorized, err.Error())
-	}
-
-	c.Response().Header().Set("X-Ignore-Toast", "true")
-	return apiHelper.ResponseWrapper(c, http.StatusOK, accessToken)
 }
