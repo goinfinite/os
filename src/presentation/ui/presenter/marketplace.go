bf9eeaf0e1234d2b89dd98c77d96f839b60d4a7d
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-10-15 18:49:33 -0300

    feat: add readMarketplaceOverviewByType on presenter and provide to marketplace templ

--- src/presentation/ui/presenter/marketplace.go
+++ src/presentation/ui/presenter/marketplace.go
@@ -1,6 +1,9 @@
 package presenter
 
 import (
+	"errors"
+	"log"
+	"log/slog"
 	"net/http"
 
 	"github.com/goinfinite/os/src/domain/entity"
@@ -8,6 +11,8 @@ import (
 	"github.com/goinfinite/os/src/presentation/service"
 	uiHelper "github.com/goinfinite/os/src/presentation/ui/helper"
 	"github.com/goinfinite/os/src/presentation/ui/page"
+	presenterDto "github.com/goinfinite/os/src/presentation/ui/presenter/dto"
+	presenterValueObject "github.com/goinfinite/os/src/presentation/ui/presenter/valueObject"
 	"github.com/labstack/echo/v4"
 )
 
@@ -23,17 +28,60 @@ func NewMarketplacePresenter(
 	}
 }
 
+func (presenter *MarketplacePresenter) readMarketplaceOverviewByType(
+	listType presenterValueObject.MarketplaceListType,
+) (overview presenterDto.MarketplaceOverview, err error) {
+	var assertOk bool
+
+	installedItemsList := []entity.MarketplaceInstalledItem{}
+	if listType.String() == "installed" {
+		responseOutput := presenter.marketplaceService.ReadInstalledItems()
+		if responseOutput.Status != service.Success {
+			return overview, errors.New("FailedToReadInstalledItems")
+		}
+
+		installedItemsList, assertOk = responseOutput.Body.([]entity.MarketplaceInstalledItem)
+		if !assertOk {
+			return overview, errors.New("FailedToReadInstalledItems")
+		}
+	}
+
+	catalogItemsList := []entity.MarketplaceCatalogItem{}
+	if listType.String() == "catalog" {
+		responseOutput := presenter.marketplaceService.ReadCatalog()
+		if responseOutput.Status != service.Success {
+			return overview, errors.New("FailedToReadCatalogItems")
+		}
+
+		catalogItemsList, assertOk = responseOutput.Body.([]entity.MarketplaceCatalogItem)
+		if !assertOk {
+			return overview, errors.New("FailedToReadCatalogItems")
+		}
+	}
+
+	return presenterDto.NewMarketplaceOverview(
+		listType, installedItemsList, catalogItemsList,
+	), nil
+}
+
 func (presenter *MarketplacePresenter) Handler(c echo.Context) error {
-	responseOutput := presenter.marketplaceService.ReadInstalledItems()
-	if responseOutput.Status != service.Success {
+	rawListType := "installed"
+	if c.QueryParam("listType") != "" {
+		rawListType = c.QueryParam("listType")
+	}
+	listType, err := presenterValueObject.NewMarketplaceListType(rawListType)
+	if err != nil {
+		slog.Error(err.Error(), slog.Any("rawListType", rawListType))
 		return nil
 	}
 
-	installedItems, assertOk := responseOutput.Body.([]entity.MarketplaceInstalledItem)
-	if !assertOk {
+	marketplaceOverview, err := presenter.readMarketplaceOverviewByType(listType)
+	if err != nil {
 		return nil
 	}
 
-	pageContent := page.MarketplaceIndex(installedItems)
+	log.Print(marketplaceOverview)
+
+	pageContent := page.MarketplaceIndex(marketplaceOverview)
 	return uiHelper.Render(c, pageContent, http.StatusOK)
 }
