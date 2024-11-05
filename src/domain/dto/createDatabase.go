b583b65a28f50e30442aeb8ca76f688990c9f8fa
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-11-04 22:54:22 -0300

    refact: remove DatabaseType from CreateDatabase DTO

--- src/domain/dto/createDatabase.go
+++ src/domain/dto/createDatabase.go
@@ -4,14 +4,12 @@ import "github.com/goinfinite/os/src/domain/valueObject"
 
 type CreateDatabase struct {
 	DatabaseName      valueObject.DatabaseName `json:"dbName"`
-	DatabaseType      valueObject.DatabaseType `json:"dbType"`
 	OperatorAccountId valueObject.AccountId    `json:"-"`
 	OperatorIpAddress valueObject.IpAddress    `json:"-"`
 }
 
 func NewCreateDatabase(
 	dbName valueObject.DatabaseName,
-	dbType valueObject.DatabaseType,
 	operatorAccountId valueObject.AccountId,
 	operatorIpAddress valueObject.IpAddress,
 ) CreateDatabase {
