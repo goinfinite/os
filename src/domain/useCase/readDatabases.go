19aa374284b8249ddb0a4edaec996b46962b3ef1
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-11-13 17:49:38 -0300

    refact: return infra error when read throw an error

--- src/domain/useCase/readDatabases.go
+++ src/domain/useCase/readDatabases.go
@@ -1,12 +1,21 @@
 package useCase
 
 import (
+	"errors"
+	"log/slog"
+
 	"github.com/goinfinite/os/src/domain/entity"
 	"github.com/goinfinite/os/src/domain/repository"
 )
 
 func ReadDatabases(
 	databaseQueryRepo repository.DatabaseQueryRepo,
-) ([]entity.Database, error) {
-	return databaseQueryRepo.Read()
+) (databasesList []entity.Database, err error) {
+	databasesList, err = databaseQueryRepo.Read()
+	if err != nil {
+		slog.Error("ReadDatabasesError", slog.Any("err", err))
+		return databasesList, errors.New("ReadDatabasesInfraError")
+	}
+
+	return databasesList, nil
 }
