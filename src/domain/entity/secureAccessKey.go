ba295c96860477d5ad7212c5ad827c363106753d
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-11-21 18:33:40 -0300

    feat: add HashId to SecureAccessKey entity

--- src/domain/entity/secureAccessKey.go
+++ src/domain/entity/secureAccessKey.go
@@ -3,15 +3,18 @@ package entity
 import "github.com/goinfinite/os/src/domain/valueObject"
 
 type SecureAccessKey struct {
-	Name    valueObject.SecureAccessKeyName    `json:"secureAccessKeyName"`
-	Content valueObject.SecureAccessKeyContent `json:"secureAccessKeyContent"`
+	HashId  valueObject.Hash                   `json:"hashId"`
+	Name    valueObject.SecureAccessKeyName    `json:"name"`
+	Content valueObject.SecureAccessKeyContent `json:"-"`
 }
 
 func NewSecureAccessKey(
+	hashId valueObject.Hash,
 	name valueObject.SecureAccessKeyName,
 	content valueObject.SecureAccessKeyContent,
 ) SecureAccessKey {
 	return SecureAccessKey{
+		HashId:  hashId,
 		Name:    name,
 		Content: content,
 	}
