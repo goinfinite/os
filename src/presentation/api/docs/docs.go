7c9ffff4f447c92c16601e0edffdf3bbce96ee8f
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-11-21 14:22:29 -0300

    chore: update swagger

--- src/presentation/api/docs/docs.go
+++ src/presentation/api/docs/docs.go
@@ -3631,7 +3631,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "0.1.2",
+	Version:          "0.1.4",
 	Host:             "localhost:1618",
 	BasePath:         "/api",
 	Schemes:          []string{},
