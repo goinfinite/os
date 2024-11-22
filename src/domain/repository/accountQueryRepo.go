48fba90b5b4ce8e1d06b1f6e0ba1e9b383e3d8ca
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-11-22 11:02:28 -0300

    feat: add ReadSecureAccessKeys to AccountQueryRepo infra

--- src/domain/repository/accountQueryRepo.go
+++ src/domain/repository/accountQueryRepo.go
@@ -9,4 +9,7 @@ type AccountQueryRepo interface {
 	Read() ([]entity.Account, error)
 	ReadByUsername(username valueObject.Username) (entity.Account, error)
 	ReadById(accountId valueObject.AccountId) (entity.Account, error)
+	ReadSecureAccessKeys(
+		accountId valueObject.AccountId,
+	) ([]entity.SecureAccessKey, error)
 }
--- src/infra/account/accountQueryRepo.go
+++ src/infra/account/accountQueryRepo.go
@@ -3,9 +3,12 @@ package accountInfra
 import (
 	"errors"
 	"log/slog"
+	"os"
+	"strings"
 
 	"github.com/goinfinite/os/src/domain/entity"
 	"github.com/goinfinite/os/src/domain/valueObject"
+	infraHelper "github.com/goinfinite/os/src/infra/helper"
 	internalDbInfra "github.com/goinfinite/os/src/infra/internalDatabase"
 	dbModel "github.com/goinfinite/os/src/infra/internalDatabase/model"
 )
@@ -78,4 +81,100 @@ func (repo *AccountQueryRepo) ReadByUsername(
 	}
 
 	return accountModel.ToEntity()
+}
+
+func (repo *AccountQueryRepo) secureAccessKeyFactory(
+	rawSecureAccessKeyContent string,
+	secureAccessKeySecret string,
+) (secureAccessKey entity.SecureAccessKey, err error) {
+	keyContent, err := valueObject.NewSecureAccessKeyContent(
+		rawSecureAccessKeyContent,
+	)
+	if err != nil {
+		return secureAccessKey, err
+	}
+
+	keyName, err := keyContent.ReadOnlyKeyName()
+	if err != nil {
+		return secureAccessKey, err
+	}
+
+	rawKeyHashId, err := infraHelper.EncryptStr(
+		secureAccessKeySecret, keyContent.String(),
+	)
+	if err != nil {
+		return secureAccessKey, err
+	}
+
+	keyHashId, err := valueObject.NewSecureAccessKeyHashId(rawKeyHashId)
+	if err != nil {
+		return secureAccessKey, err
+	}
+
+	return entity.NewSecureAccessKey(keyHashId, keyName, keyContent), nil
+}
+
+func (repo *AccountQueryRepo) ReadSecureAccessKeys(
+	accountId valueObject.AccountId,
+) ([]entity.SecureAccessKey, error) {
+	secureAccessKeys := []entity.SecureAccessKey{}
+
+	account, err := repo.ReadById(accountId)
+	if err != nil {
+		return secureAccessKeys, errors.New("AccountNotFound")
+	}
+	accountUsernameStr := account.Username.String()
+
+	secureAccessKeysDirPath := "/home/" + accountUsernameStr + "/.ssh"
+	err = infraHelper.MakeDir(secureAccessKeysDirPath)
+	if err != nil {
+		return secureAccessKeys, errors.New(
+			"CreateSecureAccessKeysDirectoryError: " + err.Error(),
+		)
+	}
+
+	secureAccessKeysFilePath := secureAccessKeysDirPath + "/authorized_keys"
+	if !infraHelper.FileExists(secureAccessKeysFilePath) {
+		_, err = os.Create(secureAccessKeysFilePath)
+		if err != nil {
+			return secureAccessKeys, errors.New(
+				"CreateSecureAccessKeysFileError: " + err.Error(),
+			)
+		}
+
+		_, err = infraHelper.RunCmd(
+			"chown", "-R", accountUsernameStr, secureAccessKeysFilePath,
+		)
+		if err != nil {
+			return secureAccessKeys, errors.New(
+				"ChownSecureAccessKeysFileError: " + err.Error(),
+			)
+		}
+	}
+
+	secureAccessKeysFileContent, err := infraHelper.GetFileContent(
+		secureAccessKeysFilePath,
+	)
+	if err != nil {
+		return secureAccessKeys, errors.New(
+			"ReadSecureAccessKeysFileContentError: " + err.Error(),
+		)
+	}
+
+	secretKey := os.Getenv("ACCOUNT_SECURE_ACCESS_KEY_SECRET")
+
+	secureAccessKeysFileContentParts := strings.Split(secureAccessKeysFileContent, "\n")
+	for index, rawSecureAccessKeyContent := range secureAccessKeysFileContentParts {
+		secureAccessKey, err := repo.secureAccessKeyFactory(
+			rawSecureAccessKeyContent, secretKey,
+		)
+		if err != nil {
+			slog.Debug(err.Error(), slog.Int("index", index))
+			continue
+		}
+
+		secureAccessKeys = append(secureAccessKeys, secureAccessKey)
+	}
+
+	return secureAccessKeys, nil
 }
