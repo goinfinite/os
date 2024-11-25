c678edcbd0e95c60e32d8f4900688f13dd20f197
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-11-25 19:53:35 -0300

    feat: add DeleteSecureAccessKey DTO

---
+++ src/domain/dto/deleteSecureAccessKey.go
@@ -0,0 +1,23 @@
+package dto
+
+import "github.com/goinfinite/os/src/domain/valueObject"
+
+type DeleteSecureAccessKey struct {
+	Id                valueObject.SecureAccessKeyId `json:"id"`
+	AccountId         valueObject.AccountId         `json:"accountId"`
+	OperatorAccountId valueObject.AccountId         `json:"-"`
+	OperatorIpAddress valueObject.IpAddress         `json:"-"`
+}
+
+func NewDeleteSecureAccessKey(
+	id valueObject.SecureAccessKeyId,
+	accountId, operatorAccountId valueObject.AccountId,
+	operatorIpAddress valueObject.IpAddress,
+) DeleteSecureAccessKey {
+	return DeleteSecureAccessKey{
+		Id:                id,
+		AccountId:         accountId,
+		OperatorAccountId: operatorAccountId,
+		OperatorIpAddress: operatorIpAddress,
+	}
+}
