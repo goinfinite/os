dfba00826da4bd6cc05083c319bd38f8e70fa3fb
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-11-29 16:40:30 -0300

    refact: change params order to create DTO in infra tests

--- src/infra/account/secureAccessKey/secureAccessKeyQueryRepo_test.go
+++ src/infra/account/secureAccessKey/secureAccessKeyQueryRepo_test.go
@@ -34,7 +34,7 @@ func TestSecureAccessKeyQueryRepo(t *testing.T) {
 		"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC+GDqLA2sGauzU5hUxBbBmm6FfeZpUbiX6IlQO9KqeqAsum+Efhvj+qpatM5PzMMwtlcFwDS5Y4RcX9uxE8IGsYiALRfnLAX5p73zrcrXamMJSx25rXAu/VJdmekxHbDgsBPyk6/4dfu+3uW7ka7HHhPytPIqW2qBuPkalJinc7qKEuXdkCyX8+8a+0uN8XodLipLJwU8A1VPvI9thYxITyHWZnXRnin0r/unHgLrg9bBILXZf0JRslelYdCvuCGnRKZfokh153shMZ63S+iV/Tohg2bOVxyz3HIQ983ga24uTFQhLpITMe9JEfq3pp2wcCE5hNFlNKyeDG8kwB+8V",
 	)
 	createDto := dto.NewCreateSecureAccessKey(
-		keyName, keyContent, accountId, accountId, ipAddress,
+		accountId, keyContent, keyName, accountId, ipAddress,
 	)
 
 	secureAccessKeyCmdRepo := NewSecureAccessKeyCmdRepo(testHelpers.GetPersistentDbSvc())
