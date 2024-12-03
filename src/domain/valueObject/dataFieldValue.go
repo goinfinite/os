68cc0054cc449d2108d53b2084c94956caadb72c
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-12-03 14:42:27 -0300

    refact: not allow simple quote and double quotes as data field value

--- src/domain/valueObject/dataFieldValue.go
+++ src/domain/valueObject/dataFieldValue.go
@@ -2,6 +2,7 @@ package valueObject
 
 import (
 	"errors"
+	"strings"
 
 	voHelper "github.com/goinfinite/os/src/domain/valueObject/helper"
 )
@@ -22,6 +23,14 @@ func NewDataFieldValue(value interface{}) (
 
 	if len(stringValue) >= 2048 {
 		return dataFieldValue, errors.New("DataFieldValueTooBig")
+	}
+
+	if strings.Contains(stringValue, "'") {
+		return dataFieldValue, errors.New("DataFieldValueDoesNotAllowSimpleQuote")
+	}
+
+	if strings.Contains(stringValue, "\"") {
+		return dataFieldValue, errors.New("DataFieldValueDoesNotSupportDoubleQuotes")
 	}
 
 	return DataFieldValue(stringValue), nil
