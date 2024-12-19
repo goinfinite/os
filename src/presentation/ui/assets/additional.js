c1369e5c21da67756c7205a69b2bd4cb92041fd1
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-12-19 15:56:42 -0300

    refact: using try catch instead callbacks on jsonAjax methods to be able to return requests results

--- src/presentation/ui/assets/additional.js
+++ src/presentation/ui/assets/additional.js
@@ -39,24 +39,28 @@ document.addEventListener("alpine:init", () => {
     const loadingOverlayElement = document.getElementById("loading-overlay");
     loadingOverlayElement.classList.add("htmx-request");
 
-    await fetch(url, {
-      method: method,
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(payload),
-    })
-      .then((response) => {
+    try {
+        const response = await fetch(url, {
+            method: method,
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(payload),
+        });
+        const parsedResponse = await response.json();
+
         loadingOverlayElement.classList.remove("htmx-request");
-        return response.json();
-      })
-      .then((parsedResponse) => {
+
+        if (!response.ok) {
+            throw new Error(parsedResponse.body);
+        }
+
         Alpine.store("toast").displayToast(parsedResponse.body, "success");
-      })
-      .catch((parsedResponse) => {
-        Alpine.store("toast").displayToast(parsedResponse.body, "danger");
-      });
+        return parsedResponse.body;
+    } catch (error) {
+        Alpine.store("toast").displayToast(error.message, "danger");
+    }
   }
 
   function createRandomPassword() {
