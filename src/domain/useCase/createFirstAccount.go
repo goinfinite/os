d14105315561889e24d4d11f302b309a29c49fbe
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2024-12-20 14:31:24 -0300

    feat: add CreateFirstAccount use case

---
+++ src/domain/useCase/createFirstAccount.go
@@ -0,0 +1,33 @@
+package useCase
+
+import (
+	"errors"
+	"log/slog"
+
+	"github.com/goinfinite/os/src/domain/dto"
+	"github.com/goinfinite/os/src/domain/repository"
+)
+
+func CreateFirstAccount(
+	accountQueryRepo repository.AccountQueryRepo,
+	accountCmdRepo repository.AccountCmdRepo,
+	activityRecordCmdRepo repository.ActivityRecordCmdRepo,
+	createDto dto.CreateAccount,
+) error {
+	readRequestDto := dto.ReadAccountsRequest{}
+	_, err := accountQueryRepo.ReadFirst(readRequestDto)
+	if err != nil {
+		return errors.New("AtLeastOneAccountAlreadyExists")
+	}
+
+	accountId, err := accountCmdRepo.Create(createDto)
+	if err != nil {
+		slog.Error("CreateFirstAccountError", slog.Any("error", err))
+		return errors.New("CreateFirstAccountInfraError")
+	}
+
+	NewCreateSecurityActivityRecord(activityRecordCmdRepo).
+		CreateAccount(createDto, accountId)
+
+	return nil
+}
