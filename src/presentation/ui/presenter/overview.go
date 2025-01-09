c997b93e0ad6e7b835912ef5927f90168f173a9b
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2025-01-09 16:16:40 -0300

    refact: using marketplace presenter to get all required data to overview page on overview presenter

--- src/presentation/ui/presenter/overview.go
+++ src/presentation/ui/presenter/overview.go
@@ -1,20 +1,17 @@
 package presenter
 
 import (
-	"errors"
 	"log/slog"
 	"net/http"
 
-	"github.com/goinfinite/os/src/domain/dto"
 	internalDbInfra "github.com/goinfinite/os/src/infra/internalDatabase"
-	"github.com/goinfinite/os/src/presentation/service"
 	uiHelper "github.com/goinfinite/os/src/presentation/ui/helper"
 	"github.com/goinfinite/os/src/presentation/ui/page"
 	"github.com/labstack/echo/v4"
 )
 
 type OverviewPresenter struct {
-	marketplaceService *service.MarketplaceService
+	marketplacePresenter *MarketplacePresenter
 }
 
 func NewOverviewPresenter(
@@ -22,34 +19,23 @@ func NewOverviewPresenter(
 	trailDbSvc *internalDbInfra.TrailDatabaseService,
 ) *OverviewPresenter {
 	return &OverviewPresenter{
-		marketplaceService: service.NewMarketplaceService(persistentDbSvc, trailDbSvc),
+		marketplacePresenter: NewMarketplacePresenter(persistentDbSvc, trailDbSvc),
 	}
-}
-
-func (presenter *OverviewPresenter) readMarketplaceInstalledItems() (
-	responseDto dto.ReadMarketplaceInstalledItemsResponse, err error,
-) {
-	responseOutput := presenter.marketplaceService.ReadInstalledItems(
-		map[string]interface{}{},
-	)
-	if responseOutput.Status != service.Success {
-		return responseDto, errors.New("FailedToReadInstalledItems")
-	}
-
-	typedOutputBody, assertOk := responseOutput.Body.(dto.ReadMarketplaceInstalledItemsResponse)
-	if !assertOk {
-		return responseDto, errors.New("FailedToReadInstalledItems")
-	}
-	return typedOutputBody, nil
 }
 
 func (presenter *OverviewPresenter) Handler(c echo.Context) error {
-	marketplaceInstalledItemsResponseDto, err := presenter.readMarketplaceInstalledItems()
+	vhostsHostnames, err := presenter.marketplacePresenter.ReadVhostsHostnames()
 	if err != nil {
 		slog.Error(err.Error())
 		return nil
 	}
 
-	pageContent := page.OverviewIndex(marketplaceInstalledItemsResponseDto)
+	marketplaceOverview, err := presenter.marketplacePresenter.MarketplaceOverviewFactory("all")
+	if err != nil {
+		slog.Error(err.Error())
+		return nil
+	}
+
+	pageContent := page.OverviewIndex(vhostsHostnames, marketplaceOverview)
 	return uiHelper.Render(c, pageContent, http.StatusOK)
 }
