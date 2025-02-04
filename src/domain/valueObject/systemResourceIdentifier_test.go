cc4da7566bf909a9c2a3c0d557a69fe5a9d6b901
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2025-02-04 17:35:40 -0300

    fix: change SystemResourceIdentifier regex to NewUnixFileSri constructor works

--- src/domain/valueObject/systemResourceIdentifier_test.go
+++ src/domain/valueObject/systemResourceIdentifier_test.go
@@ -5,7 +5,13 @@ import "testing"
 func TestSystemResourceIdentifier(t *testing.T) {
 	t.Run("ValidSystemResourceIdentifier", func(t *testing.T) {
 		validSystemResourceIdentifier := []interface{}{
-			"sri://0:account/120", "sri://0:cron/1", "sri://1:mapping/200",
+			"sri://1:account/120", "sri://10:secureAccessPublicKey/1", "sri://100:cron/1",
+			"sri://1000:database/myDb", "sri://1:databaseUser/myDbUser",
+			"sri://10:marketplaceCatalogItem/1", "sri://100:marketplaceCatalogItem/php",
+			"sri://1000:marketplaceInstalledItem/1", "sri://1:phpRuntime/local.os",
+			"sri://10:installableService/node", "sri://100:customService/node-e87qxc21",
+			"sri://1000:installedService/1", "sri://1:ssl/1", "sri://10:virtualHost/local.os",
+			"sri://100:mapping/1", "sri://1000:unixFile//app/.trash",
 		}
 
 		for _, identifier := range validSystemResourceIdentifier {
@@ -20,7 +26,7 @@ func TestSystemResourceIdentifier(t *testing.T) {
 
 	t.Run("InvalidSystemResourceIdentifier", func(t *testing.T) {
 		invalidSystemResourceIdentifier := []interface{}{
-			"", "sri://0:/",
+			"", "sri://0:/", true, 1000,
 		}
 
 		for _, identifier := range invalidSystemResourceIdentifier {
