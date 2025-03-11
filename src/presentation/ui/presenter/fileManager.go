0c3c14eb27d3f940c2e3fe207c2b3f89667fa967
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2025-03-11 19:27:04 -0300

    feat: add DirectoriesTree with files grouped by type (directories and files) injection by presenter without JS implementation

--- src/presentation/ui/presenter/fileManager.go
+++ src/presentation/ui/presenter/fileManager.go
@@ -1,8 +1,13 @@
 package presenter
 
 import (
+	"log/slog"
 	"net/http"
 
+	"github.com/goinfinite/os/src/domain/entity"
+	"github.com/goinfinite/os/src/domain/useCase"
+	"github.com/goinfinite/os/src/domain/valueObject"
+	filesInfra "github.com/goinfinite/os/src/infra/files"
 	uiHelper "github.com/goinfinite/os/src/presentation/ui/helper"
 	"github.com/goinfinite/os/src/presentation/ui/page"
 	"github.com/labstack/echo/v4"
@@ -14,7 +19,50 @@ func NewFileManagerPresenter() *FileManagerPresenter {
 	return &FileManagerPresenter{}
 }
 
+func (presenter *FileManagerPresenter) readFilesGroupedByType(
+	rawDesiredSourcePath string,
+) (filesGroupedByType page.FilesGroupedByType, err error) {
+	if rawDesiredSourcePath == "" {
+		rawDesiredSourcePath = "/"
+	}
+
+	desiredSourcePath, err := valueObject.NewUnixFilePath(rawDesiredSourcePath)
+	if err != nil {
+		return filesGroupedByType, err
+	}
+
+	filesList, err := useCase.ReadFiles(filesInfra.FilesQueryRepo{}, desiredSourcePath)
+	if err != nil {
+		return filesGroupedByType, err
+	}
+
+	filesGroupedByType = page.FilesGroupedByType{
+		Directories: []entity.UnixFile{},
+		Files:       []entity.UnixFile{},
+	}
+	for _, fileEntity := range filesList {
+		if fileEntity.MimeType.IsDir() {
+			filesGroupedByType.Directories = append(
+				filesGroupedByType.Directories, fileEntity,
+			)
+			continue
+		}
+
+		filesGroupedByType.Files = append(filesGroupedByType.Files, fileEntity)
+	}
+
+	return filesGroupedByType, nil
+}
+
 func (presenter *FileManagerPresenter) Handler(c echo.Context) error {
-	pageContent := page.FileManagerIndex()
+	filesGroupedByType, err := presenter.readFilesGroupedByType(
+		c.QueryParam("desiredSourcePath"),
+	)
+	if err != nil {
+		slog.Error(err.Error())
+		return nil
+	}
+
+	pageContent := page.FileManagerIndex(filesGroupedByType)
 	return uiHelper.Render(c, pageContent, http.StatusOK)
 }
