2f1b1aef3b7517ab36fed7db025b9a58a2283a78
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2025-03-19 15:25:36 -0300

    refact: change find button to show search bar instead to search itself

--- src/presentation/ui/assets/local-states/page/file-manager.js
+++ src/presentation/ui/assets/local-states/page/file-manager.js
@@ -229,7 +229,6 @@ document.addEventListener("alpine:init", () => {
     isUpdateFileContentModalOpen: false,
     codeEditorInstance: null,
     codeEditorFontSize: 12,
-    codeEditorFindContent: "",
     resizeCodeEditorFont(operation) {
       switch (operation) {
         case "decrease":
@@ -246,7 +245,6 @@ document.addEventListener("alpine:init", () => {
       this.resetPrimaryStates();
       this.codeEditorInstance = null;
       this.codeEditorFontSize = 12;
-      this.codeEditorFindContent = "";
 
       const fileName = this.selectedFileNames[0];
       const fileEntity = JSON.parse(
