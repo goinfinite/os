30fef9c97ccfd63a7c01d54e470f5284a4b13d7f
ntorga <northontorga@gmail.com>
2025-03-24 19:12:43 -0300

    feat: add NormalizeKnownUnixFilePathPermissions()

--- src/domain/useCase/createUnixFile.go
+++ src/domain/useCase/createUnixFile.go
@@ -3,10 +3,36 @@ package useCase
 import (
 	"errors"
 	"log/slog"
+	"strings"
 
 	"github.com/goinfinite/os/src/domain/dto"
 	"github.com/goinfinite/os/src/domain/repository"
+	"github.com/goinfinite/os/src/domain/valueObject"
 )
+
+func NormalizeKnownUnixFilePathPermissions(
+	filesCmdRepo repository.FilesCmdRepo,
+	filePath valueObject.UnixFilePath,
+) {
+	isAppDirectoryDescendant := strings.HasPrefix(
+		filePath.String(),
+		valueObject.UnixFilePathAppWorkingDir.String(),
+	)
+	if !isAppDirectoryDescendant {
+		return
+	}
+
+	err := filesCmdRepo.UpdateOwnership(dto.NewUpdateUnixFileOwnership(
+		filePath, valueObject.UnixFileOwnershipAppWorkingDir,
+	))
+	if err != nil {
+		slog.Debug(
+			"UpdateOwnershipInfraError",
+			slog.String("method", "NormalizeKnownUnixFilePathPermissions"),
+			slog.String("err", err.Error()),
+		)
+	}
+}
 
 func CreateUnixFile(
 	filesQueryRepo repository.FilesQueryRepo,
@@ -22,6 +48,8 @@ func CreateUnixFile(
 
 	NewCreateSecurityActivityRecord(activityRecordCmdRepo).
 		CreateUnixFile(createDto)
+
+	NormalizeKnownUnixFilePathPermissions(filesCmdRepo, createDto.FilePath)
 
 	return nil
 }
