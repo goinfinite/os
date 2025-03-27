9a5b3af6a2d4e187515eb51eef98e6aab05cb98d
ntorga <northontorga@gmail.com>
2025-03-27 13:45:24 -0300

    fix: use new vhost repo and add default pagination

--- src/domain/useCase/readVirtualHosts.go
+++ src/domain/useCase/readVirtualHosts.go
@@ -4,18 +4,24 @@ import (
 	"errors"
 	"log/slog"
 
-	"github.com/goinfinite/os/src/domain/entity"
+	"github.com/goinfinite/os/src/domain/dto"
 	"github.com/goinfinite/os/src/domain/repository"
 )
 
+var VirtualHostsDefaultPagination dto.Pagination = dto.Pagination{
+	PageNumber:   0,
+	ItemsPerPage: 10,
+}
+
 func ReadVirtualHosts(
 	vhostQueryRepo repository.VirtualHostQueryRepo,
-) ([]entity.VirtualHost, error) {
-	vhosts, err := vhostQueryRepo.Read()
+	requestDto dto.ReadVirtualHostsRequest,
+) (responseDto dto.ReadVirtualHostsResponse, err error) {
+	responseDto, err = vhostQueryRepo.Read(requestDto)
 	if err != nil {
 		slog.Error("ReadVirtualHostsError", slog.String("err", err.Error()))
-		return vhosts, errors.New("ReadVirtualHostsInfraError")
+		return responseDto, errors.New("ReadVirtualHostsInfraError")
 	}
 
-	return vhosts, nil
+	return responseDto, nil
 }
