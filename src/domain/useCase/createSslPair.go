638a44f5a6cc7691d2f260966bbd775b2068b63d
ntorga <northontorga@gmail.com>
2025-03-27 13:45:45 -0300

    fix: use new vhost query repo and improve code readability

--- src/domain/useCase/createSslPair.go
+++ src/domain/useCase/createSslPair.go
@@ -11,48 +11,42 @@ import (
 )
 
 func CreateSslPair(
-	sslCmdRepo repository.SslCmdRepo,
 	vhostQueryRepo repository.VirtualHostQueryRepo,
+	sslCmdRepo repository.SslCmdRepo,
 	activityRecordCmdRepo repository.ActivityRecordCmdRepo,
 	createDto dto.CreateSslPair,
 ) error {
-	existingVhosts, err := vhostQueryRepo.Read()
-	if err != nil {
-		slog.Error("ReadVhostsError", slog.String("err", err.Error()))
-		return errors.New("ReadVhostsInfraError")
-	}
+	readVirtualHostsResponse, err := vhostQueryRepo.Read(dto.ReadVirtualHostsRequest{
+		Pagination: dto.Pagination{PageNumber: 0, ItemsPerPage: 1000},
+	})
 
-	if len(existingVhosts) == 0 {
-		return errors.New("VhostsNotFound")
-	}
-
-	validSslVirtualHostsHostnames := []valueObject.Fqdn{}
-	for _, vhost := range existingVhosts {
-		if vhost.Type.String() == "alias" {
+	existingHostnames := []valueObject.Fqdn{}
+	for _, vhostEntity := range readVirtualHostsResponse.VirtualHosts {
+		if vhostEntity.Type == valueObject.VirtualHostTypeAlias {
 			continue
 		}
 
-		if slices.Contains(createDto.VirtualHostsHostnames, vhost.Hostname) {
-			validSslVirtualHostsHostnames = append(
-				validSslVirtualHostsHostnames, vhost.Hostname,
-			)
+		if !slices.Contains(createDto.VirtualHostsHostnames, vhostEntity.Hostname) {
+			continue
 		}
+
+		existingHostnames = append(existingHostnames, vhostEntity.Hostname)
 	}
 
-	if len(validSslVirtualHostsHostnames) == 0 {
-		return errors.New("VhostDoesNotExists")
+	if len(existingHostnames) == 0 {
+		return errors.New("SpecifiedVirtualHostsNotFound")
 	}
 
-	createDto.VirtualHostsHostnames = validSslVirtualHostsHostnames
+	createDto.VirtualHostsHostnames = existingHostnames
 
-	createdSslPairId, err := sslCmdRepo.Create(createDto)
+	sslPairId, err := sslCmdRepo.Create(createDto)
 	if err != nil {
 		slog.Error("CreateSslPairError", slog.String("err", err.Error()))
 		return errors.New("CreateSslPairInfraError")
 	}
 
 	NewCreateSecurityActivityRecord(activityRecordCmdRepo).
-		CreateSslPair(createDto, createdSslPairId)
+		CreateSslPair(createDto, sslPairId)
 
 	return nil
 }
