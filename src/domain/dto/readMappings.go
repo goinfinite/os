e63f1bc8ce8a27f1dd3cf0c5f642dda2b9b09465
ntorga <northontorga@gmail.com>
2025-03-27 11:58:34 -0300

    feat: add read vhost and mapping dtos

---
+++ src/domain/dto/readMappings.go
@@ -0,0 +1,24 @@
+package dto
+
+import (
+	"github.com/goinfinite/os/src/domain/entity"
+	"github.com/goinfinite/os/src/domain/valueObject"
+)
+
+type ReadMappingsRequest struct {
+	Pagination             Pagination                       `json:"pagination"`
+	MappingId              *valueObject.MappingId           `json:"mappingId"`
+	Hostname               *valueObject.Fqdn                `json:"hostname"`
+	MappingPath            *valueObject.MappingPath         `json:"mappingPath"`
+	MatchPattern           *valueObject.MappingMatchPattern `json:"matchPattern"`
+	TargetType             *valueObject.MappingTargetType   `json:"targetType"`
+	TargetValue            *valueObject.MappingTargetValue  `json:"targetValue"`
+	TargetHttpResponseCode *valueObject.HttpResponseCode    `json:"targetHttpResponseCode"`
+	CreatedBeforeAt        *valueObject.UnixTime            `json:"createdBeforeAt"`
+	CreatedAfterAt         *valueObject.UnixTime            `json:"createdAfterAt"`
+}
+
+type ReadMappingsResponse struct {
+	Pagination Pagination       `json:"pagination"`
+	Mappings   []entity.Mapping `json:"mappings"`
+}
---
+++ src/domain/dto/readVirtualHosts.go
@@ -0,0 +1,28 @@
+package dto
+
+import (
+	"github.com/goinfinite/os/src/domain/entity"
+	"github.com/goinfinite/os/src/domain/valueObject"
+)
+
+type ReadVirtualHostsRequest struct {
+	Pagination      Pagination                   `json:"pagination"`
+	Hostname        *valueObject.Fqdn            `json:"hostname"`
+	VirtualHostType *valueObject.VirtualHostType `json:"type"`
+	RootDirectory   *valueObject.UnixFilePath    `json:"rootDirectory"`
+	ParentHostname  *valueObject.Fqdn            `json:"parentHostname"`
+	WithMappings    *bool                        `json:"withMappings"`
+	CreatedBeforeAt *valueObject.UnixTime        `json:"createdBeforeAt"`
+	CreatedAfterAt  *valueObject.UnixTime        `json:"createdAfterAt"`
+}
+
+type VirtualHostWithMappings struct {
+	entity.VirtualHost
+	Mappings []entity.Mapping `json:"mappings"`
+}
+
+type ReadVirtualHostsResponse struct {
+	Pagination              Pagination                `json:"pagination"`
+	VirtualHosts            []entity.VirtualHost      `json:"virtualHosts"`
+	VirtualHostWithMappings []VirtualHostWithMappings `json:"virtualHostWithMappings"`
+}
