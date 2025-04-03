1436597c74e0be8e8f023e736efacf6e1ecf9556
ntorga <northontorga@gmail.com>
2025-04-03 12:52:28 -0300

    feat: add and use read ssl pairs dto

--- src/domain/useCase/readSslPairs.go
+++ src/domain/useCase/readSslPairs.go
@@ -1,12 +1,27 @@
 package useCase
 
 import (
-	"github.com/goinfinite/os/src/domain/entity"
+	"errors"
+	"log/slog"
+
+	"github.com/goinfinite/os/src/domain/dto"
 	"github.com/goinfinite/os/src/domain/repository"
 )
 
+var SslPairsDefaultPagination dto.Pagination = dto.Pagination{
+	PageNumber:   0,
+	ItemsPerPage: 10,
+}
+
 func ReadSslPairs(
 	sslQueryRepo repository.SslQueryRepo,
-) ([]entity.SslPair, error) {
-	return sslQueryRepo.Read()
+	requestDto dto.ReadSslPairsRequest,
+) (responseDto dto.ReadSslPairsResponse, err error) {
+	responseDto, err = sslQueryRepo.Read(requestDto)
+	if err != nil {
+		slog.Error("ReadSslPairsError", slog.String("err", err.Error()))
+		return responseDto, errors.New("ReadSslPairsInfraError")
+	}
+
+	return responseDto, nil
 }
