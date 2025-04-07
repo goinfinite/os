f0cf5c51197bd145f90ed8649c951a61812df641
ntorga <northontorga@gmail.com>
2025-04-07 16:31:10 -0300

    fix: divide tests into t.Run() blocks

--- src/infra/internalDatabase/transientDatabaseService_test.go
+++ src/infra/internalDatabase/transientDatabaseService_test.go
@@ -7,49 +7,54 @@ import (
 func TestTransientDbSvc(t *testing.T) {
 	dbSvc, err := NewTransientDatabaseService()
 	if err != nil {
-		t.Fatalf("TransientDbSvcInitFailed: %v", err)
+		t.Fatalf("TransientDatabaseServiceInitFailed: %v", err)
 	}
 
 	testKey := "test_key"
 	testValue := "test_value"
-
-	if dbSvc.Has(testKey) {
-		t.Errorf("TransientDbSvcHasNonExistentKeyFailed: %v", err)
-	}
-
-	err = dbSvc.Set(testKey, testValue)
-	if err != nil {
-		t.Errorf("TransientDbSvcSetFailed: %v", err)
-	}
-
-	if !dbSvc.Has(testKey) {
-		t.Errorf("TransientDbSvcHasExistingKeyFailed: %v", err)
-	}
-	value, err := dbSvc.Read(testKey)
-	if err != nil {
-		t.Errorf("TransientDbSvcReadFailed: %v", err)
-	}
-	if value != testValue {
-		t.Errorf("TransientDbSvcReadIncorrectValue: got %v, want %v", value, testValue)
-	}
-
 	nonExistentKey := "non_existent_key"
-	_, err = dbSvc.Read(nonExistentKey)
-	if err == nil {
-		t.Errorf("TransientDbSvcReadNonExistentKeyFailed: %v", err)
-	}
-
 	updatedValue := "updated_value"
-	err = dbSvc.Set(testKey, updatedValue)
-	if err != nil {
-		t.Errorf("TransientDbSvcSetFailedWhenUpdating: %v", err)
-	}
 
-	value, err = dbSvc.Read(testKey)
-	if err != nil {
-		t.Errorf("TransientDbSvcReadFailedAfterUpdate: %v", err)
-	}
-	if value != updatedValue {
-		t.Errorf("TransientDbSvcReadIncorrectValueAfterUpdate: got %v, want %v", value, updatedValue)
-	}
+	t.Run("HasFunctionality", func(t *testing.T) {
+		if dbSvc.Has(testKey) {
+			t.Errorf("HasReturnedTrueForNonExistentKey")
+		}
+
+		err := dbSvc.Set(testKey, testValue)
+		if err != nil {
+			t.Errorf("SetFailed: %v", err)
+		}
+
+		if !dbSvc.Has(testKey) {
+			t.Errorf("HasReturnedFalseForExistingKey")
+		}
+	})
+
+	t.Run("ReadWriteFunctionality", func(t *testing.T) {
+		value, err := dbSvc.Read(testKey)
+		if err != nil {
+			t.Errorf("ReadFailed: %v", err)
+		}
+		if value != testValue {
+			t.Errorf("IncorrectValue: Got %v, Want %v", value, testValue)
+		}
+
+		_, err = dbSvc.Read(nonExistentKey)
+		if err == nil {
+			t.Errorf("ExpectedErrorForNonExistentKeyButGotNil")
+		}
+
+		err = dbSvc.Set(testKey, updatedValue)
+		if err != nil {
+			t.Errorf("UpdateFailed: %v", err)
+		}
+
+		value, err = dbSvc.Read(testKey)
+		if err != nil {
+			t.Errorf("ReadFailedAfterUpdate: %v", err)
+		}
+		if value != updatedValue {
+			t.Errorf("IncorrectValueAfterUpdate: Got %v, Want %v", value, updatedValue)
+		}
+	})
 }
