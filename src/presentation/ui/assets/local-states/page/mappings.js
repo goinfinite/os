8d7e94869d5e0c72f2010bcfd3b538342636cc0b
ntorga <northontorga@gmail.com>
2025-04-10 16:02:29 -0300

    feat: add force https input to mapping modal

--- src/presentation/ui/assets/local-states/page/mappings.js
+++ src/presentation/ui/assets/local-states/page/mappings.js
@@ -22,6 +22,7 @@ document.addEventListener("alpine:init", () => {
         targetType: "url",
         targetValue: "",
         targetHttpResponseCode: "",
+        shouldUpgradeInsecureRequests: "false",
       };
     },
     init() {
