4452469d6f63a65caeaf5b0be2562853143292bc
ntorga <northontorga@gmail.com>
2025-04-13 16:19:28 -0300

    refactor: db query repo impl with pagination

--- src/infra/database/databaseQueryRepo.go
+++ src/infra/database/databaseQueryRepo.go
@@ -2,7 +2,9 @@ package databaseInfra
 
 import (
 	"errors"
+	"math"
 
+	"github.com/goinfinite/os/src/domain/dto"
 	"github.com/goinfinite/os/src/domain/entity"
 	"github.com/goinfinite/os/src/domain/valueObject"
 )
@@ -14,35 +16,99 @@ type DatabaseQueryRepo struct {
 func NewDatabaseQueryRepo(
 	dbType valueObject.DatabaseType,
 ) *DatabaseQueryRepo {
-	return &DatabaseQueryRepo{
-		dbType: dbType,
-	}
+	return &DatabaseQueryRepo{dbType: dbType}
 }
 
-func (repo DatabaseQueryRepo) Read() ([]entity.Database, error) {
+func (repo DatabaseQueryRepo) Read(
+	requestDto dto.ReadDatabasesRequest,
+) (responseDto dto.ReadDatabasesResponse, err error) {
+	if requestDto.DatabaseType == nil {
+		requestDto.DatabaseType = &repo.dbType
+	}
+
+	allDatabases := []entity.Database{}
 	switch repo.dbType {
 	case "mariadb":
-		return MysqlDatabaseQueryRepo{}.Read()
+		allDatabases, err = MysqlDatabaseQueryRepo{}.readAllDatabases()
 	case "postgresql":
-		return PostgresDatabaseQueryRepo{}.Read()
+		allDatabases, err = PostgresDatabaseQueryRepo{}.readAllDatabases()
 	default:
-		return []entity.Database{}, errors.New("DatabaseTypeNotSupported")
+		return responseDto, errors.New("DatabaseTypeNotSupported")
 	}
+	if err != nil {
+		return responseDto, err
+	}
+
+	filteredDatabases := []entity.Database{}
+	for _, databaseEntity := range allDatabases {
+		if requestDto.DatabaseName != nil && databaseEntity.Name != *requestDto.DatabaseName {
+			continue
+		}
+
+		if requestDto.DatabaseType != nil && databaseEntity.Type != *requestDto.DatabaseType {
+			continue
+		}
+
+		if requestDto.Username != nil {
+			userFound := false
+			for _, dbUserEntity := range databaseEntity.Users {
+				if dbUserEntity.Username != *requestDto.Username {
+					continue
+				}
+
+				userFound = true
+				break
+			}
+
+			if !userFound {
+				continue
+			}
+		}
+
+		filteredDatabases = append(filteredDatabases, databaseEntity)
+	}
+
+	paginatedDatabases := []entity.Database{}
+	startIndex := int(requestDto.Pagination.PageNumber) * int(requestDto.Pagination.ItemsPerPage)
+	endIndex := startIndex + int(requestDto.Pagination.ItemsPerPage)
+
+	if startIndex < len(filteredDatabases) {
+		if endIndex > len(filteredDatabases) {
+			endIndex = len(filteredDatabases)
+		}
+		paginatedDatabases = filteredDatabases[startIndex:endIndex]
+	}
+
+	itemsTotal := uint64(len(filteredDatabases))
+	pagesTotal := uint32(math.Ceil(float64(itemsTotal) / float64(requestDto.Pagination.ItemsPerPage)))
+
+	paginationDto := requestDto.Pagination
+	paginationDto.ItemsTotal = &itemsTotal
+	paginationDto.PagesTotal = &pagesTotal
+
+	responseDto.Pagination = paginationDto
+	responseDto.Databases = paginatedDatabases
+
+	return responseDto, nil
 }
 
-func (repo DatabaseQueryRepo) ReadByName(
-	name valueObject.DatabaseName,
-) (entity.Database, error) {
-	dbs, err := repo.Read()
+func (repo DatabaseQueryRepo) ReadFirst(
+	requestDto dto.ReadDatabasesRequest,
+) (database entity.Database, err error) {
+	requestDto.Pagination = dto.PaginationSingleItem
+
+	if requestDto.DatabaseType == nil {
+		requestDto.DatabaseType = &repo.dbType
+	}
+
+	responseDto, err := repo.Read(requestDto)
 	if err != nil {
-		return entity.Database{}, err
+		return database, err
 	}
 
-	for _, db := range dbs {
-		if db.Name == name {
-			return db, nil
-		}
+	if len(responseDto.Databases) == 0 {
+		return database, errors.New("DatabaseNotFound")
 	}
 
-	return entity.Database{}, errors.New("DatabaseNotFound")
+	return responseDto.Databases[0], nil
 }
