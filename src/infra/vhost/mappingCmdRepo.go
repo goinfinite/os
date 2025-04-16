2d7486633f025b2f3b8817e04ce94b346d4baacd
ntorga <northontorga@gmail.com>
2025-04-16 15:52:00 -0300

    feat: impl mapping rules cmd infra

--- src/infra/vhost/mappingCmdRepo.go
+++ src/infra/vhost/mappingCmdRepo.go
@@ -2,11 +2,13 @@ package vhostInfra
 
 import (
 	"errors"
+	"os"
 	"strings"
 	"text/template"
 
 	"github.com/goinfinite/os/src/domain/dto"
 	"github.com/goinfinite/os/src/domain/valueObject"
+	infraEnvs "github.com/goinfinite/os/src/infra/envs"
 	infraHelper "github.com/goinfinite/os/src/infra/helper"
 	internalDbInfra "github.com/goinfinite/os/src/infra/internalDatabase"
 	dbModel "github.com/goinfinite/os/src/infra/internalDatabase/model"
@@ -182,7 +184,7 @@ func (repo *MappingCmdRepo) serviceMappingConfigFactory(
 	if isGrpcSupported && !isGrpcsSupported && isHttpOrHttpsSupported {
 		svcMappingConfig += `
 	grpc_set_header Host $host;
-	if ($protocol = grpc) {	
+	if ($protocol = grpc) {
 		grpc_pass $protocol://$backend;
 	}
 `
@@ -191,7 +193,7 @@ func (repo *MappingCmdRepo) serviceMappingConfigFactory(
 	if !isGrpcSupported && isGrpcsSupported && isHttpOrHttpsSupported {
 		svcMappingConfig += `
 	grpc_set_header Host $host;
-	if ($protocol = grpcs) {	
+	if ($protocol = grpcs) {
 		grpc_pass $protocol://$backend;
 	}
 `
@@ -297,6 +299,9 @@ location {{ locationUriConfigFactory .MatchPattern .Path }} {
 		return 301 https://$host$request_uri;
 	}
 	{{- end }}
+	{{- if .MappingSecurityRuleId }}
+	include ` + infraEnvs.MappingsSecurityRulesConfDir + `/{{ .MappingSecurityRuleId.String }}.embeddable.conf;
+	{{- end }}
 	{{- if eq .TargetType "response-code" }}
 	return {{ .TargetHttpResponseCode }};
 	{{- end }}
@@ -378,6 +383,12 @@ func (repo *MappingCmdRepo) Create(
 		targetHttpResponseCodePtr = &targetHttpResponseCodeStr
 	}
 
+	var mappingSecurityRuleIdPtr *uint64
+	if createDto.MappingSecurityRuleId != nil {
+		ruleIdUint64 := createDto.MappingSecurityRuleId.Uint64()
+		mappingSecurityRuleIdPtr = &ruleIdUint64
+	}
+
 	mappingModel := dbModel.Mapping{
 		Hostname:                      createDto.Hostname.String(),
 		Path:                          createDto.Path.String(),
@@ -386,6 +397,7 @@ func (repo *MappingCmdRepo) Create(
 		TargetValue:                   targetValuePtr,
 		TargetHttpResponseCode:        targetHttpResponseCodePtr,
 		ShouldUpgradeInsecureRequests: createDto.ShouldUpgradeInsecureRequests,
+		MappingSecurityRuleID:         mappingSecurityRuleIdPtr,
 	}
 	err = repo.persistentDbSvc.Handler.Create(&mappingModel).Error
 	if err != nil {
@@ -455,6 +467,271 @@ func (repo *MappingCmdRepo) Delete(mappingId valueObject.MappingId) error {
 	err = repo.RecreateMappingFile(mappingEntity.Hostname)
 	if err != nil {
 		return errors.New("RecreateMappingFileError: " + err.Error())
+	}
+
+	return infraHelper.ReloadWebServer()
+}
+
+func (repo *MappingCmdRepo) recreateSecurityRuleFile(
+	mappingSecurityRuleId valueObject.MappingSecurityRuleId,
+) (err error) {
+	ruleEntity, err := repo.mappingQueryRepo.ReadFirstSecurityRule(
+		dto.ReadMappingSecurityRulesRequest{MappingSecurityRuleId: &mappingSecurityRuleId},
+	)
+	if err != nil {
+		return errors.New("ReadMappingSecurityRuleError: " + err.Error())
+	}
+
+	ruleGlobalTemplateStr := `{{- if .RpsSoftLimitPerIp && .RpsHardLimitPerIp }}
+limit_req_zone $binary_remote_addr zone=rps_limit_{{ .Id }}:10m rate={{ .RpsSoftLimitPerIp }}r/s;
+{{- else if .RpsSoftLimitPerIp }}
+limit_req_zone $binary_remote_addr zone=rps_limit_{{ .Id }}:10m rate={{ .RpsSoftLimitPerIp }}r/s;
+{{- else if .RpsHardLimitPerIp }}
+limit_req_zone $binary_remote_addr zone=rps_limit_{{ .Id }}:10m rate={{ .RpsHardLimitPerIp }}r/s;
+{{- end }}
+{{- if .RpsSoftLimitPerIp || .RpsHardLimitPerIp }}
+{{- if .ResponseCodeOnMaxRequests }}
+limit_req_status {{ .ResponseCodeOnMaxRequests }};
+{{- end }}
+{{- end }}
+
+{{- if .MaxConnectionsPerIp }}
+limit_conn_zone $binary_remote_addr zone=conn_limit_{{ .Id }}:10m;
+{{- if .ResponseCodeOnMaxConnections }}
+limit_conn_status {{ .ResponseCodeOnMaxConnections }};
+{{- end }}
+{{- end }}
+`
+
+	ruleGlobalTemplateStrPtr, err := template.New("ruleGlobalFile").Parse(ruleGlobalTemplateStr)
+	if err != nil {
+		return errors.New("GlobalTemplateParsingError: " + err.Error())
+	}
+
+	var ruleGlobalFileContent strings.Builder
+	err = ruleGlobalTemplateStrPtr.Execute(&ruleGlobalFileContent, ruleEntity)
+	if err != nil {
+		return errors.New("GlobalTemplateExecutionError: " + err.Error())
+	}
+
+	err = infraHelper.MakeDir(infraEnvs.MappingsSecurityRulesConfDir)
+	if err != nil {
+		return errors.New("CreateSecurityRulesDirError: " + err.Error())
+	}
+
+	ruleGlobalFilePath := infraEnvs.MappingsSecurityRulesConfDir + "/" +
+		mappingSecurityRuleId.String() + ".global.conf"
+	err = infraHelper.UpdateFile(ruleGlobalFilePath, ruleGlobalFileContent.String(), true)
+	if err != nil {
+		return errors.New("CreateSecurityRuleGlobalFileError: " + err.Error())
+	}
+
+	ruleEmbeddableTemplateStr := `{{- if .RpsSoftLimitPerIp && .RpsHardLimitPerIp }}
+limit_req zone=rps_limit_{{ .Id }} burst={{ .RpsHardLimitPerIp }};
+{{- else if .RpsSoftLimitPerIp || .RpsHardLimitPerIp }}
+limit_req zone=rps_limit_{{ .Id }};
+{{- end }}
+
+{{- if .MaxConnectionsPerIp }}
+limit_conn conn_limit_{{ .Id }} {{ .MaxConnectionsPerIp }};
+{{- end }}
+	
+{{- if .BandwidthBpsLimitPerConnection }}
+limit_rate {{ .BandwidthBpsLimitPerConnection }};
+{{- if .BandwidthLimitOnlyAfterBytes }}
+limit_rate_after {{ .BandwidthLimitOnlyAfterBytes }};
+{{- end }}
+{{- end }}
+
+{{- if .AllowedIps -}}
+{{- range .AllowedIps }}
+allow {{ . }};
+{{- end }}
+{{- end }}
+
+{{- if .BlockedIps -}}
+{{- range .BlockedIps -}}
+deny {{ . }};
+{{- end }}
+{{- end }}
+`
+
+	ruleEmbeddableTemplateStrPtr, err := template.New("ruleEmbeddableFile").Parse(ruleEmbeddableTemplateStr)
+	if err != nil {
+		return errors.New("EmbeddableTemplateParsingError: " + err.Error())
+	}
+
+	var ruleEmbeddableFileContent strings.Builder
+	err = ruleEmbeddableTemplateStrPtr.Execute(&ruleEmbeddableFileContent, ruleEntity)
+	if err != nil {
+		return errors.New("EmbeddableTemplateExecutionError: " + err.Error())
+	}
+
+	ruleEmbeddableFilePath := infraEnvs.MappingsSecurityRulesConfDir + "/" +
+		mappingSecurityRuleId.String() + ".embeddable.conf"
+	err = infraHelper.UpdateFile(ruleEmbeddableFilePath, ruleEmbeddableFileContent.String(), true)
+	if err != nil {
+		return errors.New("CreateSecurityRuleEmbeddableFileError: " + err.Error())
+	}
+
+	return nil
+}
+
+func (repo *MappingCmdRepo) CreateSecurityRule(
+	createDto dto.CreateMappingSecurityRule,
+) (ruleId valueObject.MappingSecurityRuleId, err error) {
+	var descriptionPtr *string
+	if createDto.Description != nil {
+		descriptionStr := createDto.Description.String()
+		descriptionPtr = &descriptionStr
+	}
+
+	allowedIps := []string{}
+	for _, ipAddress := range createDto.AllowedIps {
+		allowedIps = append(allowedIps, ipAddress.String())
+	}
+
+	blockedIps := []string{}
+	for _, ipAddress := range createDto.BlockedIps {
+		blockedIps = append(blockedIps, ipAddress.String())
+	}
+
+	var bandwidthBpsLimitPerConnectionPtr *uint64
+	if createDto.BandwidthBpsLimitPerConnection != nil {
+		perConnectionUint64 := createDto.BandwidthBpsLimitPerConnection.Uint64()
+		bandwidthBpsLimitPerConnectionPtr = &perConnectionUint64
+	}
+
+	var bandwidthLimitOnlyAfterBytesPtr *uint64
+	if createDto.BandwidthLimitOnlyAfterBytes != nil {
+		afterBytesUint64 := createDto.BandwidthLimitOnlyAfterBytes.Uint64()
+		bandwidthLimitOnlyAfterBytesPtr = &afterBytesUint64
+	}
+
+	securityRuleModel := dbModel.MappingSecurityRule{
+		Name:                           createDto.Name.String(),
+		Description:                    descriptionPtr,
+		AllowedIps:                     allowedIps,
+		BlockedIps:                     blockedIps,
+		RpsSoftLimitPerIp:              createDto.RpsSoftLimitPerIp,
+		RpsHardLimitPerIp:              createDto.RpsHardLimitPerIp,
+		ResponseCodeOnMaxRequests:      createDto.ResponseCodeOnMaxRequests,
+		MaxConnectionsPerIp:            createDto.MaxConnectionsPerIp,
+		BandwidthBpsLimitPerConnection: bandwidthBpsLimitPerConnectionPtr,
+		BandwidthLimitOnlyAfterBytes:   bandwidthLimitOnlyAfterBytesPtr,
+		ResponseCodeOnMaxConnections:   createDto.ResponseCodeOnMaxConnections,
+	}
+
+	err = repo.persistentDbSvc.Handler.Create(&securityRuleModel).Error
+	if err != nil {
+		return ruleId, errors.New("CreateMappingSecurityRuleInfraError")
+	}
+
+	ruleId, err = valueObject.NewMappingSecurityRuleId(securityRuleModel.ID)
+	if err != nil {
+		return ruleId, err
+	}
+
+	return ruleId, repo.recreateSecurityRuleFile(ruleId)
+}
+
+func (repo *MappingCmdRepo) UpdateSecurityRule(
+	updateDto dto.UpdateMappingSecurityRule,
+) error {
+	updateMap := map[string]interface{}{}
+
+	if updateDto.Name != nil {
+		updateMap["name"] = updateDto.Name.String()
+	}
+
+	if updateDto.Description != nil {
+		updateMap["description"] = updateDto.Description.String()
+	}
+
+	allowedIps := []string{}
+	for _, ipAddress := range updateDto.AllowedIps {
+		allowedIps = append(allowedIps, ipAddress.String())
+	}
+	if len(allowedIps) > 0 {
+		updateMap["allowed_ips"] = allowedIps
+	}
+
+	blockedIps := []string{}
+	for _, ipAddress := range updateDto.BlockedIps {
+		blockedIps = append(blockedIps, ipAddress.String())
+	}
+	if len(blockedIps) > 0 {
+		updateMap["blocked_ips"] = blockedIps
+	}
+
+	if updateDto.RpsSoftLimitPerIp != nil {
+		updateMap["rps_soft_limit_per_ip"] = *updateDto.RpsSoftLimitPerIp
+	}
+
+	if updateDto.RpsHardLimitPerIp != nil {
+		updateMap["rps_hard_limit_per_ip"] = *updateDto.RpsHardLimitPerIp
+	}
+
+	if updateDto.ResponseCodeOnMaxRequests != nil {
+		updateMap["response_code_on_max_requests"] = *updateDto.ResponseCodeOnMaxRequests
+	}
+
+	if updateDto.MaxConnectionsPerIp != nil {
+		updateMap["max_connections_per_ip"] = *updateDto.MaxConnectionsPerIp
+	}
+
+	if updateDto.BandwidthBpsLimitPerConnection != nil {
+		updateMap["bandwidth_bps_limit_per_connection"] = updateDto.BandwidthBpsLimitPerConnection.Uint64()
+	}
+
+	if updateDto.BandwidthLimitOnlyAfterBytes != nil {
+		updateMap["bandwidth_limit_only_after_bytes"] = updateDto.BandwidthLimitOnlyAfterBytes.Uint64()
+	}
+
+	if updateDto.ResponseCodeOnMaxConnections != nil {
+		updateMap["response_code_on_max_connections"] = *updateDto.ResponseCodeOnMaxConnections
+	}
+
+	err := repo.persistentDbSvc.Handler.Model(&dbModel.MappingSecurityRule{}).
+		Where("id = ?", updateDto.Id.Uint64()).Updates(updateMap).Error
+	if err != nil {
+		return errors.New("UpdateMappingSecurityRuleInfraError: " + err.Error())
+	}
+
+	return repo.recreateSecurityRuleFile(updateDto.Id)
+}
+
+func (repo *MappingCmdRepo) DeleteSecurityRule(
+	ruleId valueObject.MappingSecurityRuleId,
+) error {
+	err := infraHelper.ValidateWebServerConfig()
+	if err != nil {
+		return err
+	}
+
+	ruleEntity, err := repo.mappingQueryRepo.ReadFirstSecurityRule(
+		dto.ReadMappingSecurityRulesRequest{MappingSecurityRuleId: &ruleId},
+	)
+	if err != nil {
+		return errors.New("ReadMappingSecurityRuleError: " + err.Error())
+	}
+
+	err = repo.persistentDbSvc.Handler.Delete(
+		dbModel.MappingSecurityRule{}, ruleEntity.Id.Uint64(),
+	).Error
+	if err != nil {
+		return err
+	}
+
+	toRemoveFilePaths := []string{
+		infraEnvs.MappingsSecurityRulesConfDir + "/" + ruleEntity.Id.String() + ".global.conf",
+		infraEnvs.MappingsSecurityRulesConfDir + "/" + ruleEntity.Id.String() + ".embeddable.conf",
+	}
+	for _, toRemoveFilePath := range toRemoveFilePaths {
+		err = os.Remove(toRemoveFilePath)
+		if err != nil {
+			return errors.New("RemoveSecurityRuleFileError: " + err.Error())
+		}
 	}
 
 	return infraHelper.ReloadWebServer()
