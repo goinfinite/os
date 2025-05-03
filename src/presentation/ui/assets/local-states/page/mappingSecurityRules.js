1ae611912f2b72d2691ebe5c7f1e7203510d45ff
ntorga <northontorga@gmail.com>
2025-05-03 13:05:33 -0300

    feat: add list manager and use on ip control

--- src/presentation/ui/assets/local-states/page/mappingSecurityRules.js
+++ src/presentation/ui/assets/local-states/page/mappingSecurityRules.js
@@ -6,8 +6,8 @@ document.addEventListener("alpine:init", () => {
         id: "",
         name: "",
         description: "",
-        allowedIpsText: "",
-        blockedIpsText: "",
+        allowedIps: [],
+        blockedIps: [],
         rpsSoftLimitPerIp: "",
         rpsHardLimitPerIp: "",
         responseCodeOnMaxRequests: "429",
@@ -27,43 +27,6 @@ document.addEventListener("alpine:init", () => {
     get shouldDisableUpdateMappingSecurityRuleSubmitButton() {
       return this.mappingSecurityRule.name === "";
     },
-    prepareFormData() {
-      if (this.mappingSecurityRule.allowedIpsText) {
-        const allowedIps = this.mappingSecurityRule.allowedIpsText
-          .split("\n")
-          .map((ip) => ip.trim())
-          .filter((ip) => ip !== "");
-
-        if (allowedIps.length > 0) {
-          document
-            .querySelector("form")
-            .insertAdjacentHTML(
-              "beforeend",
-              `<input type="hidden" name="allowedIps" value='${JSON.stringify(
-                allowedIps
-              )}' />`
-            );
-        }
-      }
-
-      if (this.mappingSecurityRule.blockedIpsText) {
-        const blockedIps = this.mappingSecurityRule.blockedIpsText
-          .split("\n")
-          .map((ip) => ip.trim())
-          .filter((ip) => ip !== "");
-
-        if (blockedIps.length > 0) {
-          document
-            .querySelector("form")
-            .insertAdjacentHTML(
-              "beforeend",
-              `<input type="hidden" name="blockedIps" value='${JSON.stringify(
-                blockedIps
-              )}' />`
-            );
-        }
-      }
-    },
 
     isCreateMappingSecurityRuleModalOpen: false,
     openCreateMappingSecurityRuleModal() {
@@ -71,7 +34,6 @@ document.addEventListener("alpine:init", () => {
       this.isCreateMappingSecurityRuleModalOpen = true;
     },
     closeCreateMappingSecurityRuleModal() {
-      this.prepareFormData();
       this.isCreateMappingSecurityRuleModalOpen = false;
     },
 
@@ -84,70 +46,10 @@ document.addEventListener("alpine:init", () => {
           "mappingSecurityRuleEntity_" + mappingSecurityRuleId
         ).textContent
       );
-
-      this.mappingSecurityRule.id = mappingSecurityRuleId;
-      this.mappingSecurityRule.name = mappingSecurityRuleEntity.name;
-
-      if (mappingSecurityRuleEntity.description) {
-        this.mappingSecurityRule.description =
-          mappingSecurityRuleEntity.description;
-      }
-
-      if (
-        mappingSecurityRuleEntity.allowedIps &&
-        mappingSecurityRuleEntity.allowedIps.length > 0
-      ) {
-        this.mappingSecurityRule.allowedIpsText =
-          mappingSecurityRuleEntity.allowedIps.join("\n");
-      }
-
-      if (
-        mappingSecurityRuleEntity.blockedIps &&
-        mappingSecurityRuleEntity.blockedIps.length > 0
-      ) {
-        this.mappingSecurityRule.blockedIpsText =
-          mappingSecurityRuleEntity.blockedIps.join("\n");
-      }
-
-      if (mappingSecurityRuleEntity.rpsSoftLimitPerIp) {
-        this.mappingSecurityRule.rpsSoftLimitPerIp =
-          mappingSecurityRuleEntity.rpsSoftLimitPerIp;
-      }
-
-      if (mappingSecurityRuleEntity.rpsHardLimitPerIp) {
-        this.mappingSecurityRule.rpsHardLimitPerIp =
-          mappingSecurityRuleEntity.rpsHardLimitPerIp;
-      }
-
-      if (mappingSecurityRuleEntity.responseCodeOnMaxRequests) {
-        this.mappingSecurityRule.responseCodeOnMaxRequests =
-          mappingSecurityRuleEntity.responseCodeOnMaxRequests;
-      }
-
-      if (mappingSecurityRuleEntity.maxConnectionsPerIp) {
-        this.mappingSecurityRule.maxConnectionsPerIp =
-          mappingSecurityRuleEntity.maxConnectionsPerIp;
-      }
-
-      if (mappingSecurityRuleEntity.responseCodeOnMaxConnections) {
-        this.mappingSecurityRule.responseCodeOnMaxConnections =
-          mappingSecurityRuleEntity.responseCodeOnMaxConnections;
-      }
-
-      if (mappingSecurityRuleEntity.bandwidthBpsLimitPerConnection) {
-        this.mappingSecurityRule.bandwidthBpsLimitPerConnection =
-          mappingSecurityRuleEntity.bandwidthBpsLimitPerConnection;
-      }
-
-      if (mappingSecurityRuleEntity.bandwidthLimitOnlyAfterBytes) {
-        this.mappingSecurityRule.bandwidthLimitOnlyAfterBytes =
-          mappingSecurityRuleEntity.bandwidthLimitOnlyAfterBytes;
-      }
-
+      this.mappingSecurityRule = mappingSecurityRuleEntity;
       this.isUpdateMappingSecurityRuleModalOpen = true;
     },
     closeUpdateMappingSecurityRuleModal() {
-      this.prepareFormData();
       this.isUpdateMappingSecurityRuleModalOpen = false;
     },
 
