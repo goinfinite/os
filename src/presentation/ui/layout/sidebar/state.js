48c7e0313232db952f05e8473ccfef3f6a2b986a
ntorga <northontorga@gmail.com>
2025-05-26 17:26:07 -0300

    fix: update active item with alpine

--- src/presentation/ui/layout/sidebar/state.js
+++ src/presentation/ui/layout/sidebar/state.js
@@ -3,12 +3,16 @@ document.addEventListener("alpine:init", () => {
     isSidebarCollapsed: Alpine.$persist(true).as(
       "osDashboard.isSidebarCollapsed"
     ),
+    activeItemPath: "/overview/",
+    init() {
+      this.activeItemPath = window.location.pathname;
+    },
+    isActivePath(path) {
+      return String(this.activeItemPath).startsWith(path);
+    },
     clearSession() {
       document.cookie = `${Infinite.Envs.AccessTokenCookieKey}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
       window.location.href = "/login/";
-    },
-    isActivePath(path) {
-      return window.location.pathname.startsWith(path);
     },
   }));
 });
