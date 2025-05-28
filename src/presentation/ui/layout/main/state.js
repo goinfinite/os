2b04bfefc9c2c3ea00614c0e47b4cbf2e542c54f
ntorga <northontorga@gmail.com>
2025-05-28 10:17:49 -0300

    fix: use $store to update footer

--- src/presentation/ui/layout/main/state.js
+++ src/presentation/ui/layout/main/state.js
@@ -17,11 +17,25 @@ document.addEventListener("alpine:initializing", () => {
     toggleScheduledTasksPopover() {
       this.displayScheduledTasksPopover = !this.displayScheduledTasksPopover;
     },
+    refreshFooter() {
+      htmx.ajax("GET", "/fragment/footer", {
+        select: "#footer",
+        target: "#footer",
+        swap: "outerHTML transition:true",
+      });
+    },
     refreshScheduledTasksPopover() {
-      window.dispatchEvent(new CustomEvent("refresh:footer"));
+      this.refreshFooter();
       setTimeout(() => {
         this.displayScheduledTasksPopover = true;
       }, 1000);
+    },
+    navigateTo(path) {
+      htmx.ajax("GET", path, {
+        select: "#page-content",
+        target: "#page-content",
+        swap: "outerHTML transition:true",
+      });
     },
   });
 });
