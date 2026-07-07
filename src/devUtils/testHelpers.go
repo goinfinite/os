f31a51ecd77419d5d7bbb77b84ee37cec3e834ea
ntorga <northontorga@gmail.com>
2026-07-07 15:32:32 -0300

    fix(tests): use EnvsInspector for auto-fillable env vars in test helpers

--- src/devUtils/testHelpers.go
+++ src/devUtils/testHelpers.go
@@ -2,10 +2,13 @@ package testHelpers
 
 import (
 	"encoding/base64"
+	"log/slog"
+	"os"
 
 	infraEnvs "github.com/goinfinite/os/src/infra/envs"
 	internalDbInfra "github.com/goinfinite/os/src/infra/internalDatabase"
-	"github.com/joho/godotenv"
+	tkValueObject "github.com/goinfinite/tk/src/domain/valueObject"
+	tkPresentation "github.com/goinfinite/tk/src/presentation"
 )
 
 func GenerateString(desiredSize int) string {
@@ -16,9 +19,30 @@ func GenerateString(desiredSize int) string {
 }
 
 func LoadEnvVars() {
-	loadEnvErr := godotenv.Load(infraEnvs.InfiniteOsEnvFilePath)
-	if loadEnvErr != nil {
-		panic("LoadingEnvFileError: " + loadEnvErr.Error())
+	envFilePath, err := tkValueObject.NewUnixAbsoluteFilePath(
+		infraEnvs.InfiniteOsEnvFilePath, false,
+	)
+	if err != nil {
+		slog.Error("InvalidEnvFilePath", slog.String("error", err.Error()))
+		os.Exit(1)
+	}
+
+	requiredEnvVars := []string{
+		"ACCOUNT_API_KEY_SECRET",
+		"JWT_SECRET",
+	}
+	autoFillableEnvVars := []string{
+		"ACCOUNT_API_KEY_SECRET",
+		"JWT_SECRET",
+	}
+
+	envsInspector := tkPresentation.NewEnvsInspector(
+		&envFilePath, requiredEnvVars, autoFillableEnvVars,
+	)
+	inspectErr := envsInspector.Inspect()
+	if inspectErr != nil {
+		slog.Error("EnvsInspectorError", slog.String("error", inspectErr.Error()))
+		os.Exit(1)
 	}
 }
 
