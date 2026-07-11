ea9bf8196ca746199ef86d84d3b35e01bdec795c
ntorga <northontorga@gmail.com>
2026-07-11 17:10:32 -0300

    fix: swagger bug on root path access

--- src/presentation/api/middleware/readOnlyMode.go
+++ src/presentation/api/middleware/readOnlyMode.go
@@ -11,27 +11,27 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func IsSkippableApiCall(req *http.Request, apiBasePath string) bool {
-	urlPath := req.URL.Path
-	isNotApi := !strings.HasPrefix(urlPath, apiBasePath)
+var skippableApiCallsRegex *regexp.Regexp = regexp.MustCompile(
+	`^(/v\d{1,2}/(auth|health|setup)/?|/swagger/?)`,
+)
+
+func IsSkippableApiCall(httpReq *http.Request, apiBasePath string) bool {
+	isNotApi := !strings.HasPrefix(httpReq.URL.Path, apiBasePath)
 	if isNotApi {
 		return true
 	}
+	apiCallWithoutBasePath := strings.TrimPrefix(httpReq.URL.Path, apiBasePath)
 
-	urlSkipRegex := regexp.MustCompile(
-		`^` + apiBasePath + `/(v\d{1,2}/(auth|health|setup)|/swagger)/?`,
-	)
-	return urlSkipRegex.MatchString(urlPath)
+	return skippableApiCallsRegex.MatchString(apiCallWithoutBasePath)
 }
 
 func ReadOnlyMode(apiBasePath string) echo.MiddlewareFunc {
 	return func(subsequentHandler echo.HandlerFunc) echo.HandlerFunc {
 		return func(echoContext echo.Context) error {
-		isReadOnlyModeEnabled, err :=
-			tkVoUtil.InterfaceToBool(os.Getenv("READ_ONLY_MODE"))
-		if err != nil || !isReadOnlyModeEnabled {
-			return subsequentHandler(echoContext)
-		}
+			isReadOnlyModeEnabled, err := tkVoUtil.InterfaceToBool(os.Getenv("READ_ONLY_MODE"))
+			if err != nil || !isReadOnlyModeEnabled {
+				return subsequentHandler(echoContext)
+			}
 
 			shouldSkip := IsSkippableApiCall(echoContext.Request(), apiBasePath)
 			if shouldSkip {
