38a0e8b9b9b92ae404b99481910a5da3533d8aea
ntorga <northontorga@gmail.com>
2026-07-14 12:38:13 -0300

    fix: skip is dev mode check when empty var

--- src/infra/helper/isDevMode.go
+++ src/infra/helper/isDevMode.go
@@ -9,6 +9,10 @@ import (
 )
 
 func IsDevMode() bool {
+	if os.Getenv(infraEnvs.DevModeEnvKey) == "" {
+		return false
+	}
+
 	isDevModeEnabled, err := tkVoUtil.InterfaceToBool(os.Getenv(infraEnvs.DevModeEnvKey))
 	if err != nil {
 		slog.Debug("ParseDevModeEnvError", slog.String("err", err.Error()))
@@ -19,6 +23,10 @@ func IsDevMode() bool {
 }
 
 func IsSilentExitMode() bool {
+	if os.Getenv(infraEnvs.SilentExitModeEnvKey) == "" {
+		return false
+	}
+
 	isSilentExitMode, err := tkVoUtil.InterfaceToBool(os.Getenv(infraEnvs.SilentExitModeEnvKey))
 	if err != nil {
 		slog.Debug("ParseSilentExitModeEnvError", slog.String("err", err.Error()))
