04c9439afcb47a4162a342f9c3c4e17f7dd3e227
ntorga <northontorga@gmail.com>
2026-09-03 12:14:40 -0300

    feat(scheduledTask): return created task id to callers

--- src/domain/repository/scheduledTaskCmdRepo.go
+++ src/domain/repository/scheduledTaskCmdRepo.go
@@ -3,10 +3,11 @@ package repository
 import (
 	"github.com/goinfinite/os/src/domain/dto"
 	"github.com/goinfinite/os/src/domain/entity"
+	"github.com/goinfinite/os/src/domain/valueObject"
 )
 
 type ScheduledTaskCmdRepo interface {
-	Create(createDto dto.CreateScheduledTask) error
+	Create(dto.CreateScheduledTask) (valueObject.ScheduledTaskId, error)
 	Update(updateDto dto.UpdateScheduledTask) error
 	Run(pendingTask entity.ScheduledTask) error
 }
